72bb2540ed44ba9ce9f9647eb697a6a20ac82474
liuyou2 <405240259@qq.com>
2019-11-29 22:49:14 +0800

    init commit

---
+++ dss-server/src/main/java/com/webank/wedatasphere/dss/server/function/NodeServiceFunction.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2019 WeBank
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.webank.wedatasphere.dss.server.function;
+
+import com.webank.wedatasphere.dss.appjoint.exception.AppJointErrorException;
+import com.webank.wedatasphere.dss.appjoint.execution.core.AppJointNode;
+import com.webank.wedatasphere.dss.appjoint.service.NodeService;
+import com.webank.wedatasphere.dss.appjoint.service.session.Session;
+
+import java.util.Map;
+
+
+public interface NodeServiceFunction{
+    Map<String,Object> accept(NodeService nodeService, Session session, AppJointNode node, Map<String, Object> requestBody) throws AppJointErrorException;
+}

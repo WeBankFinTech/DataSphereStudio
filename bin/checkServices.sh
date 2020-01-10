3626b784e8e85fe6ccd6c01d6d1d24d4c1e40d60
chaogefeng <673120261@163.com>
2020-01-10 19:51:39 +0800

    1.Automated installation enhancements. 2.Adapt to linkis changes. 3.Add jdbc node type. 4.Resolving module dependency conflicts close #79 #80 #81 #82

--- bin/checkServices.sh
+++ bin/checkServices.sh
@@ -20,18 +20,59 @@ shellDir=`dirname $0`
 workDir=`cd ${shellDir}/..;pwd`
 
 ##load config
-source ${workDir}/conf/config.sh
-source ${workDir}/conf/db.sh
+export LINKIS_DSS_CONF_FILE=${LINKIS_DSS_CONF_FILE:-"${workDir}/conf/config.sh"}
+export DISTRIBUTION=${DISTRIBUTION:-"${workDir}/conf/config.sh"}
+source ${LINKIS_DSS_CONF_FILE}
+source ${DISTRIBUTION}
 
 MICRO_SERVICE_NAME=$1
 MICRO_SERVICE_IP=$2
 MICRO_SERVICE_PORT=$3
-echo "<--------------------------------------------------------------------------->"
-echo "Start to Check if your microservice:$MICRO_SERVICE_NAME is normal via telnet"
-echo ""
-if [ ! -d $DSS_INSTALL_HOME/$MICRO_SERVICE_NAME ];then
-  echo "$MICRO_SERVICE_NAME is not installed,the check steps will be skipped"
-  exit 0
+
+local_host="`hostname --fqdn`"
+
+ipaddr="`hostname -i`"
+
+function isLocal(){
+    if [ "$1" == "127.0.0.1" ];then
+        return 0
+    elif [ $1 == "localhost" ]; then
+        return 0
+    elif [ $1 == $local_host ]; then
+        return 0
+    elif [ $1 == $ipaddr ]; then
+        return 0
+    fi
+        return 1
+}
+
+function executeCMD(){
+   isLocal $1
+   flag=$?
+   echo "Is local "$flag
+   if [ $flag == "0" ];then
+      eval $2
+   else
+      ssh -p $SSH_PORT $1 $2
+   fi
+
+}
+
+#echo "<--------------------------------------------------------------------------->"
+#echo "Start to Check if your microservice:$MICRO_SERVICE_NAME is normal via telnet"
+#echo ""
+#if ! executeCMD $SERVER_IP "test -e $DSS_INSTALL_HOME/$MICRO_SERVICE_NAME"; then
+#  echo "$MICRO_SERVICE_NAME is not installed,the check steps will be skipped"
+#  exit 0
+#fi
+echo "==========================================================="
+echo $MICRO_SERVICE_NAME
+echo $MICRO_SERVICE_IP
+echo $MICRO_SERVICE_PORT
+echo "==========================================================="
+
+if [ $MICRO_SERVICE_NAME == "visualis-server" ]||[ $MICRO_SERVICE_IP == "127.0.0.1" ]; then
+    MICRO_SERVICE_IP="`hostname -i`"
 fi
 
 result=`echo -e "\n" | telnet $MICRO_SERVICE_IP $MICRO_SERVICE_PORT 2>/dev/null | grep Connected | wc -l`
@@ -42,7 +83,7 @@ else
       echo "ERROR your $MICRO_SERVICE_NAME microservice is not start successful !!! ERROR logs as follows :"
       echo "PLEAESE CHECK  DETAIL LOG,LOCATION:$DSS_INSTALL_HOME/$MICRO_SERVICE_NAME/logs/linkis.out"
       echo '<------------------------------------------------------------->'
-      tail -n 50 $DSS_INSTALL_HOME/$MICRO_SERVICE_NAME/logs/*.out
+      executeCMD $MICRO_SERVICE_IP "tail -n 50 $DSS_INSTALL_HOME/$MICRO_SERVICE_NAME/logs/*.out"
       echo '<-------------------------------------------------------------->'
       echo "PLEAESE CHECK DETAIL LOG,LOCATION:$DSS_INSTALL_HOME/$MICRO_SERVICE_NAME/logs/linkis.out"
       exit 1

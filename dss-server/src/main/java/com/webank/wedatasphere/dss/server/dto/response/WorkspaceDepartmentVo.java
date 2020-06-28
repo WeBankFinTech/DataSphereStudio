e8bd53a785dae288c260c58ed9a7d4758fadcc9d
Schumi Yi <yijunwen@chinatelecom.cn>
2020-06-28 13:47:14 +0800

    complete part of workspace develop

---
+++ dss-server/src/main/java/com/webank/wedatasphere/dss/server/dto/response/WorkspaceDepartmentVo.java
@@ -0,0 +1,27 @@
+package com.webank.wedatasphere.dss.server.dto.response;
+
+/**
+ * Created by schumiyi on 2020/6/23
+ */
+public class WorkspaceDepartmentVo {
+
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

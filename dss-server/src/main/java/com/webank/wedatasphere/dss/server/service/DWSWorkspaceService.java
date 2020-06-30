d20935da2fcc606d7ba102f2ee15f8f1f527aad4
Adamyuanyuan <Adamyuanyuan@gmail.com>
2020-06-30 21:14:45 +0800

    add workspace favorites and resolve conflicts

--- dss-server/src/main/java/com/webank/wedatasphere/dss/server/service/DWSWorkspaceService.java
+++ dss-server/src/main/java/com/webank/wedatasphere/dss/server/service/DWSWorkspaceService.java
@@ -5,6 +5,7 @@ import com.webank.wedatasphere.dss.server.dto.response.HomepageVideoVo;
 import com.webank.wedatasphere.dss.server.dto.response.OnestopMenuVo;
 import com.webank.wedatasphere.dss.server.entity.DWSWorkspace;
 import com.webank.wedatasphere.dss.server.dto.response.WorkspaceDepartmentVo;
+import com.webank.wedatasphere.dss.server.dto.response.*;
 
 import java.util.List;
 
@@ -27,4 +28,19 @@ public interface DWSWorkspaceService {
     List<OnestopMenuVo> getWorkspaceManagements(Long workspaceId, String username, boolean isChinese);
 
     List<OnestopMenuVo> getWorkspaceApplications(Long workspaceId, String username, boolean isChinese);
+
+    DWSWorkspace getWorkspacesById(Long id);
+
+    /**
+     * 查询用户收藏的应用，如果是新用户，就在数据库给它插入默认两个收藏：脚本开发与工作流 workflow scriptis
+     * @param workspaceId
+     * @param username
+     * @param isChinese
+     * @return
+     */
+    List<WorkspaceFavoriteVo> getWorkspaceFavorites(Long workspaceId, String username, boolean isChinese);
+
+    Long addFavorite(String username, Long workspaceId, Long menuApplicationId);
+
+    Long deleteFavorite(String username, Long favouritesId);
 }

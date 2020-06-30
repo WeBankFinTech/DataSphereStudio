c4618ce22c6cde6efd56064aa56918dd1bee988f
Adamyuanyuan <Adamyuanyuan@gmail.com>
2020-06-30 21:14:45 +0800

    add workspace favorites and resolve conflicts

--- dss-server/src/main/java/com/webank/wedatasphere/dss/server/dao/WorkspaceMapper.java
+++ dss-server/src/main/java/com/webank/wedatasphere/dss/server/dao/WorkspaceMapper.java
@@ -2,6 +2,11 @@ package com.webank.wedatasphere.dss.server.dao;
 
 import com.webank.wedatasphere.dss.server.dto.response.*;
 import com.webank.wedatasphere.dss.server.entity.*;
+import com.webank.wedatasphere.dss.server.dto.response.HomepageDemoInstanceVo;
+import com.webank.wedatasphere.dss.server.dto.response.HomepageDemoMenuVo;
+import com.webank.wedatasphere.dss.server.dto.response.HomepageVideoVo;
+import com.webank.wedatasphere.dss.server.dto.response.WorkspaceFavoriteVo;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -36,4 +41,11 @@ public interface WorkspaceMapper {
     List<OnestopMenuAppInstanceVo> getMenuAppInstancesCn(Long id);
     List<OnestopMenuAppInstanceVo> getMenuAppInstanceEn(Long id);
 
+    List<WorkspaceFavoriteVo> getWorkspaceFavoritesCn(@Param("username") String username, @Param("workspaceId") Long workspaceId);
+
+    List<WorkspaceFavoriteVo> getWorkspaceFavoritesEn(@Param("username") String username, @Param("workspaceId") Long workspaceId);
+
+    void addFavorite(DWSFavorite dwsFavorite);
+
+    void deleteFavorite(Long favouritesId);
 }

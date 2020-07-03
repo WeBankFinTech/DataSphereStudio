2c94d58602f9257e53f96b849dbcc6cee47d81e3
yuchenyao <88562874@qq.com>
2020-07-03 18:30:36 +0800

    修改workspaceId 传参格式

--- web/src/js/module/project/index.vue
+++ web/src/js/module/project/index.vue
@@ -216,7 +216,7 @@ export default {
         return item.id === projectData.taxonomyID;
       });
       if (this.checkName(projectList[0].dssProjectList, projectData.name, projectData.id)) return this.$Message.warning(this.$t('message.project.nameUnrepeatable'));
-      projectData.workspaceId = this.workspaceId;
+      projectData.workspaceId = Number(this.workspaceId);
       this.loading = true;
       if (this.actionType === 'add') {
         api.fetch('/dss/addProject', projectData, 'post').then(() => {

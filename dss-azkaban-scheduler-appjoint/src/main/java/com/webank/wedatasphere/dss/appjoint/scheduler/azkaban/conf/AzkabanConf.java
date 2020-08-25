d54f086242b414eacfc83307c7057e1090ae7b70
yuxin.yuan <yuanyuxin@chinatelecom.cn>
2020-08-25 16:39:48 +0800

    1. 修复DSS与Schedulis的数据库认证

--- dss-azkaban-scheduler-appjoint/src/main/java/com/webank/wedatasphere/dss/appjoint/scheduler/azkaban/conf/AzkabanConf.java
+++ dss-azkaban-scheduler-appjoint/src/main/java/com/webank/wedatasphere/dss/appjoint/scheduler/azkaban/conf/AzkabanConf.java
@@ -10,5 +10,15 @@ public class AzkabanConf {
 
     public static final CommonVars<String> AZKABAN_BASE_URL = CommonVars.apply("wds.dss.appjoint.scheduler.azkaban.address", "");
     public static final CommonVars<String> DEFAULT_STORE_PATH = CommonVars.apply("wds.dss.appjoint.scheduler.project.store.dir", "/appcom/tmp/wds/dss");
-    public static final CommonVars<String> AZKABAN_LOGIN_PWD = CommonVars.apply("wds.dss.appjoint.scheduler.azkaban.login.passwd", "password");
+    public static final CommonVars<String> AZKABAN_LOGIN_PWD =
+        CommonVars.apply("wds.dss.appjoint.scheduler.azkaban.login.passwd", "userpwd");
+
+    public static final CommonVars<String> DATASOURCE_URL =
+        CommonVars.apply("wds.linkis.server.mybatis.datasource.url", "");
+    public static final CommonVars<String> DATASOURCE_USERNAME =
+        CommonVars.apply("wds.linkis.server.mybatis.datasource.username", "");
+    public static final CommonVars<String> DATASOURCE_PASSWORD =
+        CommonVars.apply("wds.linkis.server.mybatis.datasource.password", "");
+    public static final CommonVars<String> DATASOURCE_DRIVER_CLASS_NAME =
+        CommonVars.apply("wds.linkis.server.mybatis.datasource.driver-class-name", "com.mysql.jdbc.Driver");
 }

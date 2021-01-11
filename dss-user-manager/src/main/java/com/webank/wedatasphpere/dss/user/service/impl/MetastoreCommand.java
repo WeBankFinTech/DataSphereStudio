174017c5d2cc3be7eb543bb1fcc8846ad7a839cc
liu <liuzexin@chinatelecom.cn>
2021-01-11 15:22:17 +0800

    The Metastore interface was added, and the Metastore service needs to be configured

---
+++ dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/impl/MetastoreCommand.java
@@ -0,0 +1,39 @@
+package com.webank.wedatasphpere.dss.user.service.impl;
+
+import com.typesafe.config.Config;
+import com.webank.wedatasphpere.dss.user.dto.request.AuthorizationBody;
+import com.webank.wedatasphpere.dss.user.service.AbsCommand;
+import com.webank.wedatasphpere.dss.user.service.Command;
+import com.webank.wedatasphpere.dss.user.service.common.CommonFun;
+import com.typesafe.config.ConfigFactory;
+import java.io.IOException;
+
+/**
+ * @author anlexander
+ * @date 2021/1/5
+ */
+public class MetastoreCommand extends AbsCommand {
+
+    @Override
+    public String authorization(AuthorizationBody body) throws IOException {
+        Config parms = ConfigFactory.load("config/properties.conf");
+        String rst = createDb(body,parms);
+        return rst != Command.SUCCESS ? rst : Command.SUCCESS;
+    }
+
+    private String createDb(AuthorizationBody body, Config parms) throws IOException {
+
+        String bashCommand;
+        String userName = body.getUsername();
+        String res = null;
+        if (userName != null) {
+                String dbName = userName + parms.getString("db_tail");
+                String path = parms.getString("base_path") + dbName + ".db";
+                bashCommand = this.getClass().getClassLoader().getResource(parms.getString("metastore_sh")).getPath();
+                Runtime runtime = Runtime.getRuntime();
+                Process process = runtime.exec("sudo sh " + bashCommand + " " + userName + " " + dbName + "" + path);
+                res = CommonFun.process(process);
+        }
+        return res;
+    }
+}

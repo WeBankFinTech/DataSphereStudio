9242a5b152c2f778d9b4de0e4f874430f0b00ae5
hantang <th3408251986@163.com>
2021-01-12 10:18:29 +0800

    Create an LDAP account initialization

---
+++ dss-user-manager/src/main/resources/default/CreateLdapAccount.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LDAP_SCRIPTSERVER=$1
+LDAP_SCRIPT_ROOT=$2
+LDAP_SCRIPT=$3
+NEW_USER=$4
+PASSWORD=$5
+
+ssh -t $LDAP_SCRIPTSERVER "sudo su - root -c 'cd $LDAP_SCRIPT_ROOT && source ../venv/bin/activate  && python $LDAP_SCRIPT add_with_pw $NEW_USER -p $PASSWORD && deactivate'"
+echo "******************LDAP USER CREATED***********************"
+
+#python ldap_user.py add_with_pw wuwenchao -p 8BFZhZbiVDrDQrWF

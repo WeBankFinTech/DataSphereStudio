a0dd71363c7f506c4712a14ffc7b372b2358d0a1
hantang <th3408251986@163.com>
2021-01-13 17:23:52 +0800

    Modify the create LDAP script

--- dss-server/src/main/resources/default/CreateLdapAccount.sh
+++ dss-server/src/main/resources/default/CreateLdapAccount.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source /etc/profile
-LDAP_SCRIPTSERVER=$1
-LDAP_SCRIPT_ROOT=$2
-LDAP_SCRIPT=$3
-NEW_USER=$4
-PASSWORD=$5
-
-ssh -t $LDAP_SCRIPTSERVER "sudo su - root -c 'cd $LDAP_SCRIPT_ROOT && source ../venv/bin/activate  && python $LDAP_SCRIPT add_with_pw $NEW_USER -p $PASSWORD && deactivate'"
+echo $2
+cd $2
+echo current path`pwd`
+source ../venv/bin/activate
+python $3 add_with_pw $4 -p $5
 echo "******************LDAP USER CREATED***********************"
 
-#python ldap_user.py add_with_pw wuwenchao -p 8BFZhZbiVDrDQrWF
+
+
+

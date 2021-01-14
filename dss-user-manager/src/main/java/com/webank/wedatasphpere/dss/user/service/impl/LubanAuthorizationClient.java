d7d141eef17e2ce3f2123f46d2e871f9d1d02a06
luxiaolong <lindaluo83@126.com>
2021-01-14 12:36:35 +0800

    Fix report error, increase log

--- dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/impl/LubanAuthorizationClient.java
+++ dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/impl/LubanAuthorizationClient.java
@@ -14,13 +14,7 @@ public class LubanAuthorizationClient {
     private LubanMacroCommand lubanCommand = new LubanMacroCommand();
 
     public LubanAuthorizationClient(){
-        lubanCommand.add(new WorkspaceCommand());
-<<<<<<< HEAD
-        lubanCommand.add(new MetastoreCommand());
-        lubanCommand.add(new KerberosCommand());
-=======
-        lubanCommand.add(new LdapCommand());
->>>>>>> 4d079d2 (创建ldap账户初始化)
+
     }
 
     public String authorization(AuthorizationBody body) {

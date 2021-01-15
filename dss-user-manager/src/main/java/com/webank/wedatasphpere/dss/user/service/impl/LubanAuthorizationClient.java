282915c9d1a5d99531074805186978a7b133ec36
hantang <th3408251986@163.com>
2021-01-15 11:03:18 +0800

    Create account command using reflection dynamic add

--- dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/impl/LubanAuthorizationClient.java
+++ dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/impl/LubanAuthorizationClient.java
@@ -1,7 +1,11 @@
 package com.webank.wedatasphpere.dss.user.service.impl;
 
 
+import com.webank.wedatasphpere.dss.user.conf.DSSUserManagerConfig;
 import com.webank.wedatasphpere.dss.user.dto.request.AuthorizationBody;
+import com.webank.wedatasphpere.dss.user.service.AbsCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @program: luban-authorization
@@ -11,13 +15,25 @@ import com.webank.wedatasphpere.dss.user.dto.request.AuthorizationBody;
  **/
 public class LubanAuthorizationClient {
 
-    private LubanMacroCommand lubanCommand = new LubanMacroCommand();
+    public LubanMacroCommand lubanCommand = new LubanMacroCommand();
+    protected final Logger logger = LoggerFactory.getLogger(LubanAuthorizationClient.class);
 
-    public LubanAuthorizationClient(){
+    public LubanAuthorizationClient()  {
 
+        String[] commandPaths = DSSUserManagerConfig.USER_ACCOUNT_COMMANDS.split(",");
+        for(String classPath: commandPaths){
+            try {
+                lubanCommand.add((AbsCommand) Class.forName(classPath).newInstance());
+            } catch (Exception e) {
+                logger.info(e.getMessage());
+                e.printStackTrace();
+            }
+        }
     }
 
     public String authorization(AuthorizationBody body) {
         return lubanCommand.authorization(body);
     }
+
+
 }

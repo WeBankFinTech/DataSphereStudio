e7bf1f861a96be3b006c688936fceb753373238c
luxiaolong <lindaluo83@126.com>
2021-03-05 10:47:45 +0800

    review fix Stream close

--- dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/impl/AzkabanCommand.java
+++ dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/impl/AzkabanCommand.java
@@ -10,6 +10,7 @@ import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
@@ -41,8 +42,19 @@ public class AzkabanCommand extends AbsCommand {
         SAXReader reader = new SAXReader();
 
         File file = new File(azkPath);
+        Document document;
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            document = reader.read(fis);
+        }catch (DocumentException e){
+            throw e;
+        }finally {
+            if (fis != null) {
+                fis.close();
+            }
+        }
 
-        Document document = reader.read(file);
         Element root = document.getRootElement();
 
         Iterator it = root.elementIterator("user");

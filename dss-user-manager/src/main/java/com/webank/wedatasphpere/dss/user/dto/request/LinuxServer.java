27b40f37472303618bb83bdc0f811b0f2858550f
luxiaolong <lindaluo83@126.com>
2021-03-05 10:47:45 +0800

    review fix Stream close

--- dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/dto/request/LinuxServer.java
+++ dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/dto/request/LinuxServer.java
@@ -2,9 +2,9 @@ package com.webank.wedatasphpere.dss.user.dto.request;
 
 public class LinuxServer {
 
-    private String linuxHost; //服务器ip,用逗号分隔
-    private String linuxLoginUser; //服务器用户名
-    private String linuxLoginPassword; //服务器密码
+    private String linuxHost; //master ip，Comma separated
+    private String linuxLoginUser;
+    private String linuxLoginPassword;
 
 
     public String getLinuxHost() {

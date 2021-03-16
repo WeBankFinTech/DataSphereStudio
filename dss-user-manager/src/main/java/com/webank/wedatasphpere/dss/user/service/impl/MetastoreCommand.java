a7bc483bd068145c6f33e9dd4a45da01019acf18
luxl@chinatelecom.cn <lindaluo83@126.com>
2021-03-16 10:11:38 +0800

    Optimize account opening script

--- dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/impl/MetastoreCommand.java
+++ dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/impl/MetastoreCommand.java
@@ -2,35 +2,38 @@ package com.webank.wedatasphpere.dss.user.service.impl;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import com.webank.wedatasphere.linkis.common.conf.CommonVars;
+import com.webank.wedatasphpere.dss.user.conf.DSSUserManagerConfig;
 import com.webank.wedatasphpere.dss.user.dto.request.AuthorizationBody;
 import com.webank.wedatasphpere.dss.user.service.AbsCommand;
 import com.webank.wedatasphpere.dss.user.service.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author anlexander
  * @date 2021/1/5
  */
 public class MetastoreCommand extends AbsCommand {
+    private static final Logger logger = LoggerFactory.getLogger(MetastoreCommand.class);
 
     @Override
     public String authorization(AuthorizationBody body) throws Exception {
-        Config parms = ConfigFactory.load("config/properties.conf");
-        String rst = createDb(body,parms);
+        String rst = createDb(body);
         return rst != Command.SUCCESS ? rst : Command.SUCCESS;
     }
 
-    private String createDb(AuthorizationBody body, Config parms) throws Exception {
-
+    private String createDb(AuthorizationBody body) throws Exception {
         String bashCommand = null;
-        String scriptCmd = null;
+        String[] args = null;
         String userName = body.getUsername();
         if (userName != null) {
-                String dbName = userName + parms.getString("db_tail");
-                String path = parms.getString("base_path") + dbName + ".db";
-                bashCommand = getResource(parms.getString("metastore_sh"));
-                scriptCmd = String.format("%s %s %s %s %s %s", userName,dbName,path,
-                                         parms.getString("realm"),parms.getString("admin"),parms.getString("keytabPath"));
+            String dbName = userName + DSSUserManagerConfig.METASTORE_DB_TAIL;
+            String path = DSSUserManagerConfig.METASTORE_HDFS_PATH + "/"+dbName+".db";
+            bashCommand = getResource(DSSUserManagerConfig.METASTORE_SCRIPT_PAHT);
+            args = new String[]{ userName,dbName,path,
+                    DSSUserManagerConfig.KERBEROS_REALM,DSSUserManagerConfig.KERBEROS_ADMIN,DSSUserManagerConfig.KERBEROS_KEYTAB_PATH,DSSUserManagerConfig.KEBBEROS_ENABLE_SWITCH};
         }
-        return this.runShell(bashCommand, scriptCmd.split(" "));
+        return this.runShell(bashCommand, args);
     }
 }

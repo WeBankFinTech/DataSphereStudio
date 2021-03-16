22d41e201e8a2f8c2ed47d1f1de02e7f4e30c459
luxl@chinatelecom.cn <lindaluo83@126.com>
2021-03-16 10:11:38 +0800

    Optimize account opening script

--- dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/Command.java
+++ dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/Command.java
@@ -9,33 +9,31 @@ import java.io.IOException;
 public interface Command {
 
     final public static String SUCCESS = "success";
-    final public static String ERROR = "create account failed";
-
     /**
      * 授权开通服务
      * @param body
      * @return 成功 success  其他失败
      */
-    public String authorization(AuthorizationBody body) throws Exception;
+    public String authorization(AuthorizationBody body) throws DocumentException, IOException, Exception;
 
     /**
      * 关闭授权
      * @param body
      * @return 成功 success  其他失败
      */
-    public String undoAuthorization(AuthorizationBody body) throws Exception;
+    public String undoAuthorization(AuthorizationBody body);
 
     /**
      * 扩容
      * @param body
      * @return 成功 success  其他失败
      */
-    public String capacity(AuthorizationBody body) throws Exception;
+    public String capacity(AuthorizationBody body);
 
     /**
      * 续费
      * @param body
      * @return 成功 success  其他失败
      */
-    public String renew(AuthorizationBody body) throws Exception;
+    public String renew(AuthorizationBody body);
 }

362a888c2789e0253d9a8f12d1e9c6e91727e822
luxl@chinatelecom.cn <lindaluo83@126.com>
2021-03-24 09:02:02 +0800

    add License

--- dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/Command.java
+++ dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/Command.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2019 WeBank
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
 package com.webank.wedatasphpere.dss.user.service;
 
 
@@ -9,6 +27,7 @@ import java.io.IOException;
 public interface Command {
 
     final public static String SUCCESS = "success";
+    final public static String ERROR = "error";
     /**
      * 授权开通服务
      * @param body
@@ -21,19 +40,19 @@ public interface Command {
      * @param body
      * @return 成功 success  其他失败
      */
-    public String undoAuthorization(AuthorizationBody body);
+    public String undoAuthorization(AuthorizationBody body) throws Exception;
 
     /**
      * 扩容
      * @param body
      * @return 成功 success  其他失败
      */
-    public String capacity(AuthorizationBody body);
+    public String capacity(AuthorizationBody body) throws Exception;
 
     /**
      * 续费
      * @param body
      * @return 成功 success  其他失败
      */
-    public String renew(AuthorizationBody body);
+    public String renew(AuthorizationBody body) throws Exception;
 }

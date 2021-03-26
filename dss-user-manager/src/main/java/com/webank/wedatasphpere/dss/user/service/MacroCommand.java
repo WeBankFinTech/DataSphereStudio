7435f9177f3957f3faf7540e3c103f57fe1ceb7c
luxl@chinatelecom.cn <lindaluo83@126.com>
2021-03-26 08:50:12 +0800

    Modify test and project name

--- dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/MacroCommand.java
+++ dss-user-manager/src/main/java/com/webank/wedatasphpere/dss/user/service/MacroCommand.java
@@ -20,7 +20,7 @@ package com.webank.wedatasphpere.dss.user.service;
 
 
 /**
- * @program: luban-authorization
+ * @program: user-authorization
  * @description: 开通命令接口
  * @author: luxl@chinatelecom.cn
  * @create: 2020-08-10 14:24

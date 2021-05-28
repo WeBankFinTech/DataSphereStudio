da66de09f6ad575899c2cde04c47c355064a1267
汪鸣峰 <wangmingfeng@chelun.com>
2021-05-28 18:21:17 +0800

    日志集成基本完成 DAG集成完成 一些原有lint修复

--- web/src/apps/workflows/module/process/dag/source/normal-node.js
+++ web/src/apps/workflows/module/process/dag/source/normal-node.js
@@ -1,7 +1,8 @@
-import $ from 'jquery';
-import {Node} from 'butterfly-dag';
+import $ from 'jquery'
+import {Node} from 'butterfly-dag'
 
-import './normal-node.scss';
+import './normal-node.scss'
+import util from '@/common/util'
 
 class NormalNode extends Node {
   constructor(opts) {
@@ -14,9 +15,19 @@ class NormalNode extends Node {
       .css('top', opts.top)
       .css('left', opts.left)
       .attr('id', opts.id)
-      .addClass(opts.options.className);
-
-    let logoContainer = $(`<div class="logo-container">${opts.options.label}</div>`);
+      .addClass(opts.options.className)
+      .on('click', () => {
+        util.Hub.$emit('dagLog', opts.options)
+      })
+    let icon = '<i></i>',
+      state = '<span></span>'
+    if (opts.options.node_type === 'SHELL') {
+      icon = '<i style="color:rgb(121, 113, 246);margin-right: 5px;" class="ivu-icon ivu-icon-md-code-working"></i>'
+    }
+    if (opts.options.state) {
+      state = `<i title='${opts.options.state.desc}' style='color:${opts.options.state.color};margin-left: 5px' class="ivu-icon ivu-icon-ios-radio-button-on"></i>`
+    }
+    let logoContainer = $(`<div class="logo-container">${icon}<span>${opts.options.label}</span>${state}</div>`);
     logoContainer.addClass(opts.options.className);
 
     container.append(logoContainer);

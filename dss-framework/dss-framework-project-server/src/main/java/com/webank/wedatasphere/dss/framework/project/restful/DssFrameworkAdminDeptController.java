f951bb5bfb285e8ad818f53a65d139aa86ebc1bc
lvjw12 <lvjiawei12@chinatelecom.cn>
2021-06-10 18:50:44 +0800

    移动到原有module

--- dss-framework/dss-framework-project-server/src/main/java/com/webank/wedatasphere/dss/framework/project/restful/DssFrameworkAdminDeptController.java
+++ dss-framework/dss-framework-project-server/src/main/java/com/webank/wedatasphere/dss/framework/project/restful/DssFrameworkAdminDeptController.java
@@ -1,10 +1,15 @@
 package com.webank.wedatasphere.dss.framework.project.restful;
 
 
+import com.webank.wedatasphere.dss.framework.admin.common.constant.UserConstants;
 import com.webank.wedatasphere.dss.framework.admin.common.domain.Message;
+import com.webank.wedatasphere.dss.framework.admin.common.utils.StringUtils;
 import com.webank.wedatasphere.dss.framework.admin.pojo.entity.DssAdminDept;
 import com.webank.wedatasphere.dss.framework.admin.service.DssAdminDeptService;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.annotation.Resource;
 import javax.ws.rs.Consumes;
@@ -14,8 +19,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
+import javax.ws.rs.POST;
+
 
 /**
  * <p>
@@ -26,7 +31,6 @@ import java.util.List;
  * @since 2021-06-01
  */
 //@RestController
-////@Api(tags = "部门管理")
 //@RequestMapping("/dss/framework/admin/dept")
 
 @Component
@@ -38,78 +42,78 @@ public class DssFrameworkAdminDeptController {
     private DssAdminDeptService dssAdminDeptService;
 
 
-//    @ApiOperation("部门查询")
-//    @GetMapping("/list")
     @GET
     @Path("/list")
     public Message listAll(DssAdminDept dept) {
         List<DssAdminDept> list = dssAdminDeptService.selectDeptList(dept);
-        return Message.ok().data("deptList" , list).message("成功");
+        return Message.ok().data("deptList", list).message("成功");
     }
-//
-////    @ApiOperation("新增部门")
-//    @PostMapping
-//    public Message add(@RequestBody DssAdminDept dssAdminDept) {
-//
-//        if (UserConstants.NOT_UNIQUE.equals(dssAdminDeptService.checkDeptNameUnique(dssAdminDept))) {
-//            return Message.error().message("新增部门'" + dssAdminDept.getDeptName() + "'失败，部门名称已存在");
-//        }
-//
-//        int saveResult = dssAdminDeptService.insertDept(dssAdminDept);
-//        System.out.println(saveResult);
-//        if (saveResult >= 1) {
-//            return Message.ok().message("保存成功");
-//        } else {
-//            return Message.error().message("保存失败");
-//        }
-//    }
-//
-//    /**
-//     * 获取部门下拉树列表
-//     */
-////    @ApiOperation("获取部门下拉树列表")
+
+    @POST
+    public Message add(@RequestBody DssAdminDept dssAdminDept) {
+
+        if (UserConstants.NOT_UNIQUE.equals(dssAdminDeptService.checkDeptNameUnique(dssAdminDept))) {
+            return Message.error().message("新增部门'" + dssAdminDept.getDeptName() + "'失败，部门名称已存在");
+        }
+
+        int saveResult = dssAdminDeptService.insertDept(dssAdminDept);
+        System.out.println(saveResult);
+        if (saveResult >= 1) {
+            return Message.ok().message("保存成功");
+        } else {
+            return Message.error().message("保存失败");
+        }
+    }
+
+    /**
+     * 获取部门下拉树列表
+     */
 //    @GetMapping("/treeselect")
-//    public Message treeselect(DssAdminDept dept) {
-//        List<DssAdminDept> depts = dssAdminDeptService.selectDeptList(dept);
-//        return Message.ok().data("deptTree" , dssAdminDeptService.buildDeptTreeSelect(depts)).message("树形部门获取成功");
-//    }
-//
-//
-////    @ApiOperation("根据部门编号获取详细信息")
-//    @GetMapping(value = "/{deptId}")
-//    public Message getInfo(@PathVariable Long deptId) {
-//        return Message.ok().data("deptInfo" , dssAdminDeptService.selectDeptById(deptId));
-//    }
-//
-////    @ApiOperation("修改部门")
-//    @PostMapping("/edit")
-//    public Message edit(@Validated @RequestBody DssAdminDept dept) {
-//        if (UserConstants.NOT_UNIQUE.equals(dssAdminDeptService.checkDeptNameUnique(dept))) {
-//            return Message.error().message("修改部门'" + dept.getDeptName() + "'失败，部门名称已存在");
-//        } else if (dept.getParentId().equals(dept.getId())) {
-//            return Message.error().message("修改部门'" + dept.getDeptName() + "'失败，上级部门不能是自己");
-//        } else if (StringUtils.equals(UserConstants.DEPT_DISABLE, dept.getStatus())
-//                && dssAdminDeptService.selectNormalChildrenDeptById(dept.getId()) > 0) {
-//            return Message.error().message("该部门包含未停用的子部门！");
-//        }
-//
-//        return Message.ok().data("修改成功" , dssAdminDeptService.updateDept(dept));
-//    }
-//
-//
-//    /**
-//     * 删除部门
-//     */
-////    @ApiOperation("删除部门")
-//    @PostMapping("/{deptId}")
-//    public Message remove(@PathVariable Long deptId) {
-//        if (dssAdminDeptService.hasChildById(deptId)) {
-//            return Message.error().message("存在下级部门,不允许删除");
-//        }
-//        if (dssAdminDeptService.checkDeptExistUser(deptId)) {
-//            return Message.error().message("部门存在用户,不允许删除");
-//        }
-//        return Message.ok().data("删除成功" , dssAdminDeptService.deleteDeptById(deptId));
-//    }
+    @GET
+    @Path("/treeselect")
+    public Message treeselect(DssAdminDept dept) {
+        List<DssAdminDept> depts = dssAdminDeptService.selectDeptList(dept);
+        return Message.ok().data("deptTree", dssAdminDeptService.buildDeptTreeSelect(depts)).message("树形部门获取成功");
+    }
+
+
+    //    @GetMapping(value = "/{deptId}")
+    @GET
+    @Path("/{deptId}")
+    public Message getInfo(@PathVariable Long deptId) {
+        return Message.ok().data("deptInfo", dssAdminDeptService.selectDeptById(deptId));
+    }
+
+    //    @PostMapping("/edit")
+    @POST
+    @Path("/edit")
+    public Message edit(@Validated @RequestBody DssAdminDept dept) {
+        if (UserConstants.NOT_UNIQUE.equals(dssAdminDeptService.checkDeptNameUnique(dept))) {
+            return Message.error().message("修改部门'" + dept.getDeptName() + "'失败，部门名称已存在");
+        } else if (dept.getParentId().equals(dept.getId())) {
+            return Message.error().message("修改部门'" + dept.getDeptName() + "'失败，上级部门不能是自己");
+        } else if (StringUtils.equals(UserConstants.DEPT_DISABLE, dept.getStatus())
+                && dssAdminDeptService.selectNormalChildrenDeptById(dept.getId()) > 0) {
+            return Message.error().message("该部门包含未停用的子部门！");
+        }
+
+        return Message.ok().data("修改成功", dssAdminDeptService.updateDept(dept));
+    }
+
+    /**
+     * 删除部门
+     */
+    @POST
+    @Path("/{deptId}")
+    public Message remove(@PathVariable Long deptId) {
+        if (dssAdminDeptService.hasChildById(deptId)) {
+            return Message.error().message("存在下级部门,不允许删除");
+        }
+        if (dssAdminDeptService.checkDeptExistUser(deptId)) {
+            return Message.error().message("部门存在用户,不允许删除");
+        }
+        return Message.ok().data("删除成功", dssAdminDeptService.deleteDeptById(deptId));
+    }
+
 
 }

3494e620e8d8a61b6b64c0fae98b1857882138c3
汪鸣峰 <wangmingfeng@chelun.com>
2021-06-11 10:39:25 +0800

    工作空间权限修改

--- web/vue.config.js
+++ web/vue.config.js
@@ -222,8 +222,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        //target: 'http://192.168.10.201:8088',
-        target: 'http://192.168.9.119:9202', //tangHan
+        target: 'http://192.168.10.201:8088',
+        //target: 'http://192.168.9.119:9202', //tangHan
         //target: 'http://192.168.10.180:8088',
         changeOrigin: true,
         pathRewrite: {

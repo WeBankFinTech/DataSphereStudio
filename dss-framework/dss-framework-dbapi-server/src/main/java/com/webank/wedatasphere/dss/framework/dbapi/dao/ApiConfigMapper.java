5447f09ded85ce2f77695bc7902ee11d84a8694d
hantang <th3408251986@163.com>
2021-07-16 15:10:01 +0800

    数据服务初始化

--- dss-framework/dss-framework-dbapi-server/src/main/java/com/webank/wedatasphere/dss/framework/dbapi/dao/ApiConfigMapper.java
+++
@@ -1,7 +0,0 @@
-package com.webank.wedatasphere.dss.framework.dbapi.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.webank.wedatasphere.dss.framework.dbapi.entity.ApiConfig;
-
-public interface ApiConfigMapper extends BaseMapper<ApiConfig> {
-}

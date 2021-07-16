4555ceac17011dc0380f1f4e6d8b87850c0da7bf
luban08 <wananpei8988@gmail.com>
2021-07-16 09:55:31 +0800

    数据服务初始化

---
+++ web/src/apps/dataService/view/develop/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="height: 100%">
+    <dataService />
+  </div>
+</template>
+
+<script>
+import dataService from '../../module/dataService';
+export default {
+  components: {
+    dataService: dataService.component
+  }
+}
+</script>

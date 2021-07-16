afd3c21b115eb746c9b5f1e682b2f4eb7fd0012e
luban08 <wananpei8988@gmail.com>
2021-07-16 09:55:31 +0800

    数据服务初始化

---
+++ web/src/apps/dataService/module/dataMangement/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    服务管理
+  </div>
+</template>
+<script>
+// import api from "@/common/service/api";
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+
+  },
+  created() {
+    // 获取api相关数据
+    // api.fetch('/dss/apiservice/queryById', {
+    //   id: this.$route.query.id,
+    // }, 'get').then((rst) => {
+    //   if (rst.result) {
+    //     // api的基础信息
+    //     this.apiData = rst.result;
+    //     this.formValidate.approvalNo = this.apiData.approvalVo.approvalNo;
+    //     // 更改网页title
+    //     document.title = rst.result.aliasName || rst.result.name;
+    //     // 加工api信息tab的数据
+    //     this.apiInfoData = [
+    //       { label: this.$t('message.apiServices.label.apiName'), value: rst.result.name },
+    //       { label: this.$t('message.apiServices.label.path'), value: rst.result.path },
+    //       { label: this.$t('message.apiServices.label.scriptsPath'), value: rst.result.scriptPath },
+    //     ]
+    //   }
+    // }).catch((err) => {
+    //   console.error(err)
+    // });
+  },
+  methods: {
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/common/style/variables.scss';
+</style>
+

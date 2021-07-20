cf6a5c30e777b0db18292cc7ac25a685b9749dca
hantang <th3408251986@163.com>
2021-07-20 14:08:42 +0800

    数据服务初始化

--- dss-framework/dss-framework-dbapi-server/src/main/java/com/webank/wedatasphere/dss/framework/dbapi/service/ApiConfigService.java
+++ dss-framework/dss-framework-dbapi-server/src/main/java/com/webank/wedatasphere/dss/framework/dbapi/service/ApiConfigService.java
@@ -1,17 +1,26 @@
 package com.webank.wedatasphere.dss.framework.dbapi.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.google.gson.JsonObject;
 import com.webank.wedatasphere.dss.framework.dbapi.entity.ApiConfig;
 import com.webank.wedatasphere.dss.framework.dbapi.entity.ApiGroup;
+import com.webank.wedatasphere.dss.framework.dbapi.entity.response.ApiExecuteInfo;
 import com.webank.wedatasphere.dss.framework.dbapi.entity.response.ApiGroupInfo;
 import com.webank.wedatasphere.dss.framework.dbapi.entity.response.ApiListInfo;
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-public interface DSSApiConfigService extends IService<ApiConfig> {
+public interface ApiConfigService extends IService<ApiConfig> {
 
     void addGroup(ApiGroup apiGroup);
+
     List<ApiGroupInfo> getGroupList(String workspaceId);
-     void saveApi(ApiConfig apiConfig) throws JSONException;
+
+    void saveApi(ApiConfig apiConfig) throws JSONException;
+
+    ApiExecuteInfo apiTest(String path, HttpServletRequest httpRequest) throws  Exception;
 }

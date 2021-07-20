4d94b068e948ea7514642fdb573a1abaaadabc48
suyc <suyc@chinatelecom.cn>
2021-07-20 19:36:03 +0800

    add monitor

--- dss-framework/dss-framework-dbapi-server/src/main/java/com/webank/wedatasphere/dss/framework/dbapi/service/ApiAuthService.java
+++ dss-framework/dss-framework-dbapi-server/src/main/java/com/webank/wedatasphere/dss/framework/dbapi/service/ApiAuthService.java
@@ -1,5 +1,6 @@
 package com.webank.wedatasphere.dss.framework.dbapi.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.webank.wedatasphere.dss.framework.dbapi.entity.ApiAuth;
 import com.webank.wedatasphere.dss.framework.dbapi.entity.response.ApiInfo;
 import com.webank.wedatasphere.linkis.common.exception.ErrorException;
@@ -12,11 +13,19 @@ import java.util.List;
  * @Date 2021/7/13 17:10
  * @Created by suyc
  */
-public interface ApiAuthService {
+public interface ApiAuthService extends IService<ApiAuth> {
     public Long createApiAuth(ApiAuth apiAuth) throws ErrorException;
 
     public List<ApiAuth> getApiAuthList(Long workspaceId, List<Long> totals, Integer pageNow, Integer pageSize);
 
+    public void deleteApiAuth(Long id);
+
+
+
 
     public List<ApiInfo> getApiInfoList(Long workspaceId, List<Long> totals, Integer pageNow, Integer pageSize);
+
+    public void offlineApi(Long apiId);
+
+    public void onlineApi(Long apiId);
 }

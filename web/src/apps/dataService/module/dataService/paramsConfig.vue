738ccaae03be796eb0b4804e5ead001ddd73616e
司海洋 <sihy@chinatelecom.cn>
2021-07-21 16:02:22 +0800

    添加数据服务开发

--- web/src/apps/dataService/module/dataService/paramsConfig.vue
+++ web/src/apps/dataService/module/dataService/paramsConfig.vue
@@ -2,7 +2,7 @@
   <div class="paramsContainer">
     <div class="cardWrap">
       <div class="cardTitle">数据表</div>
-      <div class="formWrap">
+      <div class="contentWrap">
         <Form
           ref="dbForm"
           :model="dbForm"
@@ -28,6 +28,86 @@
             </Select>
           </FormItem>
         </Form>
+      </div>
+    </div>
+    <div class="cardWrap">
+      <div class="cardTitle">环境配置</div>
+      <div class="contentWrap">
+        <Form
+          ref="envForm"
+          :model="envForm"
+          :label-width="90"
+          :rules="envRuleValidate"
+        >
+          <FormItem label="内存" prop="memory">
+            <Select v-model="envForm.memory" style="width:300px">
+              <Option value="GET">GET</Option>
+              <Option value="POST">POST</Option>
+            </Select>
+          </FormItem>
+          <FormItem label="超时时间" prop="reqTimeout">
+            <Input
+              type="text"
+              v-model="envForm.reqTimeout"
+              placeholder="请输入业务名称"
+              style="width: 300px"
+            ></Input>
+          </FormItem>
+        </Form>
+      </div>
+    </div>
+    <div class="cardWrap">
+      <div class="cardTitle">选择参数</div>
+      <div class="contentWrap">
+        <Table
+          :columns="paramsColumns"
+          :data="paramsList"
+          :loading="tableLoading"
+        >
+          <template slot-scope="{ row }" slot="fieldSort">
+            <div class="fieldSort" @click="addToSort(row)">
+              添加到字段排序
+            </div>
+          </template>
+        </Table>
+      </div>
+    </div>
+    <div class="cardWrap">
+      <div class="cardTitle">排序字段</div>
+      <div style="margin-top: 10px;">
+        <Alert show-icon closable
+          >排序字段为非必填项；如果您需要对字段进行排序，请首先在选择参数的列表中选择所需字段</Alert
+        >
+      </div>
+      <div class="contentWrap">
+        <Table :columns="sortColumns" :data="sortList">
+          <template slot-scope="{ row }" slot="SortType">
+            <Select
+              :value="row.type"
+              transfer
+              style="width:200px"
+              @on-change="value => changeSort(value, row)"
+            >
+              <Option value="asc">升序</Option>
+              <Option value="desc">降序</Option>
+            </Select>
+          </template>
+          <template slot-scope="{ index }" slot="operation">
+            <div class="sortOperation">
+              <div class="operation" @click="moveSortRow(index, 'up')">
+                上移
+              </div>
+              <Divider type="vertical" />
+              <div class="operation" @click="moveSortRow(index, 'down')">
+                下移
+              </div>
+              <Divider type="vertical" />
+              <div class="operation" @click="moveSortRow(index, 'delete')">
+                删除
+              </div>
+            </div>
+          </template>
+        </Table>
       </div>
     </div>
   </div>
@@ -63,7 +143,149 @@ export default {
             trigger: "blur"
           }
         ]
-      }
+      },
+      envForm: {
+        memory: "",
+        reqTimeout: ""
+      },
+      envRuleValidate: {
+        memory: [
+          {
+            required: true,
+            message: "请选择数据源类型",
+            trigger: "blur"
+          }
+        ],
+        reqTimeout: [
+          {
+            required: true,
+            message: "请选择数据源名称",
+            trigger: "blur"
+          }
+        ]
+      },
+      paramsColumns: [
+        {
+          title: "设为请求参数",
+          key: "setRequest",
+          renderHeader: (h, params) => {
+            return h("div", [
+              h(
+                "span",
+                {
+                  on: {
+                    click: () => {
+                      this.setParamsChoose(params.column);
+                    }
+                  }
+                },
+                [h("Checkbox")]
+              ),
+
+              h(
+                "span",
+                {
+                  style: {
+                    marginLeft: "2px"
+                  }
+                },
+                "设为请求参数"
+              )
+            ]);
+          }
+        },
+        {
+          title: "设为返回参数",
+          key: "setResponse",
+          renderHeader: (h, params) => {
+            return h("div", [
+              h(
+                "span",
+                {
+                  on: {
+                    click: () => {
+                      this.setParamsChoose(params.column);
+                    }
+                  }
+                },
+                [h("Checkbox")]
+              ),
+
+              h(
+                "span",
+                {
+                  style: {
+                    marginLeft: "2px"
+                  }
+                },
+                "设为返回参数"
+              )
+            ]);
+          }
+        },
+        {
+          title: "字段名",
+          key: "columnName"
+        },
+        {
+          title: "字段类型",
+          key: "fieldType"
+        },
+        {
+          title: "字段描述",
+          key: "Comment"
+        },
+        {
+          title: "字段排序",
+          key: "fieldSort",
+          slot: "fieldSort"
+        }
+      ],
+      paramsList: [
+        {
+          Comment: "部门id",
+          fieldType: "bigint(20)",
+          columnName: "id"
+        }
+      ],
+      tableLoading: false,
+      sortColumns: [
+        {
+          title: "序号",
+          key: "index"
+        },
+        {
+          title: "字段名",
+          key: "id"
+        },
+        {
+          title: "排序方式",
+          key: "type",
+          slot: "SortType"
+        },
+        {
+          title: "操作",
+          key: "operation",
+          slot: "operation"
+        }
+      ],
+      sortList: [
+        {
+          index: 1,
+          id: "a",
+          type: "asc"
+        },
+        {
+          index: 2,
+          id: "b",
+          type: "asc"
+        },
+        {
+          index: 3,
+          id: "bigint(20)",
+          type: "asc"
+        }
+      ]
     };
   },
   computed: {},
@@ -93,6 +315,44 @@ export default {
     },
     handleToolShow(data) {
       console.log(data);
+    },
+    addToSort(rowData) {
+      console.log(rowData);
+    },
+    setParamsChoose(column) {
+      console.log(column);
+    },
+    changeSort(value, rowData) {
+      console.log(value);
+      console.log(rowData);
+    },
+    moveSortRow(index, action) {
+      const datas = [...this.sortList];
+      if (action === "up") {
+        if (index === 0) {
+          return;
+        }
+        const pre = datas[index - 1];
+        const current = datas[index];
+        datas[index - 1] = { ...current };
+        datas[index] = { ...pre };
+      } else if (action === "down") {
+        if (index === datas.length - 1) {
+          return;
+        }
+        const suf = datas[index + 1];
+        const current = datas[index];
+        datas[index + 1] = { ...current };
+        datas[index] = { ...suf };
+      } else if (action === "delete") {
+        datas.splice(index, 1);
+      }
+      this.sortList = datas.map((item, index) => {
+        return {
+          ...item,
+          index: index + 1
+        };
+      });
     }
   }
 };
@@ -110,8 +370,26 @@ export default {
       font-size: 16px;
       color: rgba(0, 0, 0, 0.85);
     }
-    .formWrap {
+    .contentWrap {
       margin-top: 20px;
+      .fieldSort {
+        font-size: 14px;
+        color: #2e92f7;
+        cursor: pointer;
+      }
+      .sortOperation {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        height: 38px;
+        font-size: 14px;
+        font-family: PingFangSC-Medium;
+        .operation {
+          font-size: 14px;
+          color: #2e92f7;
+          cursor: pointer;
+        }
+      }
     }
   }
 }

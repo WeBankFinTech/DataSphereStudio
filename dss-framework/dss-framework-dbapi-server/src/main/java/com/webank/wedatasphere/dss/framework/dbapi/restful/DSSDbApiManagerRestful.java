3dc0fc4d4f691c4c5991dc3b470582648fbeac7e
suyc <suyc@chinatelecom.cn>
2021-07-22 21:02:15 +0800

    add call monitor

--- dss-framework/dss-framework-dbapi-server/src/main/java/com/webank/wedatasphere/dss/framework/dbapi/restful/DSSDbApiManagerRestful.java
+++ dss-framework/dss-framework-dbapi-server/src/main/java/com/webank/wedatasphere/dss/framework/dbapi/restful/DSSDbApiManagerRestful.java
@@ -1,7 +1,7 @@
 package com.webank.wedatasphere.dss.framework.dbapi.restful;
 
 import com.webank.wedatasphere.dss.framework.dbapi.entity.response.ApiInfo;
-import com.webank.wedatasphere.dss.framework.dbapi.service.ApiAuthService;
+import com.webank.wedatasphere.dss.framework.dbapi.service.ApiManagerService;
 import com.webank.wedatasphere.linkis.server.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * @author lenovo
  * @Classname DSSDbApiManagerRestful
  * @Description 服务管理--API管理
  * @Date 2021/7/19 15:38
@@ -32,13 +33,13 @@ import java.util.List;
 @Consumes(MediaType.APPLICATION_JSON)
 public class DSSDbApiManagerRestful {
     @Autowired
-    private ApiAuthService apiAuthService;
+    private ApiManagerService apiManagerService;
 
     @GET
     @Path("/list")
-    //支持按name搜索
-    public Response getApiList(@Context HttpServletRequest request, @QueryParam("workspaceId") Long workspaceId,
-                                       @QueryParam("pageNow") Integer pageNow, @QueryParam("pageSize") Integer pageSize){
+    public Response getApiList(@Context HttpServletRequest request,
+                               @QueryParam("workspaceId") Long workspaceId, @QueryParam("apiName") String apiName,
+                               @QueryParam("pageNow") Integer pageNow, @QueryParam("pageSize") Integer pageSize){
         if(pageNow == null){
             pageNow = 1;
         }
@@ -47,14 +48,14 @@ public class DSSDbApiManagerRestful {
         }
 
         List<Long> totals = new ArrayList<>();
-        List<ApiInfo> apiInfoList = apiAuthService.getApiInfoList(workspaceId,totals,pageNow,pageSize);
-        return Message.messageToResponse(Message.ok().data("apiInfoList",apiInfoList).data("total", totals.get(0)));
+        List<ApiInfo> apiInfoList = apiManagerService.getApiInfoList(workspaceId,apiName,totals,pageNow,pageSize);
+        return Message.messageToResponse(Message.ok().data("list",apiInfoList).data("total", totals.get(0)));
     }
 
     @POST
     @Path("offline")
     public Response offlineApi(@Context HttpServletRequest request, @QueryParam("apiId") Long apiId){
-        apiAuthService.offlineApi(apiId);
+        apiManagerService.offlineApi(apiId);
 
         Message message = Message.ok("下线API成功").data("apiId", apiId);
         return Message.messageToResponse(message);
@@ -63,13 +64,21 @@ public class DSSDbApiManagerRestful {
     @POST
     @Path("online")
     public Response onlineApi(@Context HttpServletRequest request, @QueryParam("apiId") Long apiId){
-        apiAuthService.onlineApi(apiId);
+        apiManagerService.onlineApi(apiId);
 
         Message message = Message.ok("上线API成功").data("apiId", apiId);
         return Message.messageToResponse(message);
     }
 
-    //复制调用地址
+    @GET
+    @Path("callPath")
+    public Response getApiCallPath(@QueryParam("apiId") Long apiId){
+        StringBuilder callPath =new StringBuilder("http://xxxx");
+        callPath.append("/api/rest_j/v1");
+
+        Message message = Message.ok().data("callPathPrefix", callPath.toString());
+        return Message.messageToResponse(message);
+    }
 
 
 }

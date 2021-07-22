56988fd5c4ff554e03312ccef7b17ec914371e99
万安沛 <wanap@chinatelecom.cn>
2021-07-22 01:09:35 +0000

    产品即文档

--- web/src/dss/module/footer/index.vue
+++ web/src/dss/module/footer/index.vue
@@ -1,39 +1,34 @@
 <template>
-  <div
-    ref="footerChannel"
-    class="layout-footer"
-    @mousedown.prevent.stop="onMouseDown"
-    @mouseup.prevent.stop="oMouseUp"
-    @click.prevent.stop="toast">
+  <div class="layout-footer">
+    <div class="footer-btn footer-doc" @click="toggleGuide">
+      <Icon custom="iconfont icon-wenhao"/>
+    </div>
+    <Guide :show="guideShow" @on-toggle="toggleGuide" />
+
     <resource-simple
       ref="resourceSimple"
       @update-job="updateJob">
     </resource-simple>
-    <div
-      :title="msg"
-      class="footer-channel">
-      <Icon
-        class="footer-channel-job"
-        type="ios-swap"/>
+    <div class="footer-btn footer-channel" :title="msg" @click.prevent.stop="toast">
+      <Icon class="footer-channel-job" custom="iconfont icon-tiaodu"/>
       <span class="footer-channel-job-num">{{ num }}</span>
     </div>
   </div>
 </template>
 <script>
 import resourceSimpleModule from '@/dss/module/resourceSimple';
+import Guide from './guide.vue'
 import api from '@/common/service/api';
 export default {
   components: {
     resourceSimple: resourceSimpleModule.component,
+    Guide,
   },
   data() {
     return {
       num: 0,
       msg: '',
-      moveX: null,
-      moveY: null,
-      isMouseDrop: false,
-      isMouseMove: false,
+      guideShow: false,
     };
   },
   created() {
@@ -44,7 +39,7 @@ export default {
   },
   watch: {
     '$route'() {
-      this.resetChannelPosition();
+      // this.resetChannelPosition();
     }
   },
   methods: {
@@ -75,63 +70,8 @@ export default {
       }
       this.$refs.resourceSimple.open();
     },
-    onMouseDown(e) {
-      e = e || window.event;
-      const footerChannel = this.$refs.footerChannel;
-      this.moveX = e.clientX - footerChannel.offsetLeft;
-      this.moveY = e.clientY - footerChannel.offsetTop;
-      this.isMouseDrop = true;
-      this.isMouseMove = false;
-      // 阻止拖拽过程中选中文本
-      document.onselectstart = () => {
-        return false;
-      }
-      // 这里无法在元素上使用@mousemove，否则拖动会有卡顿
-      // 使用setTimeout是防止点击的同时会触发move事件，这时正常的点击事件是不会触发的。
-      setTimeout(() => {
-        document.onmousemove = (e) => {
-
-          if (this.isMouseDrop) {
-            this.isMouseMove = true;
-            const footerChannel = this.$refs.footerChannel;
-            let x = e.clientX - this.moveX;
-            let y = e.clientY - this.moveY;
-            // 限制拖动范围
-            let maxX = document.documentElement.clientWidth - 120;
-            let maxY = document.documentElement.clientHeight - 60;
-            if (this.moveX <= 0) {
-              maxX = document.documentElement.scrollWidth - 120;
-            }
-            if (this.moveY <= 0) {
-              maxY = document.documentElement.scrollHeight - 60;
-            }
-            x = Math.min(maxX, Math.max(0, x));
-            y = Math.min(maxY, Math.max(0, y));
-            if(e.clientX > maxX+120 || e.clientY > maxY+60 || e.clientX < 0 || e.clientY < 0){
-              this.oMouseUp()
-            }
-            footerChannel.style.left = x + 'px';
-            footerChannel.style.top = y + 'px';
-          }
-        }
-      }, 0)
-    },
-    oMouseUp() {
-      // 清空onmousemove方法
-      document.onmousemove = null;
-      this.isMouseDrop = false;
-      setTimeout(() => {
-        this.isMouseMove = false;
-      }, 200)
-      // 恢复document的文本选中功能
-      document.onselectstart = () => {
-        return true;
-      }
-    },
-    resetChannelPosition() {
-      const footerChannel = this.$refs.footerChannel;
-      footerChannel.style.left = document.documentElement.clientWidth - 120 + 'px';
-      footerChannel.style.top = document.documentElement.clientHeight - 60 + 'px';
+    toggleGuide() {
+      this.guideShow = !this.guideShow;
     }
   },
 };

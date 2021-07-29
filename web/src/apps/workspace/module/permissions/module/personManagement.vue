3839d7716c6e43a6bf46d2fe4ce5643141be51f7
zhangchx11 <zhangchx11@chinatelecom.cn>
2021-07-29 16:22:30 +0800

    完成sidebar开发

--- web/src/apps/workspace/module/permissions/module/personManagement.vue
+++ web/src/apps/workspace/module/permissions/module/personManagement.vue
@@ -422,15 +422,6 @@ export default {
             trigger: "blur"
           }
         ]
-      },
-      passwordRuleValidate: {
-        password: [
-          {
-            required: true,
-            message: this.$t("message.permissions.passwordEmpty"),
-            trigger: "blur"
-          }
-        ]
       }
     };
   },
@@ -713,7 +704,7 @@ export default {
 <style lang="scss" scoped>
 .container {
   width: 100%;
-  height: 100%;
+  // height: 100%;
   display: flex;
   flex-direction: column;
 }

cf26ad10054c0cfcbb5a2613ab5e60d1635c3c6f
司海洋 <sihy@chinatelecom.cn>
2021-07-29 18:09:08 +0800

    添加数据服务开发

--- web/src/apps/dataService/module/common/navMenu.vue
+++ web/src/apps/dataService/module/common/navMenu.vue
@@ -16,34 +16,8 @@
         <Icon custom="iconfont icon-project" size="26"></Icon>
       </div>
     </div>
-    <div class="ds-nav-panel" v-if="currentTab == '/dataService1'">
-      <div class="panel-title-wrap">
-        <p>服务开发</p>
-        <div @click="addGroup()">
-          <Icon custom="iconfont icon-plus" size="20"></Icon>
-        </div>
-      </div>
-      <Input
-        size="small"
-        :value="searchValue"
-        prefix="ios-search"
-        placeholder="请输入"
-        style="width: 230px;border:0;margin-top: 10px;"
-        @on-change="handleSearch"
-      />
-      <Tree
-        class="tree-container"
-        :nodes="projectsTree"
-        :load="getFlow"
-        :currentTreeId="currentTreeId"
-        @on-item-click="handleTreeClick"
-        @on-add-click="handleTreeModal"
-        @on-sync-tree="handleTreeSync"
-      />
-      <Spin v-show="loadingTree" size="large" fix />
-    </div>
     <div class="ds-nav-panel" v-if="currentTab == '/dataService'">
-      <TreeMenu />
+      <TreeMenu @showModal="showModal" ref="treeMenu" />
     </div>
     <div class="ds-nav-panel" v-if="currentTab.startsWith('/dataManagement')">
       <ManageMenu />
@@ -53,14 +27,10 @@
 <script>
 import ManageMenu from "./manageMenu.vue";
 import TreeMenu from "./treeMenu.vue";
-import Tree from "@/apps/workflows/module/common/tree/tree.vue";
-import api from "@/common/service/api";
-import _ from "lodash";
 
 export default {
   name: "navMenu",
   components: {
-    Tree,
     ManageMenu,
     TreeMenu
   },
@@ -80,13 +50,7 @@ export default {
       originDatas: []
     };
   },
-  mounted() {
-    if (this.currentTab == "/dataService") {
-      this.getAllApi();
-    } else {
-      this.getAllProjects();
-    }
-  },
+  mounted() {},
   methods: {
     handleTabClick(tab) {
       if (
@@ -101,227 +65,18 @@ export default {
         });
       }
     },
-    // 获取所有project展示tree
-    getAllProjects() {
-      const projects = [
-        {
-          id: 418,
-          applicationArea: 0,
-          business: "",
-          createBy: "hadoop",
-          description: "123",
-          name: "test123",
-          source: null,
-          product: "",
-          editable: true,
-          createTime: 1624933330000,
-          updateTime: 1624933330000,
-          releaseUsers: ["hadoop"],
-          editUsers: ["", "hadoop"],
-          accessUsers: [""],
-          devProcessList: ["dev"],
-          orchestratorModeList: ["pom_work_flow"],
-          archive: false
-        },
-        {
-          id: 406,
-          applicationArea: 0,
-          business: "",
-          createBy: "hadoop",
-          description: "test",
-          name: "yu622",
-          source: null,
-          product: "",
-          editable: true,
-          createTime: 1624343400000,
-          updateTime: 1624867955000,
-          releaseUsers: ["det101"],
-          editUsers: ["hadoop"],
-          accessUsers: [""],
-          devProcessList: ["dev", "scheduler"],
-          orchestratorModeList: ["pom_work_flow"],
-          archive: false
-        },
-        {
-          id: 361,
-          applicationArea: 0,
-          business: "",
-          createBy: "hadoop",
-          description: "1",
-          name: "tet1",
-          source: null,
-          product: "",
-          editable: true,
-          createTime: 1622686085000,
-          updateTime: 1624329358000,
-          releaseUsers: ["qweasd", "det101"],
-          editUsers: ["hadoop"],
-          accessUsers: [""],
-          devProcessList: ["dev"],
-          orchestratorModeList: ["pom_work_flow"],
-          archive: false
-        }
-      ];
-      this.projectsTree = projects.map(n => {
-        return {
-          id: n.id,
-          name: n.name,
-          type: "project",
-          canWrite: () => true
-        };
-      });
-      // this.loadingTree = true;
-      // api.fetch(`${this.$API_PATH.PROJECT_PATH}getAllProjects`, {
-      //   workspaceId: +this.$route.query.workspaceId
-      // }, 'post').then((res) => {
-      //   this.loadingTree = false;
-      //   this.projectsTree = res.projects.map(n => {
-      //     return {
-      //       id: n.id,
-      //       name: n.name,
-      //       type: 'project',
-      //       canWrite: n.canWrite()
-      //     }
-      //   });
-      // })
-    },
-    // 获取project下工作流
-    getFlow(param, resolve) {
-      if (this.currentTab == "/dataService") {
-        // resolve([
-        //   {
-        //     id: "shfhsd",
-        //     name: "sfafd",
-        //     projectId: param.id,
-        //     projectName: param.name,
-        //     type: "flow"
-        //   }
-        // ]);
-        this.projectsTree = this.projectsTree.map(item => {
-          if (item.id == param.id) {
-            return {
-              ...item,
-              loaded: true,
-              loading: false,
-              opened: true,
-              isLeaf: false
-            };
-          } else {
-            return item;
-          }
-        });
-      } else {
-        api
-          .fetch(
-            `${this.$API_PATH.PROJECT_PATH}getAllOrchestrator`,
-            {
-              workspaceId: this.$route.query.workspaceId,
-              // orchestratorMode: "pom_work_flow",
-              projectId: param.id
-            },
-            "post"
-          )
-          .then(res => {
-            const flow = res.page.map(f => {
-              return {
-                ...f,
-                id: f.orchestratorId, // flow的id是orchestratorId
-                name: f.orchestratorName,
-                projectId: param.id || f.projectId,
-                // 补充projectName，点击工作流切换project时使用
-                projectName: param.name,
-                type: "flow"
-              };
-            });
-            resolve(flow);
-          });
-      }
-    },
-    handleTreeModal(project) {
-      console.log(project);
+
+    showModal(pyload) {
       console.log("addApi");
-      this.$emit("showModal", { type: "api", data: { ...project } });
-      this.treeModalShow = true;
-      this.currentTreeProject = project;
+      this.$emit("showModal", pyload);
     },
-    handleTreeModalCancel() {
-      this.treeModalShow = false;
-    },
-    handleTreeModalConfirm(param) {
-      // tree弹窗添加成功后要更新tree，还要通知右侧workFlow刷新
-    },
-    handleTreeSync(data) {
-      // tree中的状态同步到父级，保持状态，handleTreeModalConfirm用到
-      this.projectsTree = data;
-    },
-    handleTreeClick(node) {},
-    getAllApi() {
-      //获取数据服务所有的api
-      this.searchValue = "";
-      api
-        .fetch(
-          `/dss/framework/dbapi/list?workspaceId=${this.$route.query.workspaceId}`,
-          {},
-          "get"
-        )
-        .then(res => {
-          console.log(res);
-          if (res && res.list) {
-            this.projectsTree = res.list.map(n => {
-              return {
-                id: n.groupId,
-                name: n.groupName,
-                type: "project",
-                canWrite: () => true,
-                children: n.apis,
-                apis: n.apis
-              };
-            });
-            this.originDatas = _.cloneDeep(this.projectsTree);
-          } else {
-            this.projectsTree = [];
-          }
-        });
-    },
-    addGroup() {
-      //添加数据服务api分组
-      console.log("addGroup");
-      this.$emit("showModal", { type: "group" });
-    },
-    addApi(groupId, apiData) {
-      //添加数据服务api
-      this.searchValue = "";
-      this.projectsTree = this.originDatas.map(item => {
-        if (item.id == groupId) {
-          return {
-            ...item,
-            loaded: true,
-            loading: false,
-            opened: true,
-            isLeaf: false,
-            children: [...item.children, apiData]
-          };
-        } else {
-          return item;
-        }
-      });
-      this.originDatas = _.cloneDeep(this.projectsTree);
-    },
-    handleSearch: _.debounce(function(e) {
-      const value = e.target.value;
-      console.log(value);
-      console.log(2333);
-      this.executeSearch(value);
-    }, 500),
-    executeSearch(value) {
-      this.searchValue = value;
-      if (value) {
-        const temp = _.cloneDeep(this.originDatas);
-        this.projectsTree = temp.filter(item => {
-          return !!item.children.find(child => child.name.include(value));
-        });
+
+    treeMethod(name, payload = {}) {
+      if (name === "getApi") {
+        this.$refs.treeMenu.getAllApi();
       } else {
-        this.projectsTree = _.cloneDeep(this.originDatas);
+        const { id, data } = payload;
+        this.$refs.treeMenu.addApi(id, data);
       }
     }
   }
@@ -377,18 +132,5 @@ export default {
 }
 .develop_nav {
   width: 24px;
-}
-.panel-title-wrap {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  & p {
-    font-family: PingFangSC-Medium;
-    font-size: 14px;
-    color: rgba(0, 0, 0, 0.65);
-  }
-  & div {
-    cursor: pointer;
-  }
 }
 </style>

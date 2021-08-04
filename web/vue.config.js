7902d2ff09eca39302f21a97f51bcf29ea41eb57
zhangchx11 <zhangchx11@chinatelecom.cn>
2021-08-04 15:08:31 +0800

    缓存前端代码8.4

--- web/vue.config.js
+++ web/vue.config.js
@@ -222,7 +222,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://192.168.10.201:8088',
+        target: 'http://***REMOVED***:8088',
+        //target: 'http://192.168.10.201:8088',
         //target: 'http://192.168.9.119:9202', //tangHan
         //target: 'http://192.168.10.180:8088',
         //target: 'http://0.0.0.0:8087',

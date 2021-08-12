6e1fea1d282dff43c6d755effc8e02adb5cb28e8
suyc <suyc@chinatelecom.cn>
2021-08-12 17:58:18 +0800

    add timetag for api monitor

--- dss-framework/dss-framework-dbapi-server/src/main/java/com/webank/wedatasphere/dss/framework/dbapi/service/impl/ApiMonitorServiceImpl.java
+++ dss-framework/dss-framework-dbapi-server/src/main/java/com/webank/wedatasphere/dss/framework/dbapi/service/impl/ApiMonitorServiceImpl.java
@@ -6,10 +6,14 @@ import com.webank.wedatasphere.dss.framework.dbapi.entity.request.CallMonitorRes
 import com.webank.wedatasphere.dss.framework.dbapi.entity.request.SingleCallMonitorRequest;
 import com.webank.wedatasphere.dss.framework.dbapi.entity.response.ApiCallInfoByCnt;
 import com.webank.wedatasphere.dss.framework.dbapi.entity.response.ApiCallInfoByFailRate;
+import com.webank.wedatasphere.dss.framework.dbapi.entity.response.HourMonitorInfo;
 import com.webank.wedatasphere.dss.framework.dbapi.service.ApiMonitorService;
+import com.webank.wedatasphere.dss.framework.dbapi.util.TimeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -58,17 +62,75 @@ public class ApiMonitorServiceImpl implements ApiMonitorService {
     }
 
     @Override
-    public List<Map<String, Object>> getCallCntForPast24H(Long workspaceId) {
-        return apiCallMapper.getCallCntForPast24H(workspaceId);
+    public List<HourMonitorInfo> getCallCntForPast24H(Long workspaceId) throws Exception {
+        //整点小时的时间点集合
+        ArrayList<String> timeTagList = TimeUtil.getTimeLagForPast24H();
+        //小时整点-次数（很可能数据不全，会有数据缺失）
+        List<Map<String, Object>> oldLMap = apiCallMapper.getCallCntForPast24H(workspaceId);
+        Map<String, Object> oldMap =transforLMap2Map(oldLMap);
+
+        List<HourMonitorInfo> list = new ArrayList<>();
+        for (String timeTag: timeTagList) {
+            list.add(new HourMonitorInfo(timeTag,oldMap.getOrDefault(timeTag,0)));
+        }
+
+        return list;
     }
 
     @Override
-    public List<Map<String, Object>> getCallTimeForSinleApi(SingleCallMonitorRequest singleCallMonitorRequest) {
-        return apiCallMapper.getCallTimeForSinleApi(singleCallMonitorRequest);
+    public List<HourMonitorInfo> getCallTimeForSinleApi(SingleCallMonitorRequest singleCallMonitorRequest) throws Exception {
+        //整点小时的时间点集合
+        ArrayList<String> timeTagList = TimeUtil.getTimeLag(singleCallMonitorRequest.getStartTime(),singleCallMonitorRequest.getEndTime());
+        //小时整点-次数（很可能数据不全，会有数据缺失）
+        List<Map<String, Object>> oldLMap = apiCallMapper.getCallTimeForSinleApi(singleCallMonitorRequest);
+        Map<String, Object> oldMap =transforLMap2Map(oldLMap);
+
+        List<HourMonitorInfo> list = new ArrayList<>();
+        for (String timeTag: timeTagList) {
+            list.add(new HourMonitorInfo(timeTag,oldMap.getOrDefault(timeTag,0)));
+        }
+
+        return list;
     }
 
     @Override
-    public List<Map<String, Object>> getCallCntForSinleApi(SingleCallMonitorRequest singleCallMonitorRequest) {
-        return apiCallMapper.getCallCntForSinleApi(singleCallMonitorRequest);
+    public List<HourMonitorInfo> getCallCntForSinleApi(SingleCallMonitorRequest singleCallMonitorRequest) throws Exception {
+        //整点小时的时间点集合
+        ArrayList<String> timeTagList = TimeUtil.getTimeLag(singleCallMonitorRequest.getStartTime(),singleCallMonitorRequest.getEndTime());
+        //小时整点-次数（很可能数据不全，会有数据缺失）
+        List<Map<String, Object>> oldLMap = apiCallMapper.getCallCntForSinleApi(singleCallMonitorRequest);
+        Map<String, Object> oldMap =transforLMap2Map(oldLMap);
+
+        List<HourMonitorInfo> list = new ArrayList<>();
+        for (String timeTag: timeTagList) {
+            list.add(new HourMonitorInfo(timeTag,oldMap.getOrDefault(timeTag,0)));
+        }
+
+        return list;
+    }
+
+
+    /**
+     * 转换数据格式
+     */
+    private Map<String, Object> transforLMap2Map(List<Map<String, Object>> lMap){
+        Map<String, Object> newMap = new HashMap<>();
+
+        for (Map<String, Object> map: lMap) {
+            String key =null;
+            Object value =null;
+            for (Map.Entry<String, Object> maps: map.entrySet()) {
+                //System.out.println("key"+maps.getKey());
+                //System.out.println("key"+maps.getValue());
+                if("k".equals(maps.getKey())){
+                    key = String.valueOf(maps.getValue());
+                }
+                if("v".equals(maps.getKey())){
+                    value = String.valueOf(maps.getValue());
+                }
+            }
+            newMap.put(key,value);
+        }
+        return newMap ;
     }
 }

ac54100cbebf7fb0d6d3aa6bfb96ca1d634b9b6e
zhangxiang <zhangx_3@chinatelecom.cn>
2021-08-17 14:16:58 +0800

    添加新人引导功能

--- web/src/dss/module/header/index.vue
+++ web/src/dss/module/header/index.vue
@@ -324,7 +324,9 @@ export default {
       return arr;
     },
     handleOutsideClick() {
-      this.isUserMenuShow = false;
+      if(this.$parent.$children[3].currentStep !== 6){
+        this.isUserMenuShow = false;
+      }
     },
     handleUserClick() {
       this.isUserMenuShow = !this.isUserMenuShow;

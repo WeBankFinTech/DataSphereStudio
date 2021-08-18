d7c28e49be69abb2b13c66d4dec20ce778c1f27b
liuyou2 <405240259@qq.com>
2021-08-18 17:07:34 +0800

    Define some common classes for the orchestration module #352

---
+++ dss-orchestrator/dss-orchestrator-common/src/main/java/com/webank/wedatasphere/dss/orchestrator/common/entity/OrchestratorUser.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2019 WeBank
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.webank.wedatasphere.dss.orchestrator.common.entity;
+
+import java.util.Date;
+
+
+public class OrchestratorUser {
+
+    private Long id;
+
+    private Long workspaceId;
+
+    private Long projectId;
+
+    private Long orchestratorId;
+
+    private String username;
+
+    //权限等级：0:私密；1：公开
+    private Integer priv;
+
+    private Date lastUpdateTime;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getWorkspaceId() {
+        return workspaceId;
+    }
+
+    public void setWorkspaceId(Long workspaceId) {
+        this.workspaceId = workspaceId;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public Long getOrchestratorId() {
+        return orchestratorId;
+    }
+
+    public void setOrchestratorId(Long orchestratorId) {
+        this.orchestratorId = orchestratorId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getPriv() {
+        return priv;
+    }
+
+    public void setPriv(Integer priv) {
+        this.priv = priv;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+}

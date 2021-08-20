5f663f93772b9621e316914865a2cb471c713355
jinyangrao <31345964+JinyangRao@users.noreply.github.com>
2021-08-20 16:53:18 +0800

    Provide some basic function modules of appconn in dss1.0.0, and realize the implementation of third-party applications connected to DSS (#377)
    * Define AppConn core interface and default implementation class. fix #357
    
    * Implement DSS1.0 AppConn class loading and instantiation module. fix #358
    
    * Implement DSS1.0 AppConn class loading and instantiation module. fix #358
    
    * delete useless message. fix #358
    
    * Define the abstract dispatch AppConn. fix #359
    
    * Implement Linkis AppConn plug-in. fix #360
    
    * Delete useless pom location. fix #360
    
    * Realize the application of the three major access specifications of DSS1.0. fix #351
    
    * Delete qualitis appconn module. fix #351
    
    * Delete some useless code. fix #351
    
    Co-authored-by: JinyangRao <jirao4653@qq.com>

---
+++ dss-appconn/dss-appconn-manager/dss-appconn-manager-core/src/main/java/com/webank/wedatasphere/dss/appconn/manager/impl/AppConnManagerImpl.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 WeBank
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.webank.wedatasphere.dss.appconn.manager.impl;
+
+import com.webank.wedatasphere.dss.appconn.manager.service.AppConnInfoService;
+import com.webank.wedatasphere.dss.appconn.manager.service.AppConnResourceService;
+import com.webank.wedatasphere.dss.common.exception.DSSErrorException;
+import com.webank.wedatasphere.dss.common.exception.DSSRuntimeException;
+import com.webank.wedatasphere.dss.common.utils.ClassUtils;
+
+
+public class AppConnManagerImpl extends AbstractAppConnManager {
+
+    @Override
+    protected AppConnInfoService createAppConnInfoService() {
+        try {
+            return ClassUtils.getInstance(AppConnInfoService.class);
+        } catch (DSSErrorException e) {
+            throw new DSSRuntimeException(25000, "Cannot find a useful AppConnInfoService.", e);
+        }
+    }
+
+    @Override
+    protected AppConnResourceService createAppConnResourceService() {
+        try {
+            return ClassUtils.getInstance(AppConnResourceService.class);
+        } catch (DSSErrorException e) {
+            throw new DSSRuntimeException(25000, "Cannot find a useful AppConnResourceService.", e);
+        }
+    }
+
+}

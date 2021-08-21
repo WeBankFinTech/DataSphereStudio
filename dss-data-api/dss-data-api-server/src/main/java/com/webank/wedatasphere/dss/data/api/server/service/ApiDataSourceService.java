45e1377a6bfb7454514752d575d9b4aebd51a45b
hantang <th3408251986@163.com>
2021-08-21 19:58:43 +0800

    数据服务优化工程初始化

---
+++ dss-data-api/dss-data-api-server/src/main/java/com/webank/wedatasphere/dss/data/api/server/service/ApiDataSourceService.java
@@ -0,0 +1,18 @@
+package com.webank.wedatasphere.dss.data.api.server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.webank.wedatasphere.dss.data.api.server.entity.DataSource;
+
+import java.util.List;
+
+public interface ApiDataSourceService extends IService<DataSource> {
+
+    DataSource selectById(Integer datasourceId);
+
+    List<DataSource> getAllConnections(Integer workspaceId, String type);
+
+
+    List<String> getAvailableConnNames(List<DataSource> allConnections);
+
+    List<DataSource> getAvailableConns(List<DataSource> allConnections);
+}

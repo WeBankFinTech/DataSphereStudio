b49c6311df8071a9a6347e5bd106c6bc82e47a30
汪鸣峰 <wangmingfeng@chelun.com>
2021-08-23 10:56:11 +0800

    init data governance

---
+++ web/src/apps/dataGovernance/module/dataGovernance/assetsIndex.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+<script>
+//import api from "@/common/service/api";
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+<style lang="scss" scoped>
+@import "@/common/style/variables.scss";
+</style>
+

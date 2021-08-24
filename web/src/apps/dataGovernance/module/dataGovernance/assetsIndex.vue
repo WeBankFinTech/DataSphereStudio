061ff1a6888d803af2a6c05055789ed4acc03276
zhangchx11 <zhangchx11@chinatelecom.cn>
2021-08-24 19:38:07 +0800

    完成資產目錄前端頁面開發

--- web/src/apps/dataGovernance/module/dataGovernance/assetsIndex.vue
+++ web/src/apps/dataGovernance/module/dataGovernance/assetsIndex.vue
@@ -1,23 +1,319 @@
 <template>
-  <div></div>
+  <div class="assets-index-wrap">
+    <!-- top 面包屑 + 搜索框 -->
+    <div class="assets-index-t">
+      <!-- top -->
+      <div class="assets-index-t-t1">
+        <slot name="top">
+          <div
+            class="top-l-text"
+            :class="{ active: textColor }"
+            :style="{ color: textColor }"
+            @click="selectProject"
+          >
+            <span>{{ $t("message.dataGovernance.menu") }}</span>
+          </div>
+          <div class="top-r-container">
+            <template v-for="(work, index) in topTapList">
+              <div
+                :key="work.tabId"
+                :class="{
+                  active: currentTab.tabId === work.tabId && !textColor
+                }"
+                class="tab-item"
+                ref="work_item"
+              >
+                <we-tab
+                  :index="index"
+                  :work="work"
+                  @on-choose="onChooseWork"
+                  @on-remove="onRemoveWork"
+                />
+              </div>
+            </template>
+          </div>
+        </slot>
+      </div>
+
+      <!-- bottom -->
+      <div class="assets-index-t-b1">
+        <div class="assets-index-t-b1-search">
+          <Input
+            search
+            :enter-button="$t(`message.dataGovernance.search`)"
+            :placeholder="$t(`message.dataGovernance.pleaseEnterATableName`)"
+            size="large"
+            on-search="onSearch"
+          />
+        </div>
+      </div>
+    </div>
+
+    <!-- bottom 复选框搜索框 + card tabs -->
+    <div class="assets-index-b">
+      <!-- left -->
+      <div class="assets-index-b-l">
+        <div class="assets-index-b-l-title">
+          <span>筛选条件</span>
+        </div>
+        <div class="assets-index-b-l-user">
+          <span>负责人</span>
+          <Select v-model="serachOption.user" style="width:167px">
+            <Option
+              v-for="item in userList"
+              :value="item.value"
+              :key="item.value"
+              >{{ item.label }}</Option
+            >
+          </Select>
+        </div>
+        <div class="assets-index-b-l-env">
+          <span>环境</span>
+          <Radio v-model="serachOption.env"
+            ><label
+              style="color: rgba(0,0,0,0.65; fontSize: 14px; fontWeight: normal;"
+              >生产环境</label
+            ></Radio
+          >
+        </div>
+        <div class="assets-index-b-l-label">
+          <span>标签</span>
+          <Select v-model="serachOption.label" multiple style="width:167px">
+            <Option
+              v-for="item in userList"
+              :value="item.value"
+              :key="item.value"
+              >{{ item.label }}</Option
+            >
+          </Select>
+        </div>
+      </div>
+
+      <!-- right -->
+      <div class="assets-index-b-r">
+        <template v-for="model in cardTabs">
+          <tab-card
+            :model="model"
+            :key="model.id"
+            @on-choose="onChooseCard"
+          ></tab-card>
+        </template>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 //import api from "@/common/service/api";
+import weTab from "../../../workflows/module/common/tabList/tabs.vue";
+import tabCard from "../common/tabCard/index.vue";
 export default {
+  components: {
+    weTab,
+    tabCard
+  },
   data() {
     return {
+      textColor: true,
+      index: 0,
+      work: {},
+      topTapList: [],
 
-    }
+      serachOption: {},
+      userList: [
+        {
+          value: "New York",
+          label: "New York"
+        },
+        {
+          value: "London",
+          label: "London"
+        },
+        {
+          value: "Sydney",
+          label: "Sydney"
+        }
+      ],
+      cardTabs: [
+        {
+          id: 1,
+          tableName: "table1",
+          readCount: 36,
+          userName: "xxx",
+          ctime: "xxx",
+          utime: "xxx"
+        },
+        {
+          id: 2,
+          tableName: "table1",
+          readCount: 36,
+          userName: "xxx",
+          ctime: "xxx",
+          utime: "xxx"
+        },
+        {
+          id: 3,
+          tableName: "table1",
+          readCount: 36,
+          userName: "xxx",
+          ctime: "xxx",
+          utime: "xxx"
+        },
+        {
+          id: 4,
+          tableName: "table1",
+          readCount: 36,
+          userName: "xxx",
+          ctime: "xxx",
+          utime: "xxx"
+        },
+        {
+          id: 5,
+          tableName: "table1",
+          readCount: 36,
+          userName: "xxx",
+          ctime: "xxx",
+          utime: "xxx"
+        },
+        {
+          id: 6,
+          tableName: "table1",
+          readCount: 36,
+          userName: "xxx",
+          ctime: "xxx",
+          utime: "xxx"
+        },
+        {
+          id: 7,
+          tableName: "table1",
+          readCount: 36,
+          userName: "xxx",
+          ctime: "xxx",
+          utime: "xxx"
+        }
+      ]
+    };
   },
-  created() {
-
-  },
+  created() {},
   methods: {
+    // 面包屑相关
+    selectProject() {},
+    onChooseWork() {},
+    onRemoveWork() {},
 
-  },
-}
+    // 搜索
+    onSearch() {},
+
+    onChooseCard(model) {
+      console.log("on-choose-card", model);
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
 @import "@/common/style/variables.scss";
-</style>
 
+.assets-index-wrap {
+  .assets-index-t-t1 {
+    padding: 0px $padding-25;
+    border-bottom: $border-width-base $border-style-base $border-color-base;
+    @include border-color(
+      $background-color-base,
+      $dark-workspace-body-bg-color
+    );
+    @include font-color($workspace-title-color, $dark-workspace-title-color);
+    flex: none;
+    display: flex;
+    align-items: center;
+    font-size: $font-size-large;
+    .top-l-text {
+      cursor: pointer;
+      flex: none;
+      font-size: $font-size-large;
+      padding: 0 15px;
+      margin-bottom: -1px;
+      line-height: 40px;
+      position: relative;
+      &::after {
+        content: "";
+        border-left: 1px solid #dee4ec;
+        @include border-color($border-color-base, $dark-border-color-base);
+        width: 0;
+        position: absolute;
+        right: -15px;
+        top: 12px;
+        height: 16px;
+        margin: 0 15px;
+      }
+    }
+    .active {
+      border-bottom: 2px solid $primary-color;
+      @include border-color($primary-color, $dark-primary-color);
+    }
+    .top-r-container {
+      flex: 1;
+      height: 40px;
+    }
+  }
+
+  .assets-index-t-b1 {
+    height: 80px;
+    background-color: #edf1f6;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &-search {
+      min-width: 552px;
+      min-height: 40px;
+    }
+  }
+
+  .assets-index-b {
+    display: flex;
+    &-l {
+      min-width: 199px;
+      padding-left: 16px;
+      padding-right: 16px;
+      padding-top: 15px;
+      &-title {
+        font-family: PingFangSC-Medium;
+        font-size: 14px;
+        color: rgba(0, 0, 0, 0.85);
+        font-weight: bold;
+      }
+      &-user {
+        margin-top: 16px;
+        span {
+          display: block;
+          margin-bottom: 8px;
+          font-family: PingFangSC-Regular;
+          font-size: 14px;
+          color: rgba(0, 0, 0, 0.85);
+        }
+      }
+      &-env {
+        margin-top: 16px;
+        span {
+          display: block;
+          margin-bottom: 8px;
+          font-family: PingFangSC-Regular;
+          font-size: 14px;
+          color: rgba(0, 0, 0, 0.85);
+        }
+      }
+      &-label {
+        margin-top: 16px;
+        span {
+          display: block;
+          margin-bottom: 8px;
+          font-family: PingFangSC-Regular;
+          font-size: 14px;
+          color: rgba(0, 0, 0, 0.85);
+        }
+      }
+    }
+    &-r {
+      flex: 1;
+    }
+  }
+}
+</style>

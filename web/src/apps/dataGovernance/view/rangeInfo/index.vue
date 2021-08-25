0b225fe6c22ee17dec259d1e8184dfb2f0827a2a
zhangchx11 <zhangchx11@chinatelecom.cn>
2021-08-25 21:43:22 +0800

    完成资产目录字段信息和分区信息前端页面开发

---
+++ web/src/apps/dataGovernance/view/rangeInfo/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="range-info-wrap">
+    <Table :columns="columns" :data="rangeData"></Table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "rangeInfo",
+  props: {
+    rangeInfo: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      columns: [
+        { title: "分区名", key: "partName" },
+        { title: "记录数", key: "reordCt" },
+        { title: "存储量", key: "store" },
+        { title: "创建时间", key: "createTime" },
+        { title: "最后更新时间", key: "lastAccessTime" }
+      ],
+      rangeData: this.rangeInfo
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.range-info-wrap {
+  padding-left: 24px;
+  padding-top: 24px;
+}
+</style>

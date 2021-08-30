0a7e7fc4ef86fc15b2a28f67e0b94c8ace3a8c0b
汪鸣峰 <wangmingfeng@chelun.com>
2021-08-30 16:23:02 +0800

    增加血缘

---
+++ web/src/apps/dataGovernance/view/assetsInfo/components/lineage/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <div>
+    </div>
+  </div>
+</template>
+<script>
+import node from './base_node';
+import RelationEdge from './edge';
+import DagreCanvas from './dagre-canvas';
+import './index.scss';
+import 'butterfly-dag/dist/index.css';
+
+export default {
+  name: 'lineage',
+  data() {
+    return {
+      canvas: null
+    }
+  },
+  props: {
+    processId: String,
+    lineageData: {
+      required: true
+    }
+  },
+  methods: {
+    convertData() {
+      let data = {
+        nodes: [],
+        edges: []
+      }
+      let keys = Object.keys(this.lineageData.guidEntityMap)
+      keys.forEach(item => {
+        const cur = this.lineageData.guidEntityMap[item]
+        data.nodes.push({
+          id: item,
+          name: cur.displayText,
+          Class: node,
+          className: 'nodeBackground-color'
+        })
+      })
+      this.lineageData.relations.forEach(relation => {
+        data.edges.push({
+          source: relation.fromEntityId,
+          target: relation.toEntityId
+        })
+      })
+      return data
+    },
+    createOrUpdateCanvas (data) {
+      let root = document.getElementById('dag-canvas');
+      if (!this.canvas) {
+        this.canvas = new DagreCanvas({
+          root: root,
+          disLinkable: true, // 可删除连线
+          linkable: true,    // 可连线
+          draggable: true,   // 可拖动
+          zoomable: true,    // 可放大
+          moveable: true,    // 可平移
+          layout: {
+            type: 'dagreLayout',
+            options: {
+              rankdir: 'LR',
+              nodesep: 40,
+              ranksep: 40,
+              controlPoints: false,
+            },
+          },
+          theme: {
+            edge: {
+              shapeType: 'AdvancedBezier',
+              arrow: true,
+              arrowPosition: 0.5,
+              Class: RelationEdge
+            }
+          }
+        })
+        this.canvas.draw(data);
+        //this.dagData.nodes.forEach(node=> {
+        //node.Class = NNode
+        //})
+        //this.canvas.draw(this.dagData)
+      } else {
+        //this.dagData.nodes.forEach(node=> {
+        //node.Class = NNode
+        //})
+        //this.canvas.redraw(this.dagData)
+        this.canvas.drageReDraw(data);
+      }
+    }
+  },
+  mounted () {
+    this.createOrUpdateCanvas(this.convertData())
+  }
+}
+</script>

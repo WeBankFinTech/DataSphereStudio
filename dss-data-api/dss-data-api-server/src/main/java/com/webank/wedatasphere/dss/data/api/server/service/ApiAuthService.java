9628d9f4043060ce65f3e3ba68110f8ad9fb3a2f
suyc <suyc@chinatelecom.cn>
2021-09-09 09:59:28 +0800

    add groupame

--- dss-data-api/dss-data-api-server/src/main/java/com/webank/wedatasphere/dss/data/api/server/service/ApiAuthService.java
+++ dss-data-api/dss-data-api-server/src/main/java/com/webank/wedatasphere/dss/data/api/server/service/ApiAuthService.java
@@ -3,6 +3,7 @@ package com.webank.wedatasphere.dss.data.api.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.webank.wedatasphere.dss.data.api.server.entity.ApiAuth;
+import com.webank.wedatasphere.dss.data.api.server.entity.response.ApiAuthInfo;
 import com.webank.wedatasphere.dss.data.api.server.entity.response.ApiGroupInfo;
 import com.webank.wedatasphere.linkis.common.exception.ErrorException;
 
@@ -18,7 +19,7 @@ public interface ApiAuthService extends IService<ApiAuth> {
     public List<ApiGroupInfo> getApiGroupList(Long workspaceId);
     public boolean saveApiAuth(ApiAuth apiAuth) throws ErrorException;
 
-    public List<ApiAuth> getApiAuthList(Long workspaceId, List<Long> totals, Integer pageNow, Integer pageSize);
+    public List<ApiAuthInfo> getApiAuthList(Long workspaceId, List<Long> totals, Integer pageNow, Integer pageSize);
 
     public void deleteApiAuth(Long id);
 

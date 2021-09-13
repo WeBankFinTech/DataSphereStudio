ae10d6c9bac14d8f9401fbd2d04a31579c5cc6b0
Dlimeng <77954309@qq.com>
2021-09-13 20:34:16 +0800

    init

---
+++ dss-apps/dss-datamodel-center/dss-datamodel-dimension-management/src/main/java/com/webank/wedatasphere/dss/datamodel/restful/ConfigurationRestfulApi.java
@@ -0,0 +1,24 @@
+package com.webank.wedatasphere.dss.datamodel.restful;
+
+import com.webank.wedatasphere.linkis.server.Message;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+@Component
+@Path("/datamodel/config")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ConfigurationRestfulApi {
+
+    @GET
+    @Path("/view")
+    public Response getView(@Context HttpServletRequest req) throws IOException {
+        return  Message.messageToResponse(Message.ok().data("fullTree","test"));
+    }
+}

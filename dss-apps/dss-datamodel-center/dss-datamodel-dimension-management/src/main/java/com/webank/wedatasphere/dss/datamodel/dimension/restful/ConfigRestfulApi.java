67cbe5498202f5aeb0c25c33ea2c24c8dbf89805
helong <303896342@qq.com>
2021-09-14 10:21:45 +0800

    init

--- dss-apps/dss-datamodel-center/dss-datamodel-dimension-management/src/main/java/com/webank/wedatasphere/dss/datamodel/dimension/restful/ConfigRestfulApi.java
+++ dss-apps/dss-datamodel-center/dss-datamodel-dimension-management/src/main/java/com/webank/wedatasphere/dss/datamodel/dimension/restful/ConfigRestfulApi.java
@@ -4,21 +4,28 @@ import com.webank.wedatasphere.linkis.server.Message;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 
+/**
+ * @author Dylan
+ * @date 2021/9/14
+ */
 @Component
 @Path("/datamodel/config")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class ConfigurationRestfulApi {
+public class ConfigRestfulApi {
 
     @GET
-    @Path("/view")
+    @Path("/get")
     public Response getView(@Context HttpServletRequest req) throws IOException {
-        return  Message.messageToResponse(Message.ok().data("fullTree","test"));
+        return  Message.messageToResponse(Message.ok().data("fullTree","get"));
     }
 }

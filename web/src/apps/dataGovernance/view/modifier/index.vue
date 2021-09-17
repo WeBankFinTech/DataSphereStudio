8197849479e9e98e9cd1ac56521c24596fdee4b9
汪鸣峰 <wangmingfeng@chelun.com>
2021-09-17 10:07:07 +0800

    迁移数仓静态代码，路由修改

---
+++ web/src/apps/dataGovernance/view/modifier/index.vue
@@ -0,0 +1,198 @@
+<template>
+  <div class="console-page">
+    <div class="top-line">
+      <div style="display: flex">
+        <Select
+          v-model="searchParams.tokenStatus"
+          placeholder="修饰词状态"
+          style="width: 120px"
+        >
+          <Option
+            v-for="item in cityList"
+            :value="item.value"
+            :key="item.value"
+          >
+            {{ item.label }}
+          </Option>
+        </Select>
+        <Select
+          v-model="searchParams.created"
+          placeholder="添加人"
+          style="width: 120px"
+        >
+          <Option
+            v-for="item in cityList"
+            :value="item.value"
+            :key="item.value"
+          >
+            {{ item.label }}
+          </Option>
+        </Select>
+        <Input
+          search
+          v-model="searchParams.name"
+          enter-button
+          placeholder="输入名称搜索"
+          style="width: 300px"
+        />
+      </div>
+      <Button type="primary" icon="md-add" @click="handleCreate">
+        新增修饰词
+      </Button>
+    </div>
+    <Table
+      :columns="columns"
+      :data="datalist"
+      :loading="loading"
+      style="margin-bottom: 16px"
+    >
+      <template slot-scope="{}" slot="action">
+        <Button type="primary" size="small" style="margin-right: 5px">
+          修改
+        </Button>
+        <Button type="error" size="small" style="margin-right: 5px">
+          启用
+        </Button>
+        <Button type="error" size="small">禁用</Button>
+      </template>
+    </Table>
+    <div class="page-line">
+      <Page :total="100" show-sizer />
+    </div>
+    <edit-modal
+      :visible.sync="modalCfg.visible"
+      :id="modalCfg.id"
+      :mode="modalCfg.mode"
+    />
+  </div>
+</template>
+
+<script>
+import EditModal from './editModal.vue'
+const data = [
+  {
+    name: '主题域名字',
+    ename: 'cname',
+    des: '描述描述描述',
+    created: '创建人',
+    ctime: '2010-1-1',
+    authority: '权限1,权限2',
+    action: '操作',
+  },
+]
+export default {
+  components: { EditModal },
+  methods: {
+    handleModalFinish() {},
+    handleCreate() {
+      this.modalCfg = {
+        visible: true,
+        mode: 'create',
+      }
+    },
+    handleDelete() {},
+    handleEdit(id) {
+      this.modalCfg = {
+        visible: true,
+        mode: 'edit',
+        id,
+      }
+    },
+    handleSearch() {},
+  },
+  data() {
+    return {
+      cityList: [
+        {
+          value: 'New York',
+          label: 'New York',
+        },
+        {
+          value: 'London',
+          label: 'London',
+        },
+        {
+          value: 'Sydney',
+          label: 'Sydney',
+        },
+        {
+          value: 'Ottawa',
+          label: 'Ottawa',
+        },
+        {
+          value: 'Paris',
+          label: 'Paris',
+        },
+        {
+          value: 'Canberra',
+          label: 'Canberra',
+        },
+      ],
+      columns: [
+        {
+          title: '主题域名称',
+          key: 'name',
+        },
+        {
+          title: '主题域英文名',
+          key: 'ename',
+        },
+        {
+          title: '描述',
+          key: 'des',
+        },
+        {
+          title: '主题域选择权限',
+          key: 'authority',
+        },
+        {
+          title: '创建人',
+          key: 'created',
+        },
+        {
+          title: '创建时间',
+          key: 'ctime',
+        },
+        {
+          title: '操作',
+          key: 'action',
+          slot: 'action',
+        },
+      ],
+      // 搜索参数
+      searchParams: {
+        tokenStatus: '',
+        name: '',
+        created: '',
+      },
+      // 数据列表
+      datalist: data,
+      // 弹窗参数
+      modalCfg: {
+        mode: '',
+        id: '',
+        visible: false,
+      },
+      // 是否加载中
+      loading: false,
+      // 分页配置
+      pageCfg: {
+        current: 1,
+        pageSize: 10,
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.top-line {
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: space-between;
+}
+.page-line {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>

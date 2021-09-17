9bf36285fffa2beeb9a896965a8498bfa7307ef4
KidUncle <764393905@qq.com>
2021-09-17 19:08:46 +0800

    fixbug: 组件接入问题

--- web/src/apps/workspace/module/managementPlatform/util/fomat.js
+++ web/src/apps/workspace/module/managementPlatform/util/fomat.js
@@ -1,3 +1,14 @@
+const menuInfo = JSON.parse(sessionStorage.getItem("menuOptions"));
+const menuOptions = [];
+menuInfo.forEach(info => {
+  menuOptions.push(info.title_en);
+});
+const _menuOptions = new Map();
+menuOptions.forEach((item, idx) => {
+  _menuOptions.set(item, idx);
+});
+console.log("_menuOptions", _menuOptions);
+
 /**
  *
  * @param {Object} _component
@@ -5,34 +16,18 @@
  */
 function formatComponentData(component) {
   let _component = Object.assign({}, component);
-  if( _component.if_iframe == 1 ) {
+  if (_component.if_iframe == 1) {
     _component.if_iframe = true;
   } else {
     _component.if_iframe = false;
   }
-  if( _component.is_active == 1 ) {
+  if (_component.is_active == 1) {
     _component.is_active = true;
   } else {
     _component.is_active = false;
   }
-  switch(_component.onestop_menu_id) {
-    case 1:
-      _component.onestop_menu_id = 'dataAccess';
-      break;
-    case 2:
-      _component.onestop_menu_id = 'dataanAlysis';
-      break;
-    case 3:
-      _component.onestop_menu_id = 'productionOperation';
-      break;
-    case 4:
-      _component.onestop_menu_id = 'dataQuality';
-      break;
-    case 5:
-      _component.onestop_menu_id = 'administratorFunction';
-      break;
-  }
-  return _component
+  _component.onestop_menu_id = menuOptions[_component.onestop_menu_id - 1];
+  return _component;
 }
 
 /**
@@ -41,47 +36,28 @@ function formatComponentData(component) {
  */
 function formatComponentDataForPost(component) {
   let _component = Object.assign({}, component);
-  if ( _component.if_iframe ) {
+  if (_component.if_iframe) {
     _component.if_iframe = 1;
   } else {
     _component.if_iframe = 0;
   }
-  if ( _component.is_active ) {
+  if (_component.is_active) {
     _component.is_active = 1;
   } else {
     _component.is_active = 0;
   }
-  switch(_component.onestop_menu_id) {
-    case 'dataAccess':
-      _component.onestop_menu_id = 1;
-      break;
-    case 'dataanAlysis':
-      _component.onestop_menu_id = 2;
-      break;
-    case 'productionOperation':
-      _component.onestop_menu_id = 3;
-      break;
-    case 'dataQuality':
-      _component.onestop_menu_id = 4;
-      break;
-    case 'administratorFunction':
-      _component.onestop_menu_id = 5;
-      break;
-  }
+
+  _component.onestop_menu_id = _menuOptions.get(_component.onestop_menu_id) + 1;
 
   // 删除多余的属性
-  if ( Object.keys(_component).includes('_id') ) {
+  if (Object.keys(_component).includes("_id")) {
     delete _component._id;
   }
-  if ( Object.keys(_component).includes('isAdded') ) {
+  if (Object.keys(_component).includes("isAdded")) {
     delete _component.isAdded;
   }
 
-  return _component
+  return _component;
 }
 
-
-export {
-  formatComponentData,
-  formatComponentDataForPost
-}
+export { formatComponentData, formatComponentDataForPost };

3f77030657e1a42e2d4b1359ba92cc903de4e763
hantang <th3408251986@163.com>
2021-09-17 15:45:52 +0800

    linki1.0.2版本迁移

---
+++ dss-appconn/dss-appconn-manager/dss-appconn-manager-client/src/main/scala/com/webank/wedatasphere/dss/appconn/manager/action/GetAppConnInfosAction.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 WeBank
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.webank.wedatasphere.dss.appconn.manager.action
+
+
+import com.webank.wedatasphere.linkis.httpclient.dws.request.DWSHttpAction
+import com.webank.wedatasphere.linkis.httpclient.request.{GetAction, UserAction}
+
+class GetAppConnInfosAction extends GetAction with DWSHttpAction with UserAction{
+
+  private var user:String = _
+
+  override def suffixURLs: Array[String] = Array("dss", "framework", "project", "appconn", "listAppConnInfos")
+
+  override def setUser(user: String): Unit =  this.user = user
+
+  override def getUser: String = this.user
+}
+
+class GetAppConnInfoAction extends GetAction with DWSHttpAction with UserAction {
+
+  private var appConnName: String = _
+  private var user:String = _
+
+  override def suffixURLs: Array[String] = Array("dss", "framework", "project", "appconn", appConnName, "get")
+
+  def setAppConnName(appConnName: String): Unit = this.appConnName = appConnName
+
+  override def setUser(user: String): Unit =  this.user = user
+
+  override def getUser: String = this.user
+
+}
---
+++ dss-standard/development-standard/development-process-standard/src/main/java/com/webank/wedatasphere/dss/standard/app/development/service/RefCRUDService.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 WeBank
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.webank.wedatasphere.dss.standard.app.development.service;
+
+
+import com.webank.wedatasphere.dss.standard.app.development.operation.RefCopyOperation;
+import com.webank.wedatasphere.dss.standard.app.development.operation.RefCreationOperation;
+import com.webank.wedatasphere.dss.standard.app.development.operation.RefDeletionOperation;
+import com.webank.wedatasphere.dss.standard.app.development.operation.RefUpdateOperation;
+import com.webank.wedatasphere.dss.standard.app.development.ref.CopyRequestRef;
+import com.webank.wedatasphere.dss.standard.app.development.ref.CreateRequestRef;
+import com.webank.wedatasphere.dss.standard.app.development.ref.DeleteRequestRef;
+import com.webank.wedatasphere.dss.standard.common.entity.ref.RequestRef;
+import com.webank.wedatasphere.dss.standard.common.entity.ref.ResponseRef;
+
+public interface RefCRUDService extends DevelopmentService {
+
+     <K extends CreateRequestRef> RefCreationOperation<K> getRefCreationOperation();
+
+     <K extends CopyRequestRef> RefCopyOperation<K> getRefCopyOperation();
+
+     <K extends RequestRef> RefUpdateOperation<K> getRefUpdateOperation();
+
+     <K extends DeleteRequestRef> RefDeletionOperation<K> getRefDeletionOperation();
+
+}

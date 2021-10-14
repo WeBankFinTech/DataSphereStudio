284daaa81aa481bf5456b79e675c888629cb357c
hantang <th3408251986@163.com>
2021-10-14 13:56:00 +0800

    dss适配linkis-1.0.2工程初始化

--- dss-appconn/appconns/dss-eventchecker-appconn/src/main/scala/com/webank/wedatasphere/dss/appconn/eventchecker/execution/EventCheckerExecutionAction.scala
+++ dss-appconn/appconns/dss-eventchecker-appconn/src/main/scala/com/webank/wedatasphere/dss/appconn/eventchecker/execution/EventCheckerExecutionAction.scala
@@ -1,18 +1,16 @@
 /*
+ * Copyright 2019 WeBank
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  * Copyright 2019 WeBank
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  *  you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -20,11 +18,8 @@ package com.webank.wedatasphere.dss.appconn.eventchecker.execution
 
 import com.webank.wedatasphere.dss.appconn.eventchecker.EventCheckerCompletedExecutionResponseRef
 import com.webank.wedatasphere.dss.appconn.eventchecker.entity.EventChecker
-import com.webank.wedatasphere.dss.standard.app.development.execution.common.{AbstractRefExecutionAction, LongTermRefExecutionAction, RefExecutionState}
+import com.webank.wedatasphere.dss.standard.app.development.listener.common.{AbstractRefExecutionAction, LongTermRefExecutionAction, RefExecutionState}
 
-/**
-  * Created by allenlliu on 2019/11/12.
-  */
 class EventCheckerExecutionAction extends AbstractRefExecutionAction with LongTermRefExecutionAction {
   private[this] var _state: RefExecutionState = _
   private var schedulerId: Int = _
--- dss-appconn/appconns/dss-orchestrator-framework-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/orchestrator/operation/OrchestratorFrameworkExportOperation.java
+++ dss-appconn/appconns/dss-orchestrator-framework-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/orchestrator/operation/OrchestratorFrameworkExportOperation.java
@@ -1,51 +1,42 @@
 /*
+ * Copyright 2019 WeBank
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  * Copyright 2019 WeBank
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  *  you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 package com.webank.wedatasphere.dss.appconn.orchestrator.operation;
 
-import com.webank.wedatasphere.dss.appconn.orchestrator.conf.OrchestratorConf;
 import com.webank.wedatasphere.dss.appconn.orchestrator.ref.DefaultOrchestratorExportResponseRef;
 import com.webank.wedatasphere.dss.common.protocol.ResponseExportOrchestrator;
 import com.webank.wedatasphere.dss.common.utils.DSSCommonUtils;
 import com.webank.wedatasphere.dss.common.utils.DSSExceptionUtils;
 import com.webank.wedatasphere.dss.orchestrator.common.protocol.RequestExportOrchestrator;
-import com.webank.wedatasphere.dss.orchestrator.core.ref.OrchestratorExportRequestRef;
-import com.webank.wedatasphere.dss.orchestrator.core.ref.OrchestratorExportResponseRef;
-import com.webank.wedatasphere.dss.standard.app.development.DevelopmentService;
-import com.webank.wedatasphere.dss.standard.app.development.publish.RefExportOperation;
+import com.webank.wedatasphere.dss.orchestrator.common.ref.OrchestratorExportRequestRef;
+import com.webank.wedatasphere.dss.orchestrator.common.ref.OrchestratorExportResponseRef;
+import com.webank.wedatasphere.dss.sender.service.DSSSenderServiceFactory;
+import com.webank.wedatasphere.dss.standard.app.development.service.DevelopmentService;
+import com.webank.wedatasphere.dss.standard.app.development.operation.RefExportOperation;
 import com.webank.wedatasphere.dss.standard.common.exception.operation.ExternalOperationFailedException;
 import com.webank.wedatasphere.linkis.rpc.Sender;
 import com.webank.wedatasphere.linkis.server.BDPJettyServerHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * @author allenlliu
- * @date 2020/12/15 11:48
- */
-public class OrchestratorFrameworkExportOperation implements RefExportOperation<OrchestratorExportRequestRef, OrchestratorExportResponseRef> {
-
-    private  DevelopmentService service;
-
-    private final Sender sender = Sender.getSender(OrchestratorConf.ORCHESTRATOR_SERVER_DEV_NAME.getValue());
-
+public class OrchestratorFrameworkExportOperation implements RefExportOperation<OrchestratorExportRequestRef> {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrchestratorFrameworkExportOperation.class);
 
+    private final Sender sender = DSSSenderServiceFactory.getOrCreateServiceInstance().getOrcSender();
+    private  DevelopmentService service;
 
     @Override
     public OrchestratorExportResponseRef exportRef(OrchestratorExportRequestRef requestRef) throws ExternalOperationFailedException {
@@ -55,7 +46,7 @@ public class OrchestratorFrameworkExportOperation implements RefExportOperation<
         }
         LOGGER.info("Begin to ask to export orchestrator, requestRef is {}", DSSCommonUtils.COMMON_GSON.toJson(requestRef));
         RequestExportOrchestrator exportRequest = new RequestExportOrchestrator(requestRef.getUserName(),
-                requestRef.getWorkspaceName(), requestRef.getOrchestratorId(), -1L,
+                requestRef.getWorkspaceName(), requestRef.getOrcId(), -1L,
                 requestRef.getProjectName(), requestRef.getDSSLabels(), requestRef.getAddOrcVersionFlag(),
                 BDPJettyServerHelper.gson().toJson(requestRef.getWorkspace()));
         ResponseExportOrchestrator exportResponse = null;

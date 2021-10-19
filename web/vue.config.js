2a08c01ee1c4a3fb3864542aaf8da2c6a622070c
luban08 <1017573685@qq.com>
2021-10-19 16:08:09 +0800

    解决body背景色在开发和部署两种模式下的不一致问题

--- web/vue.config.js
+++ web/vue.config.js
@@ -261,10 +261,10 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://172.24.2.230:8088",
+        // target: "http://172.24.2.230:8088",
         //target: 'http://172.24.2.29:9202', //yichao
         // target: "http://172.24.2.83:9202", //jiawei
-        //target: 'http://luban.ctyun.cn:8088',
+        target: 'http://luban.ctyun.cn:8088',
         //target: 'http://devluban.ctyun.cn:8088',
         changeOrigin: true,
         pathRewrite: {

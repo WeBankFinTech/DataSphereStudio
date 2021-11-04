7a935270020f208a20fd21c64eb738193f61457d
张晨曦 <zhangchx11@chinatelecom.cn>
2021-11-04 10:19:46 +0800

    修复数据存储大数据量显示错误

--- web/vue.config.js
+++ web/vue.config.js
@@ -261,7 +261,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        //target: "http://127.0.0.1:8088",
+        // target: "http://127.0.0.1:8088",
         //target: 'http://127.0.0.1:9202', //yichao
         // target: "http://127.0.0.1:9202", //jiawei
         target: "http://luban.ctyun.cn:8088",

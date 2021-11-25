825821510f306bdaa57722d8af688c874472ef7a
汪鸣峰 <wangmf2@chinatelecom.cn>
2021-11-25 02:15:02 +0000

    Ui optimization

--- web/src/common/config/apiPath.js
+++ web/src/common/config/apiPath.js
@@ -4,8 +4,10 @@ export default {
   PROJECT_PATH: '/dss/framework/project/',
   ORCHESTRATOR_PATH: '/dss/framework/orchestrator/',
   WORKFLOW_PATH: '/dss/workflow/',
-  PUBLISH_PATH: '/dss/framework/release/',
+  //PUBLISH_PATH: '/dss/framework/release/',
+  PUBLISH_PATH: '/dss/workflow/', // 社区版本适配
   DATASOURCE_PATH: '/dss/data/api/datasource/',
   DATA_GOVERNANCE: '/dss/data/governance/asset/',
   WAREHOUSE_PATH: '/dss/data/governance/classification/'
 }
+

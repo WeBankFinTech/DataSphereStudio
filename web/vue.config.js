f3b4cb18b7b88ab5eba1dd08486cd85b19c2f96e
luban08 <1017573685@qq.com>
2021-12-01 17:06:03 +0800

    向导建表显示

--- web/vue.config.js
+++ web/vue.config.js
@@ -247,10 +247,10 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8088",
+        // target: "http://127.0.0.1:8088",
         //target: 'http://127.0.0.1:9202', //yichao
         // target: "http://127.0.0.1:9202", //jiawei
-        //target: "http://luban.ctyun.cn:8088",
+        target: "http://luban.ctyun.cn:8088",
         //target: "http://devluban.ctyun.cn:8088",
         changeOrigin: true,
         pathRewrite: {

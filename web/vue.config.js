e18fdb0a69d5e8f280b109fa887ef5fd4216c8a2
Magnus Wang <mingfengx.wang@intel.com>
2021-12-02 14:21:44 +0800

    fix bug

--- web/vue.config.js
+++ web/vue.config.js
@@ -242,11 +242,11 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        // target: "http://127.0.0.1:8088",
+        target: "http://127.0.0.1:8088",
         //target: 'http://127.0.0.1:9202', //yichao
         // target: "http://127.0.0.1:9202", //jiawei
         // target: "http://luban.ctyun.cn:8088",
-        target: "http://devluban.ctyun.cn:8088",
+        //target: "http://devluban.ctyun.cn:8088",
         changeOrigin: true,
         pathRewrite: {
           "^/api": "/api"

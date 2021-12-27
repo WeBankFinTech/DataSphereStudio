8a0a1da860b8618d85eb3341bf3cfaa020a7ad1d
suyc <suyc@chinatelecom.cn>
2021-12-27 17:10:25 +0800

    add dss-guide-server server

--- dss-guide/dss-guide-server/src/main/java/com/webank/wedatasphere/dss/guide/server/conf/GuideConf.java
+++ dss-guide/dss-guide-server/src/main/java/com/webank/wedatasphere/dss/guide/server/conf/GuideConf.java
@@ -10,6 +10,6 @@ import org.apache.linkis.common.conf.CommonVars;
  * @Created by suyc
  */
 public interface GuideConf {
-    CommonVars<String> GUIDE_CONTENT_IMAGES_PATH = CommonVars.apply("guide_content_images_path", "/usr/local/anlexander/all_bak/dss_linkis/dss-linkis-1.0.2/images");
+    CommonVars<String> GUIDE_CONTENT_IMAGES_PATH = CommonVars.apply("guide.content.images.path", "/usr/local/anlexander/all_bak/dss_linkis/dss-linkis-1.0.2/images");
 
 }

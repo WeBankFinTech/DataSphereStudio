c756642edcf76a7aaf717bb69cb714926580395f
Magnus Wang <mingfengx.wang@intel.com>
2022-01-11 13:36:39 +0800

    dashboard增加耗时排行

--- web/vue.config.js
+++ web/vue.config.js
@@ -269,8 +269,8 @@ module.exports = {
         }
       },
       "/dolphinscheduler": {
-        target: "http://172.24.2.233:12345",
-        //target: "https://dolphin.ctyun.cn:10002",
+        //target: "http://172.24.2.233:12345",
+        target: "https://dolphin.ctyun.cn:10002",
         changeOrigin: true,
         pathRewrite: {
           "^/dolphinscheduler": "/dolphinscheduler"

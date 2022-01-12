09599259fad73187135c7c0e39c527373f310ac7
张晨曦 <zhangchx11@chinatelecom.cn>
2022-01-12 16:50:30 +0800

    fix 開發中心樣式

--- web/vue.config.js
+++ web/vue.config.js
@@ -240,10 +240,10 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        //target: "http://***REMOVED***:8088",
+        // target: "http://***REMOVED***:8088",
         //target: 'http://***REMOVED***:9202', //yichao
-        // target: "http://***REMOVED***:9202", //jiawei
-         target: "http://luban.ctyun.cn:8088",
+        target: "***REMOVED***:9202", //jiawei
+        // target: "http://luban.ctyun.cn:8088",
         //target: "http://devluban.ctyun.cn:8088",
         changeOrigin: true,
         pathRewrite: {
@@ -277,7 +277,7 @@ module.exports = {
         }
       },
       "/application": {
-        target: "http://***REMOVED***:3022",
+        target: "http://***REMOVED***:9202",
         changeOrigin: true
       }
     }

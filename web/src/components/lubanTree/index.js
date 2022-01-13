4112195fa28feb6709ab307757c6ff2c7b772e06
luban08 <1017573685@qq.com>
2022-01-13 20:38:11 +0800

    知识库后台管理

--- web/src/components/lubanTree/index.js
+++ web/src/components/lubanTree/index.js
@@ -1,7 +1,9 @@
 import libraryTree from './libraryTree/tree.vue';
+import libraryAdminTree from './libraryAdminTree/tree.vue';
 import guideTree from './guideTree/tree.vue';
 
 export default {
   libraryTree,
+  libraryAdminTree,
   guideTree,
 };

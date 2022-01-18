212a1349d332cd929846442af54133f3d4817c58
suyc <suyc@chinatelecom.cn>
2022-01-18 18:25:03 +0800

    fix dss-guide bug

--- dss-guide/dss-guide-server/src/main/java/com/webank/wedatasphere/dss/guide/server/entity/GuideCatalog.java
+++ dss-guide/dss-guide-server/src/main/java/com/webank/wedatasphere/dss/guide/server/entity/GuideCatalog.java
@@ -8,7 +8,6 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
-import java.util.List;
 
 /**
  * @author suyc
@@ -30,11 +29,11 @@ public class GuideCatalog {
     private String createBy;
     private String updateBy;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
     //private Integer isDelete;

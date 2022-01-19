376786f464e0bd8f795433e6d66cbd7aadd423a3
luban08 <1017573685@qq.com>
2022-01-19 10:57:29 +0800

    内置产品手册后端管理平台完成

--- web/src/dss/module/footer/libraryDetail.vue
+++ web/src/dss/module/footer/libraryDetail.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="library-detail" >
     <div class="library-detail-title">{{ doc.title }}</div>
-    <div class="library-detail-content">
+    <div class="library-detail-content" ref="libraryDetail">
       <p v-html="doc.contentHtml"></p>
     </div>
   </div>
 </template>
 <script>
+import { GetChapterDetail } from "@/common/service/apiGuide";
 export default {
   name: "libraryDetail",
   props: {
     doc: {
       type: Object,
       default: () => {},
+    },
+  },
+  mounted() {
+    this.init();
+  },
+  beforeDestroy() {
+    this.dispose();
+  },
+  methods: {
+    init() {
+      const libraryDetail = this.$refs.libraryDetail;
+      libraryDetail.addEventListener("click", this.handleAnchor);
+    },
+    dispose() {
+      const libraryDetail = this.$refs.libraryDetail;
+      libraryDetail.removeEventListener("click", this.handleAnchor);
+    },
+    handleAnchor(e) {
+      // 文章内部链接跳转处理
+      if (e.target.tagName == "A" && e.target.target != "_blank") {
+        const match = e.target.href.match(/\/\d+\.html/g); // 暂定/17.html的格式
+        if (match && match[0]) {
+          const id = match[0].match(/\d+/g)[0];
+          GetChapterDetail(id).then((res) => {
+            this.$emit("on-chapter-click", res.result)
+          });
+        }
+        e.preventDefault();
+      }
     },
   }
 };

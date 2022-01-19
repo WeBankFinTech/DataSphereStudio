aff210778ae4ccae3abc2325d16b27dc66a2f111
luban08 <1017573685@qq.com>
2022-01-19 10:57:29 +0800

    内置产品手册后端管理平台完成

--- web/src/apps/workspace/module/managementPlatform/component/library/menu.vue
+++ web/src/apps/workspace/module/managementPlatform/component/library/menu.vue
@@ -24,7 +24,7 @@
         :model="groupForm"
         :label-width="100"
         :rules="groupRule"
-        v-show="modalType === 'group'"
+        v-show="modalType === 'catalog'"
       >
         <FormItem label="目录名称" prop="title">
           <Input
@@ -34,36 +34,18 @@
             style="width: 300px"
           ></Input>
         </FormItem>
-        <FormItem label="目录描述" prop="description">
-          <Input
-            type="textarea"
-            v-model="groupForm.description"
-            placeholder="请输入描述"
-            style="width: 300px"
-          >
-          </Input>
-        </FormItem>
       </Form>
       <Form
         ref="contentForm"
         :model="contentForm"
         :label-width="100"
         :rules="contentRule"
-        v-show="modalType !== 'group'"
+        v-show="modalType !== 'catalog'"
       >
-        <FormItem label="所属目录" prop="catalog">
-          <Input
-            type="text"
-            v-model="contentForm.catalog"
-            style="width: 300px"
-            disabled
-          >
-          </Input>
-        </FormItem>
         <FormItem label="节点类型" prop="type">
           <RadioGroup v-model="contentForm.type">
-            <Radio label="1" :disabled="!!contentForm.id">文档</Radio>
-            <Radio label="2" :disabled="!!contentForm.id">目录</Radio>
+            <Radio label="chapter" :disabled="!!contentForm.id">文档</Radio>
+            <Radio label="catalog" :disabled="!!contentForm.id">目录</Radio>
           </RadioGroup>
         </FormItem>
         <FormItem label="节点标题" prop="title">
@@ -95,16 +77,16 @@
 
 <script>
 import {
-  GetGuideTree,
-  SaveGuideGroup,
-  SaveGuideContent,
-  DeleteGuideContent,
-  DeleteGuideGroup,
+  GetLibraryTreeTop,
+  GetLibraryTreeById,
+  SaveCatalog,
+  DeleteCatalog,
+  SaveChapter,
+  DeleteChapter,
 } from "@/common/service/apiGuide";
-import util from "@/common/util";
 import lubanTree from "@/components/lubanTree";
 export default {
-  name: "guideMenu",
+  name: "libraryMenu",
   components: {
     "library-admin-tree": lubanTree.libraryAdminTree,
   },
@@ -118,6 +100,7 @@ export default {
       modalTitle: "",
       submitLoading: false,
       groupForm: {
+        parentId: -1, // 第一级目录父id为-1
         title: "",
         description: "",
       },
@@ -125,7 +108,8 @@ export default {
         title: [{ required: true, message: "请输入标题", trigger: "blur" }],
       },
       contentForm: {
-        type: "", // 类型: 1文档，2目录
+        catalogId: -1,
+        type: "",
         title: "",
       },
       contentRule: {
@@ -137,68 +121,40 @@ export default {
     };
   },
   mounted() {
-    this.refreshTree();
+    this.initTree();
   },
   methods: {
-    refreshTree() {
-      GetGuideTree().then((data) => {
-        this.nodes = this.mergeTree(this.nodes, data.result || []);
-        this.nodes = [
-          {
-            id: 1,
-            title: '工作空间首页',
-            type: 'category',
-            isLeaf: false,
-            treeId: `category_1`,
-            canAdd: true,
-            canDelete: true,
-            canUpdate: true,
-            children: []
-          },
-          {
-            id: 2,
-            title: '工作空间首页2',
-            type: 'category',
-            isLeaf: false,
-            treeId: `category_2`,
-            canAdd: true,
-            canDelete: true,
-            canUpdate: true,
-            children: []
-          }
-        ]
+    initTree() {
+      GetLibraryTreeTop().then((data) => {
+        this.nodes = this.refreshTreeTop(this.nodes, data.result || []);
       });
     },
-    mergeTree(source = [], nodes) {
-      // 以新的nodes为主，但要保留source中的opened等属性，path，title属性及新的节点取自nodes
+    refreshTreeTop(source = [], nodes) {
+      // 以新的nodes为主，但要保留source，新的节点取自nodes
       const data = [];
       for (let i = 0, len = nodes.length; i < len; i++) {
         const origin = source.find(item => item.id == nodes[i].id)
-        data.push({
-          ...nodes[i],
-          opened: origin ? origin.opened : false,
-          treeId: `group_${nodes[i].id}`,
-          path: nodes[i].path,
-          title: nodes[i].title || nodes[i].path,
-          canAdd: true,
-          canDelete: true,
-          canUpdate: true,
-          children: nodes[i].children.map((c) => {
-            return {
-              ...c,
-              treeId: `content_${c.id}`,
-              type: c.type == 1 ? "step" : "question", // 类型: 1文档，2目录
-              canDelete: true,
-              canUpdate: true,
-            };
-          }),
-        });
+        if (origin) {
+          data.push({
+            ...origin,
+            title: nodes[i].title
+          })
+        } else {
+          data.push({
+            ...nodes[i],
+            type: 'catalog',
+            treeId: `catalog_${nodes[i].id}`,
+            isLeaf: false,
+            canAdd: true,
+            canDelete: true,
+            canUpdate: true
+          });
+        }
       }
       return data;
     },
     handleTreeClick(node) {
       if (node.isLeaf) {
-        console.log('isleaf', node)
         this.currentTreeId = node.treeId;
         this.$router.push({
           path: "/managementPlatform/library",
@@ -207,64 +163,46 @@ export default {
       } else {
         if (!node.opened && !node.loaded) {
           this.nodes = this.handleLoading(this.nodes, node)
-          setTimeout(() => {
-            this.nodes = this.mockTree(this.nodes, node);
-            console.log(this.nodes, node)
-          }, 200)
+          this.refreshTree(node.id);
         } else {
-          console.log('toggle')
           this.nodes = this.handleToggle(this.nodes, node)
         }
       }
     },
-    mockTree(nodes, node) {
+    refreshTree(catalogId) {
+      GetLibraryTreeById(catalogId).then((data) => {
+        const childrenCatalog = data.result ? (data.result.childrenCatalog || []).map(i => { return {...i, type: "catalog", isLeaf: false, canAdd: true }}) : []
+        const childrenChapter = data.result ? (data.result.childrenChapter || []).map(i => { return {...i, type: "chapter", isLeaf: true }}) : []
+        const children = childrenCatalog.concat(childrenChapter);
+        this.nodes = this.mergeTree(this.nodes, catalogId, children);
+      });
+    },
+    mergeTree(nodes, catalogId, children) {
       const arr = [];
       let matched = false;
       for (let i=0,len=nodes.length; i<len; i++) {
         let item = {};
-        if (nodes[i].id == node.id) {
+        if (nodes[i].id == catalogId) {
           item = {
             ...nodes[i],
             loaded: true,
             loading: false,
             opened: true,
             isLeaf: false,
-            children: [
-              {
-                id: util.guid(),
-                title: '章节1',
-                type: 'chapter',
-                isLeaf: true,
-                treeId: util.guid(),
-                canDelete: true,
-                canUpdate: true,
-              },
-              {
-                id: util.guid(),
-                title: '章节2',
-                type: 'chapter',
-                isLeaf: true,
-                treeId: util.guid(),
-                canDelete: true,
-                canUpdate: true,
-              },
-              {
-                id: util.guid(),
-                title: '子目录',
-                type: 'category',
-                isLeaf: false,
-                treeId: util.guid(),
-                canAdd: true,
+            children: children.map(c => {
+              return {
+                ...c,
+                treeId: `${c.type}_${c.id}`,
                 canDelete: true,
                 canUpdate: true,
               }
-            ]
+            })
           }
           matched = true;
         } else if (!matched && nodes[i].children && nodes[i].children.length) {
           item = {
             ...nodes[i],
-            children: this.mockTree(nodes[i].children, node)
+            children: this.mergeTree(nodes[i].children, catalogId, children)
           }
         } else {
           item = nodes[i];
@@ -319,22 +257,35 @@ export default {
       }
       return arr;
     },
+    haveChildren(nodes, node) {
+      for (let i=0,len=nodes.length; i<len; i++) {
+        if (nodes[i].id == node.id && nodes[i].type == node.type ) {
+          return nodes[i].children && nodes[i].children.length > 0;
+        } else if (nodes[i].children && nodes[i].children.length) {
+          return this.haveChildren(nodes[i].children, node);
+        }
+      }
+      return false;
+    },
     handleDeleteClick(node) {
       this.$Modal.confirm({
         title: "确认删除吗",
         content: "",
         onOk: () => {
-          if (node.type) {
-            DeleteGuideContent(node.id).then((res) => {
-              this.refreshTree();
+          if (node.type == 'chapter') {
+            DeleteChapter(node.id).then((res) => {
+              this.refreshTree(node.catalogId);
             });
           } else {
-            const group = this.nodes.find((i) => i.id == node.id);
-            if (group && group.children && group.children.length) {
-              this.$Message.info("该页面group还有所属内容，不能删除");
+            if (this.haveChildren(this.nodes, node)) {
+              this.$Message.info("该目录下还有所属内容，不能删除");
             } else {
-              DeleteGuideGroup(node.id).then((res) => {
-                this.refreshTree();
+              DeleteCatalog(node.id).then((res) => {
+                if (node.parentId == -1) {
+                  this.initTree();
+                } else {
+                  this.refreshTree(node.parentId);
+                }
               });
             }
           }
@@ -343,40 +294,57 @@ export default {
       });
     },
     handleUpdateClick(node) {
-      const type = node.type ? "content" : "group";
-      this.showModal({
-        type: type,
-        op: "修改",
-      });
-      // 修改操作时，不要直接把node赋值，会同时影响tree的数据
-      if (type == "group") {
-        this.groupForm = {
-          id: node.id,
-          title: node.title || "",
-          description: node.description || "",
-        };
+      if (node.type == "catalog") {
+        // 按当时创建的modal还原
+        if (node.parentId == -1) {
+          this.showModal({
+            type: "catalog",
+            op: "修改",
+          });
+          this.groupForm = {
+            id: node.id,
+            title: node.title || "",
+            description: node.description || "",
+          };
+        } else {
+          this.showModal({
+            type: "chapter",
+            op: "修改",
+          });
+          this.contentForm = {
+            catalogId: node.parentId,
+            id: node.id,
+            type: node.type || 'catalog',
+            title: node.title || ""
+          };
+        }
       } else {
+        this.showModal({
+          type: "chapter",
+          op: "修改",
+        });
         this.contentForm = {
+          catalogId: node.catalogId,
           id: node.id,
-          type: node.type == "step" ? "1" : "2", // 类型: 1文档，2目录
+          type: node.type || 'chapter',
           title: node.title || ""
         };
       }
     },
     handleAddClick(node) {
       this.showModal({
-        type: "content",
+        type: "chapter",
         op: "新增",
       });
       this.contentForm = {
-        groupId: node.id,
+        catalogId: node.id,
         type: "",
         title: "",
       };
     },
     handleAddGroup() {
       this.showModal({
-        type: "group",
+        type: "catalog",
         op: "新增",
       });
     },
@@ -384,7 +352,7 @@ export default {
       const { type, op } = payload;
       this.modalVisible = true;
       this.modalType = type;
-      this.modalTitle = type === "group" ? `${op}目录` : `${op}文档`;
+      this.modalTitle = type === "catalog" ? `${op}目录` : `${op}节点`;
     },
     handleModalCancel() {
       this.modalVisible = false;
@@ -393,14 +361,15 @@ export default {
     },
     handleModalOk() {
       const modalType = this.modalType;
-      if (modalType === "group") {
+      if (modalType === "catalog") {
         this.$refs["groupForm"].validate((valid) => {
           if (valid) {
             this.submitLoading = true;
-            SaveGuideGroup(this.groupForm).then((res) => {
+            console.log('add', this.groupForm)
+            SaveCatalog(this.groupForm).then((res) => {
               this.submitLoading = false;
               this.handleModalCancel();
-              this.refreshTree();
+              this.initTree();
             });
           }
         });
@@ -408,24 +377,44 @@ export default {
         this.$refs["contentForm"].validate((valid) => {
           if (valid) {
             this.submitLoading = true;
-            // 类型: 1-步骤step，2-问题question
-            SaveGuideContent({
-              ...this.contentForm,
-              type: +this.contentForm.type,
-            }).then((res) => {
-              this.submitLoading = false;
-              this.handleModalCancel();
-              this.refreshTree();
-            });
+            if (this.contentForm.type == "catalog") {
+              const data = this.contentForm.id ? {
+                id: this.contentForm.id,
+                parentId: this.contentForm.catalogId,
+                title: this.contentForm.title
+              } : {
+                parentId: this.contentForm.catalogId,
+                title: this.contentForm.title
+              }
+              SaveCatalog(data).then((res) => {
+                this.refreshTree(this.contentForm.catalogId);
+                this.submitLoading = false;
+                this.handleModalCancel();
+              });
+            } else {
+              const data = this.contentForm.id ? {
+                id: this.contentForm.id,
+                catalogId: this.contentForm.catalogId,
+                title: this.contentForm.title
+              } : {
+                catalogId: this.contentForm.catalogId,
+                title: this.contentForm.title
+              }
+              SaveChapter(data).then((res) => {
+                this.refreshTree(this.contentForm.catalogId);
+                this.submitLoading = false;
+                this.handleModalCancel();
+              });
+            }
           }
         });
       }
     },
     resetForm() {
-      if (this.modalType === "group") {
+      if (this.modalType === "catalog") {
         this.groupForm = {
+          parentId: -1,
           title: "",
-          description: "",
         };
         this.$refs["groupForm"].resetFields();
       } else {

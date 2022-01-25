7718a3d37696cc6bbd3979deb9b4c598039be85e
HmhWz <934178858@qq.com>
2022-01-25 15:50:19 +0800

    fix dependencies

--- dss-data-api/dss-data-api-server/src/main/java/com/webank/wedatasphere/dss/data/api/server/restful/DSSDbApiMonitorRestful.java
+++ dss-data-api/dss-data-api-server/src/main/java/com/webank/wedatasphere/dss/data/api/server/restful/DSSDbApiMonitorRestful.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BeanParam;
-import javax.ws.rs.core.Context;
 import java.util.ArrayList;
 import java.util.List;
 

ddfec6302411c024f931ef3cf7f916b77e0fe601
HmhWz <934178858@qq.com>
2022-01-25 15:50:19 +0800

    fix dependencies

--- dss-framework/dss-framework-ctyun-service/src/main/java/com/webank/wedatasphere/dss/framework/admin/restful/DssExchangisController.java
+++ dss-framework/dss-framework-ctyun-service/src/main/java/com/webank/wedatasphere/dss/framework/admin/restful/DssExchangisController.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.core.Context;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;

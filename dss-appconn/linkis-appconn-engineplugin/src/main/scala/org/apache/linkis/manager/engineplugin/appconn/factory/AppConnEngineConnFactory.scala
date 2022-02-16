8fd63565deec6f79aa082a9f330c6a2a03ca731d
liuyou2 <405240259@qq.com>
2022-02-16 19:42:25 +0800

    Update the appconn engine to the latest version

--- dss-appconn/linkis-appconn-engineplugin/src/main/scala/org/apache/linkis/manager/engineplugin/appconn/factory/AppConnEngineConnFactory.scala
+++ dss-appconn/linkis-appconn-engineplugin/src/main/scala/org/apache/linkis/manager/engineplugin/appconn/factory/AppConnEngineConnFactory.scala
@@ -19,6 +19,7 @@ package org.apache.linkis.manager.engineplugin.appconn.factory
 import java.io.File
 
 import com.webank.wedatasphere.dss.appconn.loader.utils.AppConnUtils
+import com.webank.wedatasphere.dss.appconn.manager.AppConnManager
 import org.apache.linkis.DataWorkCloudApplication
 import org.apache.linkis.engineconn.common.conf.EngineConnConf
 import org.apache.linkis.engineconn.common.creation.EngineCreationContext
@@ -26,6 +27,7 @@ import org.apache.linkis.engineconn.common.engineconn.EngineConn
 import org.apache.linkis.engineconn.computation.executor.creation.ComputationSingleExecutorEngineConnFactory
 import org.apache.linkis.engineconn.executor.entity.LabelExecutor
 import org.apache.linkis.governance.common.exception.engineconn.EngineConnExecutorErrorCode
+import org.apache.linkis.manager.engineplugin.appconn.conf.AppConnEngineConnConfiguration
 import org.apache.linkis.manager.engineplugin.appconn.executor.AppConnEngineConnExecutor
 import org.apache.linkis.manager.engineplugin.common.exception.EngineConnPluginErrorException
 import org.apache.linkis.manager.label.entity.engine.EngineType.EngineType
@@ -35,17 +37,24 @@ import org.apache.linkis.manager.label.entity.engine.{EngineType, RunType}
 class AppConnEngineConnFactory extends ComputationSingleExecutorEngineConnFactory {
 
   private val appConnHomePath = new File(EngineConnConf.getWorkHome, AppConnUtils.APPCONN_DIR_NAME)
-  if(!appConnHomePath.exists() && !appConnHomePath.mkdir())
+  if (!appConnHomePath.exists() && !appConnHomePath.mkdir()) {
     throw new EngineConnPluginErrorException(EngineConnExecutorErrorCode.INIT_EXECUTOR_FAILED,
       s"Cannot mkdir ${appConnHomePath.getPath}, please make sure the permission is ok.")
+  }
   DataWorkCloudApplication.setProperty(AppConnUtils.APPCONN_HOME_PATH.key, appConnHomePath.getPath)
   warn(s"Set ${AppConnUtils.APPCONN_HOME_PATH.key}=${appConnHomePath.getPath}.")
 
   override protected def newExecutor(id: Int,
                                      engineCreationContext: EngineCreationContext,
-                                     engineConn: EngineConn): LabelExecutor =
-    new AppConnEngineConnExecutor(id)
-
+                                     engineConn: EngineConn): LabelExecutor = {
+    /**
+     * This is for loading all AppConns when EngineConn is starting, 2 reasons:
+     * 1. Load AppConns will cost lots of time, since all zips must pull from BML
+     * 2. when a task is killed by user, and the AppConns is loading, the loading will be failed, cause a bug when new task submitted in later
+     */
+    AppConnManager.getAppConnManager.init()
+    new AppConnEngineConnExecutor(AppConnEngineConnConfiguration.ENGINE_DEFAULT_LIMIT.getValue, id)
+  }
 
   override protected def getEngineConnType: EngineType = EngineType.APPCONN
 

3bcc21d23eef88f91bdfa5791b90f31ecbc0cd1d
hantang <th3408251986@163.com>
2022-02-16 12:30:28 +0800

    ldap创建用户

--- dss-framework/dss-framework-ctyun-service/src/main/java/com/webank/wedatasphere/dss/framework/admin/service/impl/LdapServiceImpl.java
+++ dss-framework/dss-framework-ctyun-service/src/main/java/com/webank/wedatasphere/dss/framework/admin/service/impl/LdapServiceImpl.java
@@ -1,74 +1,92 @@
 package com.webank.wedatasphere.dss.framework.admin.service.impl;
 
+import com.webank.wedatasphere.dss.framework.admin.restful.DssAuditController;
 import com.webank.wedatasphere.dss.framework.admin.service.LdapService;
 import com.webank.wedatasphere.dss.framework.admin.util.LdapUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.directory.*;
 import javax.naming.ldap.LdapContext;
+import java.util.ArrayList;
+import java.util.Collections;
 
 @Service
 public class LdapServiceImpl implements LdapService {
 
 
-    /** add user to AD */
+
+    /**
+     * add user to AD
+     */
     @Override
-    public void addUser(String adminName,String adminPassword,String ldapUrl,String baseDN,String userName,String pwd) throws NamingException {
-        LdapContext ctx = LdapUtils.connectLDAP(adminName, adminPassword, ldapUrl);
-        BasicAttributes basicAttributes = new BasicAttributes();
-        BasicAttribute objectClassSet = new BasicAttribute("objectclass");
-        objectClassSet.add("inetOrgPerson");
-        basicAttributes.put(objectClassSet);
-        basicAttributes.put("sn", userName);
-        basicAttributes.put("cn", userName);
-        basicAttributes.put("uid", userName);
-        basicAttributes.put("userPassword", pwd);
-        ctx.createSubcontext("uid="+userName+","+baseDN, basicAttributes);
-        LdapUtils.closeContext(ctx);
+    public void addUserWithPwd(String adminName, String adminPassword, String ldapUrl, String baseDN, String userName, String pwd, String group) throws NamingException {
+        boolean groupIsExist = exist(adminName, adminPassword, ldapUrl,baseDN, group, "group");
+        if(!groupIsExist){
+            LdapUtils.addGroup(adminName, adminPassword, ldapUrl, baseDN, group);
+        }
+        LdapUtils.addUser(adminName, adminPassword, ldapUrl, baseDN, userName, pwd, group);
+
     }
 
 
     @Override
-    public void update(String adminName,String adminPassword,String ldapUrl,String baseDN,String userName,String pwd) throws NamingException {
+    public void updatePwd(String adminName, String adminPassword, String ldapUrl, String baseDN, String userName, String pwd) throws NamingException {
         LdapContext ctx = LdapUtils.connectLDAP(adminName, adminPassword, ldapUrl);
         ModificationItem[] mods = new ModificationItem[1];
         Attribute attr = new BasicAttribute("userPassword", pwd);
-          // Support add, replace and remove an attribute.
+        // Support add, replace and remove an attribute.
+        baseDN="ou=user,"+baseDN;
         mods[0] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, attr);
-        ctx.modifyAttributes("uid="+userName+","+baseDN, mods);
+        ctx.modifyAttributes("uid=" + userName + "," + baseDN, mods);
         LdapUtils.closeContext(ctx);
 
     }
+
     @Override
-    public boolean exist(String adminName,String adminPassword,String ldapUrl,String baseDN,String userName) throws NamingException {
+    public boolean exist(String adminName, String adminPassword, String ldapUrl, String baseDN, String cn, String type) throws NamingException {
         LdapContext ctx = LdapUtils.connectLDAP(adminName, adminPassword, ldapUrl);
         SearchControls searchCtls = new SearchControls();
         searchCtls.setSearchScope(SearchControls.SUBTREE_SCOPE);
-        String searchFilter = "uid="+userName;
+        String searchFilter = null;
+        String returnedAtts[] = null;
+
+        if ("user".equals(type)) {
+            searchFilter = "uid=" + cn;
+            baseDN = "ou=group," + baseDN;
+            returnedAtts = new String[]{"uid"};
+
+        } else if ("group".equals(type)) {
+            searchFilter = "cn=" + cn;
+            baseDN = "ou=group," + baseDN;
+            returnedAtts = new String[]{"cn"};
+
+        }
         String searchBase = baseDN;
-        String returnedAtts[] = { "cn" };
         searchCtls.setReturningAttributes(returnedAtts);
         boolean exist = false;
-
         NamingEnumeration entries = ctx.search(searchBase, searchFilter, searchCtls);
-        if(entries.hasMore()){
+        if (entries.hasMore()) {
             exist = true;
         }
         LdapUtils.closeContext(ctx);
-        return  exist;
-
-        }
+        return exist;
+    }
 
 
     public static void main(String[] args) throws NamingException {
-        LdapService ldapService = new LdapServiceImpl();
-        ldapService.addUser("cn=Manager,dc=example,dc=com","Aaht123@chinanet","ldap://172.24.2.230:389","ou=user,dc=example,dc=com","ht01001","Th34082519861!");
-        ldapService.update("cn=Manager,dc=example,dc=com","Aaht123@chinanet","ldap://172.24.2.230:389","ou=user,dc=example,dc=com","ht02","1Th34082519861!");
-       boolean exist =  ldapService.exist("cn=Manager,dc=example,dc=com","Aaht123@chinanet","ldap://172.24.2.230:389","ou=user,dc=example,dc=com","ht03");
-       System.out.println("==="+exist);
+        LdapServiceImpl ldapService = new LdapServiceImpl();
+//        ldapService.addUserWithPwd("cn=Manager,dc=example,dc=com", "hantang", "ldap://xxxxx:389", "dc=example,dc=com", "ht010023", "Th34082519861!","ht010014");
+        ldapService.updatePwd("cn=Manager,dc=example,dc=com", "hantang", "ldap://hadoop02:389", "dc=example,dc=com", "ht010023", "Th34082519861!");
 
+//        ldapService.addUser("cn=Manager,dc=example,dc=com", "hantang", "ldap://hadoop02:389", "ou=user,dc=example,dc=com", "ht01001", "Th34082519861!");
+//        ldapService.update("cn=Manager,dc=example,dc=com", "Aaht123@chinanet", "ldap://1xxxx:389", "ou=user,dc=example,dc=com", "ht02", "1Th34082519861!");
+//        boolean exist = ldapService.exist("cn=Manager,dc=example,dc=com", "Aaht123@chinanet", "ldap://xxxxx:389", "ou=user,dc=example,dc=com", "ht03");
+//        System.out.println("===" + exist);
+//        int  id = ldapService.getMaxId("cn=Manager,dc=example,dc=com", "hantang", "ldap://hadoop02:389", "ou=user,dc=example,dc=com","user");
     }
 
 }
--- dss-framework/dss-framework-ctyun-service/src/main/java/com/webank/wedatasphere/dss/framework/admin/util/LdapUtils.java
+++ dss-framework/dss-framework-ctyun-service/src/main/java/com/webank/wedatasphere/dss/framework/admin/util/LdapUtils.java
@@ -1,21 +1,29 @@
 package com.webank.wedatasphere.dss.framework.admin.util;
 
+import com.webank.wedatasphere.dss.framework.admin.service.impl.LdapServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.naming.Context;
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.directory.*;
 import javax.naming.ldap.InitialLdapContext;
 import javax.naming.ldap.LdapContext;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Hashtable;
 
 public class LdapUtils {
-    public static LdapContext connectLDAP(String adminName, String adminPassword, String ldapUrl) throws NamingException {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LdapUtils.class);
 
+    public static LdapContext connectLDAP(String adminName, String adminPassword, String ldapUrl) throws NamingException {
         Hashtable<String, String> HashEnv = new Hashtable<String, String>();
         HashEnv.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
         HashEnv.put(Context.SECURITY_AUTHENTICATION, "simple");// "none","simple","strong"
         HashEnv.put(Context.SECURITY_PRINCIPAL, adminName);
         HashEnv.put(Context.SECURITY_CREDENTIALS, adminPassword);
         HashEnv.put(Context.PROVIDER_URL, ldapUrl);
-
         LdapContext ctx = new InitialLdapContext(HashEnv, null);
         return ctx;
     }
@@ -25,5 +33,93 @@ public class LdapUtils {
         if (ctx != null) {
             ctx.close();
         }
+    }
+
+
+    /*
+    * group 为 * 代表查询所有group
+     */
+    public static  int getMaxId(String adminName, String adminPassword, String ldapUrl, String baseDN, String type, String group) throws NamingException {
+        LdapContext ctx = LdapUtils.connectLDAP(adminName, adminPassword, ldapUrl);
+        SearchControls searchCtls = new SearchControls();
+        searchCtls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        String returnedAtts[] = null;
+
+        String searchFilter = "";
+        if ("user".equals(type)) {
+            baseDN="ou=user,"+baseDN;
+            searchFilter = "uid=*";
+            returnedAtts = new String[]{"uidNumber"};
+
+        } else if ("group".equals(type)) {
+            baseDN="ou=group,"+baseDN;
+            searchFilter = "cn=" + group;
+            returnedAtts = new String[]{"gidNumber"};
+        }
+        String searchBase = baseDN;
+        searchCtls.setReturningAttributes(returnedAtts);
+        NamingEnumeration entries = ctx.search(searchBase, searchFilter, searchCtls);
+        if (!entries.hasMore()) {
+            return 10000;
+        }
+        ArrayList<Integer> ids = new ArrayList<>();
+        while (entries.hasMore()) {
+            SearchResult sr = (SearchResult) entries.next();
+            Attributes attributes = sr.getAttributes();
+            if ("user".equals(type)) {
+                int id = Integer.parseInt(attributes.get("uidNumber").get().toString());
+                ids.add(id);
+            } else if ("group".equals(type)) {
+                int id = Integer.parseInt(attributes.get("gidNumber").get().toString());
+                ids.add(id);
+            }
+        }
+
+        Collections.sort(ids);
+        return ids.get(ids.size() - 1);
+    }
+
+
+
+    public static void addUser(String adminName, String adminPassword, String ldapUrl, String baseDN, String userName, String pwd, String group) throws NamingException {
+        LdapContext ctx = LdapUtils.connectLDAP(adminName, adminPassword, ldapUrl);
+        BasicAttributes basicAttributes = new BasicAttributes();
+        BasicAttribute objectClassSet = new BasicAttribute("objectclass");
+        objectClassSet.add("top");
+        objectClassSet.add("account");
+        objectClassSet.add("posixAccount");
+        objectClassSet.add("shadowAccount");
+        basicAttributes.put(objectClassSet);
+        basicAttributes.put("cn", userName);
+        basicAttributes.put("uid", userName);
+        basicAttributes.put("userPassword", pwd);
+        int userId = getMaxId(adminName, adminPassword, ldapUrl, baseDN, "user", null) + 1;
+        int groupId = getMaxId(adminName, adminPassword, ldapUrl, baseDN, "group", group);
+        LOGGER.info(">>>>>>>>>>>>>add user:groupId="+groupId+",userId="+userId);
+        baseDN = "ou=user," + baseDN;
+        basicAttributes.put("uidNumber", userId+"");
+        basicAttributes.put("gidNumber", groupId+"");
+        basicAttributes.put("homeDirectory", "home/" + userName);
+        basicAttributes.put("loginShell", "/bin/bash");
+        ctx.createSubcontext("uid=" + userName + "," + baseDN, basicAttributes);
+        LdapUtils.closeContext(ctx);
+    }
+
+
+    public static void addGroup(String adminName, String adminPassword, String ldapUrl, String baseDN, String groupName) throws NamingException {
+        LdapContext ctx = LdapUtils.connectLDAP(adminName, adminPassword, ldapUrl);
+        BasicAttributes basicAttributes = new BasicAttributes();
+        BasicAttribute objectClassSet = new BasicAttribute("objectclass");
+        objectClassSet.add("top");
+        objectClassSet.add("posixGroup");
+        basicAttributes.put(objectClassSet);
+        int groupId = getMaxId(adminName, adminPassword, ldapUrl, baseDN, "group", "*") + 1;
+        LOGGER.info(">>>>>>>>>>>>>add group:groupId="+groupId);
+        baseDN = "ou=group," + baseDN;
+        basicAttributes.put("cn", groupName);
+        basicAttributes.put("gidNumber", groupId+"");
+        basicAttributes.put("memberUid", groupName+"");
+        ctx.createSubcontext("cn=" + groupName + "," + baseDN, basicAttributes);
+        LdapUtils.closeContext(ctx);
     }
 }

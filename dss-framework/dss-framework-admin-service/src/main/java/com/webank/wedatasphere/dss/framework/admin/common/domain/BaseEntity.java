4b9ab936d82fe89e816f1be076d1b52a3b04191a
HmhWz <934178858@qq.com>
2022-02-24 14:36:21 +0800

    Fix timezone problems.

--- dss-framework/dss-framework-admin-service/src/main/java/com/webank/wedatasphere/dss/framework/admin/common/domain/BaseEntity.java
+++ dss-framework/dss-framework-admin-service/src/main/java/com/webank/wedatasphere/dss/framework/admin/common/domain/BaseEntity.java
@@ -18,14 +18,14 @@ public class BaseEntity implements Serializable
     private String createBy;
 
     /** 创建时间 */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
     /** 更新者 */
     private String updateBy;
 
     /** 更新时间 */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     /** 备注 */

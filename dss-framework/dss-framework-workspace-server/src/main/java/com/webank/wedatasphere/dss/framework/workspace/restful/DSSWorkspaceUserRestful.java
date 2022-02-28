6a112dae71c0f61badec66115137aa3056643778
HmhWz <934178858@qq.com>
2022-02-28 14:51:04 +0800

    Field "username" rename to "userName".

--- dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/restful/DSSWorkspaceUserRestful.java
+++ dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/restful/DSSWorkspaceUserRestful.java
@@ -57,27 +57,27 @@ public class DSSWorkspaceUserRestful {
     @Autowired
     private DSSWorkspaceUserService dssWorkspaceUserService;
 
-    @RequestMapping(path ="getWorkspaceUsers", method = RequestMethod.GET)
+    @RequestMapping(path = "getWorkspaceUsers", method = RequestMethod.GET)
     public Message getWorkspaceUsers(HttpServletRequest request, @RequestParam(WORKSPACE_ID_STR) String workspaceId,
                                      @RequestParam(required = false, name = "pageNow") Integer pageNow, @RequestParam(required = false, name = "pageSize") Integer pageSize,
                                      @RequestParam(required = false, name = "department") String department, @RequestParam(required = false, name = "username") String username,
-                                     @RequestParam(required = false, name = "roleName") String roleName){
+                                     @RequestParam(required = false, name = "roleName") String roleName) {
         //todo 获取工作空间中所有的用户以及他们的角色信息
-        if(pageNow == null){
+        if (pageNow == null) {
             pageNow = 1;
         }
-        if(pageSize == null){
+        if (pageSize == null) {
             //默认改成20
             pageSize = 20;
         }
-        if(StringUtils.isNotEmpty(roleName)){
+        if (StringUtils.isNotEmpty(roleName)) {
             //如果roleName不是空的话，就按照roleName来吧
             List<Long> totals = new ArrayList<>();
             List<DSSWorkspaceUserVO> workspaceUsers =
-                    dssWorkspaceService.getWorkspaceUsersByRole(Integer.parseInt(workspaceId),roleName, totals, pageNow, pageSize);
+                    dssWorkspaceService.getWorkspaceUsersByRole(Integer.parseInt(workspaceId), roleName, totals, pageNow, pageSize);
             List<DSSWorkspaceRoleVO> dssRoles = workspaceDBHelper.getRoleVOs(Integer.parseInt(workspaceId));
             return Message.ok().data("roles", dssRoles).data("workspaceUsers", workspaceUsers).data("total", totals.get(0));
-        }else{
+        } else {
             List<Long> totals = new ArrayList<>();
             List<DSSWorkspaceUserVO> workspaceUsers =
                     dssWorkspaceService.getWorkspaceUsers(workspaceId, department, username, roleName, pageNow, pageSize, totals);
@@ -86,8 +86,8 @@ public class DSSWorkspaceUserRestful {
         }
     }
 
-    @RequestMapping(path ="getAllWorkspaceUsers", method = RequestMethod.GET)
-    public Message getAllWorkspaceUsers(HttpServletRequest request, @RequestParam(WORKSPACE_ID_STR) int workspaceId ){
+    @RequestMapping(path = "getAllWorkspaceUsers", method = RequestMethod.GET)
+    public Message getAllWorkspaceUsers(HttpServletRequest request, @RequestParam(WORKSPACE_ID_STR) int workspaceId) {
         DSSWorkspaceUsersVo dssWorkspaceUsersVo = new DSSWorkspaceUsersVo();
         dssWorkspaceUsersVo.setAccessUsers(dssWorkspaceUserService.getAllWorkspaceUsers(workspaceId));
         dssWorkspaceUsersVo.setEditUsers(dssWorkspaceUserService.getWorkspaceEditUsers(workspaceId));
@@ -95,8 +95,8 @@ public class DSSWorkspaceUserRestful {
         return Message.ok().data("users", dssWorkspaceUsersVo);
     }
 
-    @RequestMapping(path ="addWorkspaceUser", method = RequestMethod.POST)
-    public Message addWorkspaceUser(HttpServletRequest request,@RequestBody UpdateWorkspaceUserRequest updateWorkspaceUserRequest){
+    @RequestMapping(path = "addWorkspaceUser", method = RequestMethod.POST)
+    public Message addWorkspaceUser(HttpServletRequest request, @RequestBody UpdateWorkspaceUserRequest updateWorkspaceUserRequest) {
         //todo 工作空间添加用户
         String creator = SecurityFilter.getLoginUsername(request);
         List<Integer> roles = updateWorkspaceUserRequest.getRoles();
@@ -110,12 +110,12 @@ public class DSSWorkspaceUserRestful {
         if (!dssWorkspaceService.isAdminUser(Long.valueOf(workspaceId), creator)) {
             return Message.error("无权限进行该操作");
         }
-        dssWorkspaceService.addWorkspaceUser(roles, workspaceId, userName, creator,userId);
+        dssWorkspaceService.addWorkspaceUser(roles, workspaceId, userName, creator, userId);
         return Message.ok();
     }
 
-    @RequestMapping(path ="updateWorkspaceUser", method = RequestMethod.POST)
-    public Message updateWorkspaceUser(HttpServletRequest request,@RequestBody UpdateWorkspaceUserRequest updateWorkspaceUserRequest){
+    @RequestMapping(path = "updateWorkspaceUser", method = RequestMethod.POST)
+    public Message updateWorkspaceUser(HttpServletRequest request, @RequestBody UpdateWorkspaceUserRequest updateWorkspaceUserRequest) {
         String creator = SecurityFilter.getLoginUsername(request);
         List<Integer> roles = updateWorkspaceUserRequest.getRoles();
         int workspaceId = updateWorkspaceUserRequest.getWorkspaceId();
@@ -127,34 +127,34 @@ public class DSSWorkspaceUserRestful {
         return Message.ok();
     }
 
-    @RequestMapping(path ="deleteWorkspaceUser", method = RequestMethod.POST)
-    public Message deleteWorkspaceUser(HttpServletRequest request,@RequestBody DeleteWorkspaceUserRequest deleteWorkspaceUserRequest){
+    @RequestMapping(path = "deleteWorkspaceUser", method = RequestMethod.POST)
+    public Message deleteWorkspaceUser(HttpServletRequest request, @RequestBody DeleteWorkspaceUserRequest deleteWorkspaceUserRequest) {
         //todo 删除工作空间中的用户
-        String userName = deleteWorkspaceUserRequest.getUsername();
+        String userName = deleteWorkspaceUserRequest.getUserName();
         int workspaceId = deleteWorkspaceUserRequest.getWorkspaceId();
         String creator = SecurityFilter.getLoginUsername(request);
-        if(!dssWorkspaceService.checkAdmin(creator)||!dssWorkspaceService.checkAdminByWorkspace(creator,workspaceId)){
+        if (!dssWorkspaceService.checkAdmin(creator) || !dssWorkspaceService.checkAdminByWorkspace(creator, workspaceId)) {
             return Message.error("无权限进行该操作");
         }
-        dssWorkspaceUserService.deleteWorkspaceUser(userName,workspaceId);
+        dssWorkspaceUserService.deleteWorkspaceUser(userName, workspaceId);
         return Message.ok();
     }
 
-    @RequestMapping(path ="listAllUsers", method = RequestMethod.GET)
-    public Message listAllUsers(HttpServletRequest request){
+    @RequestMapping(path = "listAllUsers", method = RequestMethod.GET)
+    public Message listAllUsers(HttpServletRequest request) {
         List<StaffInfoVO> dssUsers = dssWorkspaceUserService.listAllDSSUsers();
         return Message.ok().data("users", dssUsers);
     }
 
-    @RequestMapping(path ="getWorkspaceIdByUserName", method = RequestMethod.GET)
-    public Message getWorkspaceIdByUserName(HttpServletRequest request,@RequestParam(required = false, name = "userName") String userName){
+    @RequestMapping(path = "getWorkspaceIdByUserName", method = RequestMethod.GET)
+    public Message getWorkspaceIdByUserName(HttpServletRequest request, @RequestParam(required = false, name = "userName") String userName) {
         String loginUserName = SecurityFilter.getLoginUsername(request);
         String queryUserName = userName;
-        if(StringUtils.isEmpty(userName)){
+        if (StringUtils.isEmpty(userName)) {
             queryUserName = loginUserName;
         }
         List<Integer> userWorkspaceIds = dssWorkspaceUserService.getUserWorkspaceIds(queryUserName);
-        String userWorkspaceIdStr = userWorkspaceIds.stream().map(x->x.toString()).collect(Collectors.joining(","));
+        String userWorkspaceIdStr = userWorkspaceIds.stream().map(x -> x.toString()).collect(Collectors.joining(","));
         return Message.ok().data("userWorkspaceIds", userWorkspaceIdStr);
     }
 }

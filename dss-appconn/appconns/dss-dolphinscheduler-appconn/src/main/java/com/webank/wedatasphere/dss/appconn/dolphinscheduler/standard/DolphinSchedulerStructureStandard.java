80ec0dd90f7fa2cf4aef1657626f09761c7c368f
wushengyeyouya <690574002@qq.com>
2022-03-17 23:41:58 +0800

    Optimize the DolphinScheduler AppConn.

--- dss-appconn/appconns/dss-dolphinscheduler-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/dolphinscheduler/standard/DolphinSchedulerStructureStandard.java
+++ dss-appconn/appconns/dss-dolphinscheduler-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/dolphinscheduler/standard/DolphinSchedulerStructureStandard.java
@@ -1,10 +1,12 @@
 package com.webank.wedatasphere.dss.appconn.dolphinscheduler.standard;
 
+import com.webank.wedatasphere.dss.appconn.dolphinscheduler.service.DolphinSchedulerOrchestrationService;
 import com.webank.wedatasphere.dss.appconn.dolphinscheduler.service.DolphinSchedulerProjectService;
-import com.webank.wedatasphere.dss.standard.app.structure.AbstractStructureIntegrationStandard;
+import com.webank.wedatasphere.dss.appconn.scheduler.AbstractSchedulerStructureIntegrationStandard;
+import com.webank.wedatasphere.dss.appconn.scheduler.structure.orchestration.OrchestrationService;
 import com.webank.wedatasphere.dss.standard.app.structure.project.ProjectService;
 
-public class DolphinSchedulerStructureStandard extends AbstractStructureIntegrationStandard {
+public class DolphinSchedulerStructureStandard extends AbstractSchedulerStructureIntegrationStandard {
 
     private static volatile DolphinSchedulerStructureStandard instance;
 
@@ -26,5 +28,10 @@ public class DolphinSchedulerStructureStandard extends AbstractStructureIntegrat
     @Override
     protected ProjectService createProjectService() {
         return new DolphinSchedulerProjectService();
+    }
+
+    @Override
+    protected OrchestrationService createOrchestrationService() {
+        return new DolphinSchedulerOrchestrationService();
     }
 }

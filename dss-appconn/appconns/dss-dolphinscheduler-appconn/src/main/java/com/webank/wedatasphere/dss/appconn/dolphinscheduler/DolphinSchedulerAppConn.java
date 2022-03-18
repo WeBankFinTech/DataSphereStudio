0cb3527339b6b7dd52f59b33f9a5e4587e325305
wushengyeyouya <690574002@qq.com>
2022-03-18 21:14:24 +0800

    Add the support of 'OptionalAppConn' for DolphinSchedulerAppConn.

--- dss-appconn/appconns/dss-dolphinscheduler-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/dolphinscheduler/DolphinSchedulerAppConn.java
+++ dss-appconn/appconns/dss-dolphinscheduler-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/dolphinscheduler/DolphinSchedulerAppConn.java
@@ -1,12 +1,13 @@
 package com.webank.wedatasphere.dss.appconn.dolphinscheduler;
 
+import com.webank.wedatasphere.dss.appconn.core.ext.OptionalAppConn;
 import com.webank.wedatasphere.dss.appconn.dolphinscheduler.standard.DolphinSchedulerStructureStandard;
 import com.webank.wedatasphere.dss.appconn.scheduler.AbstractSchedulerAppConn;
 import com.webank.wedatasphere.dss.appconn.scheduler.SchedulerStructureIntegrationStandard;
 import com.webank.wedatasphere.dss.orchestrator.converter.standard.ConversionIntegrationStandard;
 import com.webank.wedatasphere.dss.workflow.conversion.WorkflowConversionIntegrationStandard;
 
-public class DolphinSchedulerAppConn extends AbstractSchedulerAppConn {
+public class DolphinSchedulerAppConn extends AbstractSchedulerAppConn implements OptionalAppConn {
 
     public static final String DOLPHINSCHEDULER_APPCONN_NAME = "dolphinscheduler";
 

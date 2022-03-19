b1068e482c65be0914cb4b71d106c9de4fab8de3
wushengyeyouya <690574002@qq.com>
2022-03-19 09:56:12 +0800

    resolve the compilation problems.

--- dss-appconn/appconns/dss-dolphinscheduler-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/dolphinscheduler/service/DolphinSchedulerProjectService.java
+++ dss-appconn/appconns/dss-dolphinscheduler-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/dolphinscheduler/service/DolphinSchedulerProjectService.java
@@ -37,8 +37,4 @@ public class DolphinSchedulerProjectService extends ProjectService {
         return this.getOrCreate(DolphinSchedulerProjectGrantOperation::new, DolphinSchedulerProjectGrantOperation.class);
     }
 
-    public DolphinSchedulerTokenGetOperation getDolphinSchedulerTokenGetOperation() {
-        return this.getOrCreate(DolphinSchedulerTokenGetOperation::new, DolphinSchedulerTokenGetOperation.class);
-    }
-
 }

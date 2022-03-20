8fb7aabc47991988f04e6e6ba3f1e58c37a6d320
wushengyeyouya <690574002@qq.com>
2022-03-20 20:39:54 +0800

    Optimize the architecture of DSSOrchestrator, in this case, DSS can support to integrated with multi-SchedulerAppConn when DSS is started.

--- dss-orchestrator/dss-orchestrator-loader/src/main/java/com/webank/wedatasphere/dss/orchestrator/loader/DefaultOrchestratorLoader.java
+++ dss-orchestrator/dss-orchestrator-loader/src/main/java/com/webank/wedatasphere/dss/orchestrator/loader/DefaultOrchestratorLoader.java
@@ -17,18 +17,22 @@
 package com.webank.wedatasphere.dss.orchestrator.loader;
 
 import com.webank.wedatasphere.dss.appconn.core.AppConn;
-import com.webank.wedatasphere.dss.appconn.core.exception.AppConnErrorException;
-import com.webank.wedatasphere.dss.appconn.core.ext.OnlyDevelopmentAppConn;
 import com.webank.wedatasphere.dss.appconn.manager.AppConnManager;
+import com.webank.wedatasphere.dss.appconn.scheduler.SchedulerAppConn;
 import com.webank.wedatasphere.dss.common.label.DSSLabel;
 import com.webank.wedatasphere.dss.orchestrator.core.DSSOrchestrator;
 import com.webank.wedatasphere.dss.orchestrator.core.DSSOrchestratorContext;
 import com.webank.wedatasphere.dss.orchestrator.core.impl.DefaultOrchestrator;
-import java.util.List;
+import com.webank.wedatasphere.dss.orchestrator.core.type.DSSOrchestratorRelation;
+import com.webank.wedatasphere.dss.orchestrator.core.type.DSSOrchestratorRelationManager;
+import com.webank.wedatasphere.dss.standard.common.exception.operation.ExternalOperationFailedException;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Component
@@ -45,27 +49,32 @@ public class DefaultOrchestratorLoader implements OrchestratorLoader {
     public DSSOrchestrator loadOrchestrator(String userName,
                                             String workspaceName,
                                             String typeName,
-                                            String appConnName,
                                             List<DSSLabel> dssLabels) {
 
-        //todo load DSSOrchestatror by type name
-        DSSOrchestrator dssOrchestrator = new DefaultOrchestrator() {
+        DefaultOrchestrator dssOrchestrator = new DefaultOrchestrator(typeName) {
             @Override
             protected DSSOrchestratorContext createOrchestratorContext() {
                 return dssOrchestratorContext;
             }
         };
+        DSSOrchestratorRelation relation = DSSOrchestratorRelationManager.getDSSOrchestratorRelationByName(typeName);
         // 添加实现了第三级规范的AppConn
         List<AppConn> appConnList = linkedAppConnResolver.resolveAppConnByUser(userName, workspaceName, typeName);
-        appConnList.forEach(appConn -> {
-            if(appConn instanceof OnlyDevelopmentAppConn){
-                dssOrchestrator.addLinkedAppConn(appConn);
-            }
-        });
-        LOGGER.info("Load dss orchestrator: " + typeName + ", with the binding AppConn: " + typeName);
-        AppConn appConn = AppConnManager.getAppConnManager().getAppConn(appConnName);
-        dssLabels.forEach(dssOrchestrator::addLinkedDssLabels);
+        appConnList.stream().filter(relation.isLinkedAppConn()).forEach(dssOrchestrator::addLinkedAppConn);
+        AppConn appConn = AppConnManager.getAppConnManager().getAppConn(relation.getBindingAppConnName());
         dssOrchestrator.setAppConn(appConn);
+        List<SchedulerAppConn> schedulerAppConns = AppConnManager.getAppConnManager().listAppConns(SchedulerAppConn.class);
+        SchedulerAppConn schedulerAppConn;
+        if(StringUtils.isBlank(relation.getBindingSchedulerAppConnName())) {
+            schedulerAppConn = schedulerAppConns.get(0);
+        } else {
+            schedulerAppConn = schedulerAppConns.stream().filter(appConn1 -> appConn1.getAppDesc().getAppName().equals(relation.getBindingSchedulerAppConnName()))
+                    .findAny().orElseThrow(() -> new ExternalOperationFailedException(50032, "cannot find the matched SchedulerAppConn with name " + relation.getBindingSchedulerAppConnName()));
+        }
+        dssOrchestrator.setSchedulerAppConn(schedulerAppConn);
+        LOGGER.info("Load dss orchestrator: {}, with the binding AppConn: {} and binding SchedulerAppConn {}.",
+                typeName, relation.getBindingAppConnName(), schedulerAppConn.getAppDesc().getAppName());
+        dssLabels.forEach(dssOrchestrator::addLinkedDssLabels);
         return dssOrchestrator;
     }
 }

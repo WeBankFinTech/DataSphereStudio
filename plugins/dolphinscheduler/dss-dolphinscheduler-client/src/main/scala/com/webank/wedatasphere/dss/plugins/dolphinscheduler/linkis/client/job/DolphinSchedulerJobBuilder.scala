487e27b47243a48ace81836769d7aebd91e23bfa
wushengyeyouya <690574002@qq.com>
2022-03-20 20:36:36 +0800

    Optimize the DolphinScheduler AppConn related codes, DSS try to support to integrate with DolphinScheduler.

---
+++ plugins/dolphinscheduler/dss-dolphinscheduler-client/src/main/scala/com/webank/wedatasphere/dss/plugins/dolphinscheduler/linkis/client/job/DolphinSchedulerJobBuilder.scala
@@ -0,0 +1,85 @@
+package com.webank.wedatasphere.dss.plugins.dolphinscheduler.linkis.client.job
+
+import java.util
+import java.util.{Map => JMap}
+
+import com.webank.wedatasphere.dss.linkis.node.execution.conf.LinkisJobExecutionConfiguration
+import com.webank.wedatasphere.dss.linkis.node.execution.entity.BMLResource
+import com.webank.wedatasphere.dss.linkis.node.execution.job.{Builder, CommonLinkisJob, Job, LinkisJob}
+import com.webank.wedatasphere.dss.linkis.node.execution.utils.LinkisJobExecutionUtils
+import com.webank.wedatasphere.dss.plugins.dolphinscheduler.linkis.client.conf.LinkisJobTypeConf
+import org.apache.commons.lang.StringUtils
+import org.apache.linkis.common.utils.{JsonUtils, Logging}
+
+import scala.collection.JavaConverters._
+
+class DolphinSchedulerJobBuilder(jobProps: JMap[String, String]) extends Builder with Logging {
+
+  override protected def getJobType: String = jobProps.get(LinkisJobTypeConf.LINKIS_TYPE)
+
+  override protected def creatLinkisJob(isLinkisType: Boolean): LinkisJob = {
+    val job = if (isLinkisType) new DolphinSchedulerCommonLinkisJob else new DolphinSchedulerAppConnLinkisJob
+    job.setJobProps(jobProps)
+    job
+  }
+
+  override protected def fillJobInfo(job: Job): Unit = {
+    job.setCode(jobProps.get(LinkisJobTypeConf.JOB_COMMAND))
+    job.setParams(new java.util.HashMap[String, Object]())
+    job.setRuntimeParams(new java.util.HashMap[String, Object])
+    job.setJobProps(jobProps)
+  }
+
+  override protected def fillLinkisJobInfo(linkisJob: LinkisJob): Unit = {
+    val jobParams = jobProps.get(LinkisJobTypeConf.JOB_PARAMS)
+    val paramsMap = JsonUtils.jackson.readValue(jobParams, classOf[util.Map[String, Object]])
+    linkisJob.setConfiguration(getConfiguration(paramsMap))
+    linkisJob.setVariables(getVariables(paramsMap))
+    linkisJob.getVariables.put("run_date", jobProps.get(LinkisJobTypeConf.RUN_DATE))
+    val source = getSource
+    linkisJob.setSource(source)
+    linkisJob.getRuntimeParams.put("nodeName", source.get("nodeName"))
+    if(!linkisJob.getRuntimeParams.containsKey("contextID")) {
+      linkisJob.getRuntimeParams.put("contextID", LinkisJobExecutionConfiguration.FLOW_CONTEXTID)
+    }
+  }
+
+  override protected def fillCommonLinkisJobInfo(commonLinkisJob: CommonLinkisJob): Unit = {
+    commonLinkisJob.setJobResourceList(LinkisJobExecutionUtils.getResourceListByJson(jobProps.get(LinkisJobTypeConf.JOB_RESOURCES)))
+    val flowNameAndResources = new util.HashMap[String, util.List[BMLResource]]
+    if(jobProps.containsKey(LinkisJobTypeConf.FLOW_RESOURCES)) {
+      flowNameAndResources.put(getSource.get("flowName") + LinkisJobExecutionConfiguration.RESOURCES_NAME,
+        LinkisJobExecutionUtils.getResourceListByJson(jobProps.get(LinkisJobTypeConf.FLOW_RESOURCES)))
+    }
+    commonLinkisJob.setFlowNameAndResources(flowNameAndResources)
+  }
+
+  private def getSource: JMap[String, String] = {
+    val jobSource = jobProps.get(LinkisJobTypeConf.JOB_SOURCE)
+    if(StringUtils.isNotBlank(jobSource)) {
+      JsonUtils.jackson.readValue(jobSource, classOf[util.Map[String, String]])
+    } else new util.HashMap[String, String]
+  }
+
+    private def getVariables(params: util.Map[String, Object]): util.Map[String, Object] = {
+      val variable = params.get("variable") match {
+        case variables: util.Map[String, Object] =>
+          variables
+        case _ => new util.HashMap[String, AnyRef]
+      }
+      val flowProperties = jobProps.get(LinkisJobTypeConf.FLOW_PROPERTIES)
+      if(StringUtils.isNotBlank(flowProperties))
+        JsonUtils.jackson.readValue(flowProperties, classOf[util.List[util.Map[String, Object]]])
+          .asScala.foreach(variable.putAll(_))
+      variable
+    }
+
+    private def getConfiguration(params: util.Map[String, Object]): util.Map[String, Object] = {
+      params.get("configuration") match {
+        case configuration: util.Map[String, Object] =>
+          configuration
+        case _ => new util.HashMap[String, AnyRef]
+      }
+    }
+
+}

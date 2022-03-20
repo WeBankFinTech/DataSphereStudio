bd6fe8341e538ff2666c8e91c5662b749db24d9c
wushengyeyouya <690574002@qq.com>
2022-03-20 20:41:39 +0800

    Optimize the codes of DSS-workflow to support to integrated with multi-SchedulerAppConn when DSS is started.

--- dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/service/impl/PublishServiceImpl.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/service/impl/PublishServiceImpl.java
@@ -26,14 +26,33 @@ import com.webank.wedatasphere.dss.orchestrator.common.protocol.ResponseConvertO
 import com.webank.wedatasphere.dss.sender.service.DSSSenderServiceFactory;
 import com.webank.wedatasphere.dss.standard.app.sso.Workspace;
 import com.webank.wedatasphere.dss.standard.common.desc.AppInstance;
+import com.webank.wedatasphere.dss.workflow.common.entity.DSSFlow;
+import com.webank.wedatasphere.dss.workflow.common.parser.WorkFlowParser;
+import com.webank.wedatasphere.dss.workflow.constant.DSSWorkFlowConstant;
+import com.webank.wedatasphere.dss.workflow.service.DSSFlowService;
 import com.webank.wedatasphere.dss.workflow.service.PublishService;
+import org.apache.commons.lang.StringUtils;
 import org.apache.linkis.rpc.Sender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
+import static com.webank.wedatasphere.dss.workflow.constant.DSSWorkFlowConstant.DEFAULT_SCHEDULER_APP_CONN;
+
 public class PublishServiceImpl implements PublishService {
+
+    private DSSFlowService dssFlowService;
+    private WorkFlowParser workFlowParser;
+
+    public void setDssFlowService(DSSFlowService dssFlowService) {
+        this.dssFlowService = dssFlowService;
+    }
+
+    public void setWorkFlowParser(WorkFlowParser workFlowParser) {
+        this.workFlowParser = workFlowParser;
+    }
+
     protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     protected Sender getOrchestratorSender() {
@@ -43,25 +62,39 @@ public class PublishServiceImpl implements PublishService {
     @Override
     public String submitPublish(String convertUser, Long workflowId,
         Map<String, Object> dssLabel, Workspace workspace, String comment) throws Exception {
-        LOGGER.info("User {} begins to convert workflow {}", convertUser, workflowId);
+        LOGGER.info("User {} begins to convert workflow {}.", convertUser, workflowId);
         //1 获取对应的orcId 和 orcVersionId
         //2.进行提交
+        RequestFrameworkConvertOrchestration requestFrameworkConvertOrchestration = new RequestFrameworkConvertOrchestration();
+        requestFrameworkConvertOrchestration.setComment(comment);
+        requestFrameworkConvertOrchestration.setOrcAppId(workflowId);
+        requestFrameworkConvertOrchestration.setUserName(convertUser);
+        requestFrameworkConvertOrchestration.setWorkspace(workspace);
+        DSSFlow dssFlow = null;
         try {
-            RequestFrameworkConvertOrchestration requestFrameworkConvertOrchestration = new RequestFrameworkConvertOrchestration();
-            requestFrameworkConvertOrchestration.setComment(comment);
-            requestFrameworkConvertOrchestration.setOrcAppId(workflowId);
-            requestFrameworkConvertOrchestration.setUserName(convertUser);
-            requestFrameworkConvertOrchestration.setWorkspace(workspace);
-            SchedulerAppConn schedulerAppConn = AppConnManager.getAppConnManager().getAppConn(SchedulerAppConn.class);
+            dssFlow = dssFlowService.getFlow(workflowId);
+            if(dssFlow == null) {
+                DSSExceptionUtils.dealErrorException(63325, "workflow " + workflowId + " is not exists.", DSSErrorException.class);
+                return null;
+            }
+            String schedulerAppConnName = workFlowParser.getValueWithKey(dssFlow.getFlowJson(), DSSWorkFlowConstant.SCHEDULER_APP_CONN_NAME);
+            if(StringUtils.isBlank(schedulerAppConnName)) {
+                // 向下兼容老版本
+                schedulerAppConnName = DEFAULT_SCHEDULER_APP_CONN.getValue();
+            }
+            SchedulerAppConn schedulerAppConn = (SchedulerAppConn) AppConnManager.getAppConnManager().getAppConn(schedulerAppConnName);
             // 只是为了获取是否需要发布所有Orc，这里直接拿第一个AppInstance即可。
             AppInstance appInstance = schedulerAppConn.getAppDesc().getAppInstances().get(0);
             requestFrameworkConvertOrchestration.setConvertAllOrcs(schedulerAppConn.getOrCreateConversionStandard().getDSSToRelConversionService(appInstance).isConvertAllOrcs());
             requestFrameworkConvertOrchestration.setLabels(dssLabel);
             ResponseConvertOrchestrator response = (ResponseConvertOrchestrator) getOrchestratorSender().ask(requestFrameworkConvertOrchestration);
             return response.getId();
+        } catch (DSSErrorException e) {
+            throw e;
         } catch (final Exception t) {
-            LOGGER.error("Failed to submit publish {}.", workflowId, t);
-            DSSExceptionUtils.dealErrorException(63325, "Failed to submit publish " + workflowId, t, DSSErrorException.class);
+            Object str = dssFlow == null ? workflowId : dssFlow.getName();
+            LOGGER.error("Failed to submit publish {}.", str, t);
+            DSSExceptionUtils.dealErrorException(63325, "Failed to submit publish " + str, t, DSSErrorException.class);
         }
         return null;
     }

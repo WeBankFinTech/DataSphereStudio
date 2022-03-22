adcd11cfa6151a3af54181333481e560ab841d14
wushengyeyouya <690574002@qq.com>
2022-03-22 10:52:51 +0800

    Optimize the url generation of DolphinScheduler.

--- dss-appconn/appconns/dss-dolphinscheduler-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/dolphinscheduler/conf/DolphinSchedulerConf.java
+++ dss-appconn/appconns/dss-dolphinscheduler-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/dolphinscheduler/conf/DolphinSchedulerConf.java
@@ -7,7 +7,7 @@ public interface DolphinSchedulerConf {
 
     CommonVars<String> DS_ADMIN_USER = CommonVars.apply("wds.dss.appconn.ds.admin.user", "admin");
 
-    CommonVars<String> DS_ADMIN_TOKEN = CommonVars.apply("wds.dss.appconn.ds.admin.token", "fcdd944c03d5792719781f2c6e7b7542");
+    CommonVars<String> DS_ADMIN_TOKEN = CommonVars.apply("wds.dss.appconn.ds.admin.token", "");
 
     CommonVars<TimeType> DS_TOKEN_EXPIRE_TIME = CommonVars.apply("wds.dss.appconn.ds.token.expire.time", new TimeType("2h"));
 

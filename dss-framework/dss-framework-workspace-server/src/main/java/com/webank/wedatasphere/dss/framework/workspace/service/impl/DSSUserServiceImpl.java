2c0ffdbe2eb8bb013a19093f105da22f25eacf4d
wushengyeyouya <690574002@qq.com>
2022-03-29 11:07:48 +0800

    Optimize the workspace interfaces and db tables.

--- dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/impl/DSSUserServiceImpl.java
+++ dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/impl/DSSUserServiceImpl.java
@@ -26,7 +26,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 

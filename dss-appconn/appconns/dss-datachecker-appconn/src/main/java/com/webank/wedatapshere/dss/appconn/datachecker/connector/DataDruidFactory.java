642b249b87a051adbaa68551aca8963ddf7c7727
ElishaZhang <1049909975@qq.com>
2022-04-09 13:05:18 +0800

    [fix bugs] Fixed the dataccherker logs not found problem

--- dss-appconn/appconns/dss-datachecker-appconn/src/main/java/com/webank/wedatapshere/dss/appconn/datachecker/connector/DataDruidFactory.java
+++ dss-appconn/appconns/dss-datachecker-appconn/src/main/java/com/webank/wedatapshere/dss/appconn/datachecker/connector/DataDruidFactory.java
@@ -18,7 +18,7 @@ package com.webank.wedatapshere.dss.appconn.datachecker.connector;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
 
 import java.util.Base64;
 import java.util.Properties;

6932be0c26efc135061eaa6b6f278f3170ddbd14
HmhWz <934178858@qq.com>
2022-04-18 12:40:43 +0800

    Add getOrCreateConversionStandard method for SchedulisAppConn.

--- dss-appconn/appconns/dss-schedulis-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/schedulis/SchedulisAppConn.java
+++ dss-appconn/appconns/dss-schedulis-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/schedulis/SchedulisAppConn.java
@@ -19,10 +19,16 @@ package com.webank.wedatasphere.dss.appconn.schedulis;
 import com.webank.wedatasphere.dss.appconn.scheduler.AbstractSchedulerAppConn;
 import com.webank.wedatasphere.dss.appconn.scheduler.SchedulerStructureIntegrationStandard;
 import com.webank.wedatasphere.dss.appconn.schedulis.standard.SchedulisStructureStandard;
+import com.webank.wedatasphere.dss.orchestrator.converter.standard.ConversionIntegrationStandard;
+import com.webank.wedatasphere.dss.workflow.conversion.ProjectConversionIntegrationStandard;
 
 public class SchedulisAppConn extends AbstractSchedulerAppConn {
 
     public static final String SCHEDULIS_APPCONN_NAME = "Schedulis";
+
+    public ConversionIntegrationStandard getOrCreateConversionStandard() {
+        return super.getOrCreateConversionStandard();
+    }
 
     @Override
     public SchedulerStructureIntegrationStandard getOrCreateStructureStandard() {

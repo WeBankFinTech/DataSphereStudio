59406b2d0c4a976f5c6dc53176b5e46a2d051519
HmhWz <934178858@qq.com>
2022-04-23 14:43:26 +0800

    Bugs fixed.

--- dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/io/input/impl/NodeInputServiceImpl.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/io/input/impl/NodeInputServiceImpl.java
@@ -142,7 +142,7 @@ public class NodeInputServiceImpl implements NodeInputService {
                 return BDPJettyServerHelper.jacksonJson().writeValueAsString(nodeJsonMap);
             }
         } else {
-            logger.error("appConn node resource file does not exists." + nodeId);
+            logger.error("appConn node resource file does not exists. nodeId: {}" + nodeId);
         }
 
 

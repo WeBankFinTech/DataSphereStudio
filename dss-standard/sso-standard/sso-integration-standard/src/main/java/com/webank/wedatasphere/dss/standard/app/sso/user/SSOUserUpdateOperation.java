93ed5eb10831f8a64dfc6358e3693df2dc9332a4
wushengyeyouya <690574002@qq.com>
2022-04-26 21:48:13 +0800

    Add user sync Operation for OnlySSOStandard.

---
+++ dss-standard/sso-standard/sso-integration-standard/src/main/java/com/webank/wedatasphere/dss/standard/app/sso/user/SSOUserUpdateOperation.java
@@ -0,0 +1,17 @@
+package com.webank.wedatasphere.dss.standard.app.sso.user;
+
+import com.webank.wedatasphere.dss.standard.app.sso.user.ref.DSSUserContentRequestRef;
+import com.webank.wedatasphere.dss.standard.common.entity.ref.ResponseRef;
+import com.webank.wedatasphere.dss.standard.common.exception.operation.ExternalOperationFailedException;
+
+/**
+ * @author enjoyyin
+ * @date 2022-04-25
+ * @since 0.5.0
+ */
+public abstract class SSOUserUpdateOperation<R extends DSSUserContentRequestRef<R>>
+        extends SSOUserOperation<R, ResponseRef> {
+
+    public abstract ResponseRef updateUser(R requestRef) throws ExternalOperationFailedException;
+
+}

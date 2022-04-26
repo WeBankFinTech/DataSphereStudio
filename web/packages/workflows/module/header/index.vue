330bb54d8be65e4e9f84a381069f6abd6ee8a953
ryanlei <ryanlei@webank.com>
2022-04-26 14:17:10 +0800

    1.1.3 refactor web

---
+++ web/packages/workflows/module/header/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <div class="layout-header">
+      <div
+        class="layout-header-menu-icon"
+      >
+        <div class="logo">
+          <img
+            class="logo-img"
+            src="../../assets/images/dssLogo.png"
+            :alt="$APP_CONF.app_name"
+          >
+          <span class="version">{{sysVersion}}</span>
+        </div>
+      </div>
+
+      <div
+        v-clickoutside="handleOutsideClick"
+        :class="{'selected': isUserMenuShow}"
+        class="user"
+        @click="handleUserClick"
+      >
+        <span>{{ userName }}</span>
+        <Icon v-show="!isUserMenuShow" type="ios-arrow-down" class="user-icon"/>
+        <Icon v-show="isUserMenuShow" type="ios-arrow-up" class="user-icon"/>
+        <userMenu v-show="isUserMenuShow" @clear-session="clearSession"/>
+      </div>
+      <ul class="menu">
+        <li class="menu-item"  @click="goRolesPath">{{moduleString}}</li>
+      </ul>
+      <div class="icon-group">
+        <Icon
+          v-if="isSandbox"
+          :title="$t('message.common.home')"
+          class="book"
+          type="ios-chatboxes"
+          @click="linkTo('freedback')"
+        ></Icon>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import userMenu from "./userMenu.vue";
+import clickoutside from '@dataspherestudio/shared/common/helper/clickoutside';
+import mixin from '@dataspherestudio/shared/common/service/mixin';
+import util from '@dataspherestudio/shared/common/util';
+
+export default {
+  directives: {
+    clickoutside
+  },
+  components: {
+    userMenu,
+  },
+  data() {
+    return {
+      // 根据接口返回渲染快捷入口
+      homeRoles: null,
+      sysVersion: process.env.VUE_APP_VERSION,
+      isUserMenuShow: false,
+      userName: "",
+      isSandbox: process.env.NODE_ENV === "sandbox",
+    };
+  },
+  mixins: [mixin],
+  created() {
+  },
+  mounted() {
+    this.userName = this.getUserName();
+  },
+  computed: {
+    moduleString() {
+      const currentModules = util.currentModules();
+      return currentModules.microModule ? 'process' : 'workflow'
+    }
+  },
+  methods: {
+    handleOutsideClick() {
+      this.isUserMenuShow = false;
+    },
+    handleUserClick() {
+      this.isUserMenuShow = !this.isUserMenuShow;
+    },
+    clearSession() {
+      this.$emit("clear-session");
+    },
+    linkTo(type) {
+      let url = "";
+      if (type === "book") {
+        url = `https://github.com/WeBankFinTech/DataSphereStudio/blob/master/docs/zh_CN/ch3/DSS_User_Manual.md`;
+      } else if (type === "github") {
+        url = `https://github.com/WeBankFinTech/DataSphereStudio`;
+      } else if (type === "freedback") {
+        url = "https://wj.qq.com/s2/4943071/c037/ ";
+        if (localStorage.getItem("locale") === "en") {
+          url = "https://wj.qq.com/s2/4943706/5a8b";
+        }
+      }
+      util.windowOpen(url);
+    },
+    goRolesPath() {
+      this.$router.push({path: `/${this.moduleString}`, query: Object.assign({}, this.$route.query)});
+    },
+  }
+};
+</script>
+<style lang="scss" scoped src="./index.scss"></style>
---
+++ web/packages/workspace/router.js
@@ -0,0 +1,134 @@
+export const apiServicesRoutes = [
+  {
+    path: 'newhome',
+    name: 'newhome',
+    meta: {
+      publicPage: true,
+      admin: true
+    },
+    component: () => import('./view/home/index.vue'),
+  },
+  {
+    path: '/workspaceManagement',
+    name: 'workspaceManagement',
+    redirect: '/workspaceManagement/productsettings',
+    meta: {
+      publicPage: true
+    },
+    component: () => import('./view/management/index.vue'),
+    children: [{
+      path: 'productsettings',
+      name: 'productsettings',
+      meta: {
+        publicPage: true
+      },
+      component: () =>
+        import('./module/management/module/productsettings.vue'),
+    },
+    {
+      path: 'usertable',
+      name: 'usertable',
+      meta: {
+        publicPage: true
+      },
+      component: () =>
+        import('./module/management/module/usertable.vue'),
+    },
+    {
+      path: 'jurisdiction',
+      name: 'jurisdiction',
+      meta: {
+        publicPage: true
+      },
+      component: () =>
+        import('./module/management/module/jurisdiction.vue'),
+    },
+    {
+      path: 'dataSourceAdministration',
+      name: 'dataSourceAdministration',
+      meta: {
+        publicPage: true
+      },
+      component: () =>
+        import('./module/management/module/dataSourceAdministration.vue'),
+    }]
+  },
+  {
+    path: 'permissions',
+    name: 'Permissions',
+    meta: {
+      publicPage: true,
+      admin: true
+    },
+    component: () => import('./view/permissions/index.vue'),
+  },
+  {
+    path: 'managementPlatform',
+    name: 'ManagementPlatform',
+    meta: {
+      publicPage: true,
+    },
+    redirect: '/managementPlatform/departManagement',
+    component: () => import('./view/managementPlatform/index.vue'),
+    children: [
+      {
+        path: 'departManagement',
+        name: 'departManagement',
+        meta: {
+          title: 'departManagement',
+          publicPage: true
+        },
+        component: () => import('./module/permissions/module/departManagement.vue')
+      },
+      {
+        path: 'personManagement',
+        name: 'personManagement',
+        meta: {
+          title: 'personManagement',
+          publicPage: true
+        },
+        component: () => import('./module/permissions/module/personManagement.vue')
+      },
+      {
+        path: 'accessComponents',
+        name: 'accessComponents',
+        meta: {
+          title: 'Access Components',
+          publicPage: true
+        },
+        component: () => import('./module/managementPlatform/component/accessComponent/index.vue')
+      },
+      {
+        path: 'guide',
+        name: 'guide',
+        meta: {
+          title: 'Guide',
+          publicPage: true
+        },
+        component: () => import('./module/managementPlatform/component/guide/index.vue')
+      },
+      {
+        path: 'library',
+        name: 'library',
+        meta: {
+          title: 'Library',
+          publicPage: true
+        },
+        component: () => import('./module/managementPlatform/component/library/index.vue')
+      }
+    ]
+  }
+];
+
+const routes = apiServicesRoutes.concat([{
+  path: '/workspaceHome',
+  name: 'workspaceHome',
+  meta: {
+    title: 'workspaceHome',
+    publicPage: true,
+  },
+  component: () => import('./view/workspaceHome/index.vue'),
+}])
+
+export default routes
+

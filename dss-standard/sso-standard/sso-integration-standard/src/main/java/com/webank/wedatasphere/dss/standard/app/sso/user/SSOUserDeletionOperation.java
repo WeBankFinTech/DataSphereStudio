49ebff5a58d2b4c13dce579388c1f0a20c0a5b5b
wushengyeyouya <690574002@qq.com>
2022-04-26 22:26:29 +0800

    Add user sync Operation for OnlySSOStandard.

--- dss-standard/sso-standard/sso-integration-standard/src/main/java/com/webank/wedatasphere/dss/standard/app/sso/user/SSOUserDeletionOperation.java
+++ dss-standard/sso-standard/sso-integration-standard/src/main/java/com/webank/wedatasphere/dss/standard/app/sso/user/SSOUserDeletionOperation.java
@@ -7,11 +7,18 @@ import com.webank.wedatasphere.dss.standard.common.exception.operation.ExternalO
 /**
  * @author enjoyyin
  * @date 2022-04-25
- * @since 0.5.0
+ * @since 1.1.0
  */
 public abstract class SSOUserDeletionOperation<R extends DSSUserContentRequestRef<R>>
         extends SSOUserOperation<R, ResponseRef> {
 
+    /**
+     * 该方法暂时尚未被使用。
+     * 删除用户操作，当 DSS Admin 模块删除一个用户时，同步请求第三方 AppConn 删除一个用户。
+     * @param requestRef
+     * @return 如果删除成功，请返回 ResponseRef.newBuilder().success()
+     * @throws ExternalOperationFailedException 删除失败时抛出该异常
+     */
     public abstract ResponseRef deleteUser(R requestRef) throws ExternalOperationFailedException;
 
 }

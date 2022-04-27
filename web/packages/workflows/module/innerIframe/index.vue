5478504ead2450cbcc83b4e5d91b24ddb1bd8b24
ryanlei <ryanlei@webank.com>
2022-04-27 17:42:35 +0800

    fix url

--- web/packages/workflows/module/innerIframe/index.vue
+++ web/packages/workflows/module/innerIframe/index.vue
@@ -24,7 +24,8 @@ export default {
     let applicationItem = {}
     applications.forEach(item => {
       if (item.appconns) {
-        applicationItem = item.appconns.find(app => app.name == 'realTimeJobCenter') || {}
+        const appItem = item.appconns.find(app => app.name == 'realTimeJobCenter')
+        if (appItem) applicationItem = appItem
       }
     })
     this.url = applicationItem.appInstance && applicationItem.appInstance[0] && applicationItem.appInstance[0].homepageUri;

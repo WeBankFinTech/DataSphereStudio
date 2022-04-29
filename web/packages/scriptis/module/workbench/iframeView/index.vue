9f8107086931ba9cd5e1806c68cd1f5c862e611e
ryanlei <ryanlei@webank.com>
2022-04-29 10:37:12 +0800

    support view db/table detail in iframe

---
+++ web/packages/scriptis/module/workbench/iframeView/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="iframeClass">
+    <iframe class="iframeClass" :src="url" frameborder="0" width="100%" height="100%" />
+  </div>
+</template>
+<script>
+
+export default {
+  props: {
+    url: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+  },
+  mounted() {
+  },
+  methods: {
+  },
+  beforeDestroy() {
+  }
+};
+
+</script>
+<style lang="scss" scoped>
+  .iframeClass {
+    position: relative;
+    height: 100%;
+    width: 100%;
+  }
+
+</style>

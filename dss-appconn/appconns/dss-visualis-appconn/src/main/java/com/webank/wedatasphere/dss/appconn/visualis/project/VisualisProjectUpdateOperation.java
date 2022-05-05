01f2633ee2b29a77cbedc3b269d54fc6322039cf
jinyangrao <jirao4753@qq.com>
2022-05-05 11:54:55 +0800

    Implement the project update interface of visualis

--- dss-appconn/appconns/dss-visualis-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/visualis/project/VisualisProjectUpdateOperation.java
+++ dss-appconn/appconns/dss-visualis-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/visualis/project/VisualisProjectUpdateOperation.java
@@ -5,27 +5,26 @@ import com.webank.wedatasphere.dss.appconn.visualis.utils.URLUtils;
 import com.webank.wedatasphere.dss.appconn.visualis.utils.VisualisCommonUtil;
 import com.webank.wedatasphere.dss.common.label.EnvDSSLabel;
 import com.webank.wedatasphere.dss.common.label.LabelRouteVO;
-import com.webank.wedatasphere.dss.standard.app.sso.origin.request.action.DSSPostAction;
+import com.webank.wedatasphere.dss.standard.app.sso.origin.request.action.DSSPutAction;
 import com.webank.wedatasphere.dss.standard.app.structure.AbstractStructureOperation;
-import com.webank.wedatasphere.dss.standard.app.structure.project.ProjectDeletionOperation;
+import com.webank.wedatasphere.dss.standard.app.structure.project.ProjectUpdateOperation;
 import com.webank.wedatasphere.dss.standard.app.structure.project.ref.ProjectResponseRef;
 import com.webank.wedatasphere.dss.standard.app.structure.project.ref.RefProjectContentRequestRef;
 import com.webank.wedatasphere.dss.standard.common.entity.ref.ResponseRef;
 import com.webank.wedatasphere.dss.standard.common.exception.operation.ExternalOperationFailedException;
 
-public class VisualisProjectDeletionOperation extends AbstractStructureOperation<RefProjectContentRequestRef.RefProjectContentRequestRefImpl, ProjectResponseRef>
-        implements ProjectDeletionOperation<RefProjectContentRequestRef.RefProjectContentRequestRefImpl> {
-
+public class VisualisProjectUpdateOperation extends AbstractStructureOperation<RefProjectContentRequestRef.RefProjectContentRequestRefImpl, ProjectResponseRef>
+        implements ProjectUpdateOperation<RefProjectContentRequestRef.RefProjectContentRequestRefImpl> {
     @Override
     protected String getAppConnName() {
         return VisualisAppConn.VISUALIS_APPCONN_NAME;
     }
 
     @Override
-    public ResponseRef deleteProject(RefProjectContentRequestRef.RefProjectContentRequestRefImpl projectRef)
-            throws ExternalOperationFailedException {
+    public ResponseRef updateProject(RefProjectContentRequestRef.RefProjectContentRequestRefImpl projectRef) throws ExternalOperationFailedException {
         String url = getBaseUrl() + URLUtils.PROJECT_DELETE_UPDATE_URL;
-        DSSPostAction deleteAction = new DSSPostAction();
+        DSSPutAction deleteAction = new DSSPutAction();
+        deleteAction.
         LabelRouteVO routeVO = new LabelRouteVO();
         routeVO.setRoute(((EnvDSSLabel) (projectRef.getDSSLabels().get(0))).getEnv());
         deleteAction.addRequestPayload("labels", routeVO);

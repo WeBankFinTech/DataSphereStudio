4d27215e95c96b19076154c70187460f3041bbb1
yuankang134 <373346037@qq.com>
2022-05-06 16:09:36 +0800

    add learning guide function

--- dss-apps/dss-user-guide/dss-user-guide-server/src/main/java/com/webank/wedatasphere/dss/guide/server/service/impl/GuideCatalogServiceImpl.java
+++ dss-apps/dss-user-guide/dss-user-guide-server/src/main/java/com/webank/wedatasphere/dss/guide/server/service/impl/GuideCatalogServiceImpl.java
@@ -9,7 +9,7 @@ import com.webank.wedatasphere.dss.guide.server.entity.GuideChapter;
 import com.webank.wedatasphere.dss.guide.server.entity.response.GuideCatalogDetail;
 import com.webank.wedatasphere.dss.guide.server.service.GuideCatalogService;
 import com.webank.wedatasphere.dss.guide.server.util.GuideException;
-import com.webank.wedatasphere.dss.guide.server.util.ReadMdFile;
+import com.webank.wedatasphere.dss.guide.server.util.MdAnalysis;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -80,20 +80,20 @@ public class GuideCatalogServiceImpl extends ServiceImpl<GuideCatalogMapper, Gui
         List<GuideChapter> chapters = null;
         try {
             //1.解析SUMMARY.md文件
-            List<Map<String, Map<String, String>>> maps = ReadMdFile.analysisMd(summaryPath);
+            List<Map<String, Map<String, String>>> maps = MdAnalysis.analysisMd(summaryPath,"knowledge");
             //2.解析maps并插入dss_guide_catalog表
             catalogs = new ArrayList<>();
             chapters = new ArrayList<>();
             for (Map<String, Map<String, String>> map : maps) {
                 for (Map.Entry tag : map.entrySet()) {
                     //可以转为int类型则为一级标题
-                    if (isStr2Num(String.valueOf(tag.getKey()))) {
+                    if (MdAnalysis.isStr2Num(String.valueOf(tag.getKey()))) {
                         //参数组装
                         catalogs.add(buildParams(map, tag, Long.valueOf(tag.getKey().toString()), -1l));
                     }
 
                     //解析二级标题
-                    if (isSecondOrThird(String.valueOf(tag.getKey())) == 1) {
+                    if (MdAnalysis.isSecondOrThird(String.valueOf(tag.getKey())) == 1) {
                         //[1-1, 1-2, 1-3, 1-4, 1-5, 1-6, 1-7, 1-8, 1-9, 1-10, 2-1, 2-2, 3-1, 3-2, 3-3]
                         String secondTag = String.valueOf(tag.getKey());
                         //将split[1]作为id,split[0]作为parentId
@@ -103,12 +103,12 @@ public class GuideCatalogServiceImpl extends ServiceImpl<GuideCatalogMapper, Gui
                             String mdFilePath = parentPath + map.get(tag.getKey()).get("file");
                             chapters.add(buildChapterParams(map, tag, Long.valueOf(split[0]), mdFilePath));
                         } else {
-                            catalogs.add(buildParams(map, tag, Long.valueOf(split[1]) + ReadMdFile.getRootCount(), Long.valueOf(split[0])));
+                            catalogs.add(buildParams(map, tag, Long.valueOf(split[1]) + MdAnalysis.getRootCount(), Long.valueOf(split[0])));
                         }
                     }
 
                     //解析三级标题
-                    if (isSecondOrThird(String.valueOf(tag.getKey())) == 2) {
+                    if (MdAnalysis.isSecondOrThird(String.valueOf(tag.getKey())) == 2) {
                         //[1-1-1, 1-1-2, 1-1-3, 1-1-4, 1-2-1, 1-2-2, 1-2-3, 1-2-4, 1-2-5, 1-2-6, 1-2-7, 1-2-8, 1-2-9, 1-2-10, 1-2-11, 1-2-12, 1-2-13, 1-2-14, 1-2-15, 1-2-16, 1-2-17, 1-2-18, 1-2-19, 1-2-20, 1-2-21, 1-2-22, 1-2-23, 1-2-24, 1-2-25, 1-2-26, 1-3-1, 1-4-1, 1-4-2, 1-4-3, 1-4-4, 1-4-5, 1-4-6, 1-4-7, 1-4-8, 1-4-9, 1-4-10, 1-4-11, 1-4-12, 1-4-13, 1-4-14, 1-4-15, 1-4-16, 1-4-17, 1-4-18, 1-5-1, 1-5-2, 1-5-3, 1-5-4, 1-5-5, 1-5-6, 1-5-7, 1-5-8, 1-5-9, 1-5-10, 1-5-11, 1-5-12, 1-5-13, 1-5-14, 1-5-15, 1-5-16, 1-6-1, 1-6-2, 1-6-3, 1-6-4, 1-6-5, 1-6-6, 1-7-1, 1-7-2, 1-7-3, 1-7-4, 1-7-5, 1-7-6, 1-7-7, 1-7-8, 1-7-9, 1-7-10, 1-7-11, 1-7-12, 1-7-13, 1-7-14, 1-8-1, 1-8-2, 1-8-3, 1-8-4, 1-8-5, 1-9-1, 1-9-2, 1-9-3]
                         String secondTag = String.valueOf(tag.getKey());
                         //split[1]作为catalogId
@@ -116,7 +116,7 @@ public class GuideCatalogServiceImpl extends ServiceImpl<GuideCatalogMapper, Gui
                         //获取summary.md文件中的md文件
                         String mdFilePath = parentPath + map.get(tag.getKey()).get("file");
                         //组装参数存入dss_guide_chapter表
-                        chapters.add(buildChapterParams(map, tag, Long.valueOf(split[1]) + ReadMdFile.getRootCount(), mdFilePath));
+                        chapters.add(buildChapterParams(map, tag, Long.valueOf(split[1]) + MdAnalysis.getRootCount(), mdFilePath));
                     }
                 }
             }
@@ -131,7 +131,7 @@ public class GuideCatalogServiceImpl extends ServiceImpl<GuideCatalogMapper, Gui
             logger.info("=========开始批量插入dss_guide_catalog表============");
             int flag = guideCatalogMapper.batchInsert(catalogs);
             if(flag == 0){
-                logger.info("=========开始批量插入dss_guide_catalog表============");
+                logger.info("=========批量插入失败dss_guide_catalog表============");
                 throw new GuideException("批量插入失败！");
             }
         }
@@ -141,43 +141,11 @@ public class GuideCatalogServiceImpl extends ServiceImpl<GuideCatalogMapper, Gui
             logger.info("=========开始批量插入dss_guide_chapter表============");
             int flag = guideChapterMapper.batchInsert(chapters);
             if(flag == 0){
-                logger.info("=========开始批量插入dss_guide_chapter表============");
+                logger.info("=========批量插入失败dss_guide_chapter表============");
                 throw new GuideException("批量插入失败！");
             }
         }
-        ReadMdFile.rootCountInit();
-    }
-
-    /**
-     * 解析一级标题
-     *
-     * @param str
-     * @return
-     */
-    private boolean isStr2Num(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    /**
-     * 解析二级三级标题
-     *
-     * @param str
-     * @return
-     */
-    private int isSecondOrThird(String str) {
-        int count = 0;
-        for (int i = 0; i < str.length(); i++) {
-            int i1 = str.indexOf("-", i);
-            if (i1 == i) {
-                count++;
-            }
-        }
-        return count;
+        logger.info("知识库同步完成。。。");
     }
 
     private GuideCatalog buildParams(Map<String, Map<String, String>> map, Map.Entry tag, Long id, Long parentId) {
@@ -196,9 +164,9 @@ public class GuideCatalogServiceImpl extends ServiceImpl<GuideCatalogMapper, Gui
         guideChapter.setTitle(map.get(tag.getKey()).get("title"));
         if (!StringUtils.isEmpty(mdFilePath)) {
             logger.info("GuideChapter文件路径："+ mdFilePath);
-            String content = ReadMdFile.readMd(mdFilePath);
+            String content = MdAnalysis.readMd(mdFilePath);
             guideChapter.setContent(content);
-            guideChapter.setContentHtml(ReadMdFile.markdown2Html(mdFilePath));
+            guideChapter.setContentHtml(MdAnalysis.markdown2Html(mdFilePath));
         }
         guideChapter.setUpdateTime(new Date());
         guideChapter.setCreateTime(new Date());
@@ -212,5 +180,6 @@ public class GuideCatalogServiceImpl extends ServiceImpl<GuideCatalogMapper, Gui
         guideChapterMapper.delete(null);
         //初始化dss_guide_chapter表id
         guideChapterMapper.initChapterId();
+        MdAnalysis.rootCountInit();
     }
 }

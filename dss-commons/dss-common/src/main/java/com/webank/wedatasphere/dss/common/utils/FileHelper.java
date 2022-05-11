da970360fa52a3962b906824f44c516a4ce0ac0f
ElishaZhang <1049909975@qq.com>
2022-05-11 18:48:18 +0800

    repair eventchecker connect  create  bug

--- dss-commons/dss-common/src/main/java/com/webank/wedatasphere/dss/common/utils/FileHelper.java
+++ dss-commons/dss-common/src/main/java/com/webank/wedatasphere/dss/common/utils/FileHelper.java
@@ -17,12 +17,38 @@
 package com.webank.wedatasphere.dss.common.utils;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileHelper {
 
-    public static boolean checkDirExists(String dir){
+    public static boolean checkDirExists(String dir) {
         File file = new File(dir);
         return file.exists() && file.isDirectory();
     }
 
+    /**
+     * 获取某个文件夹路径下所有文件的全路径名
+     *
+     * @param path         文件夹路径
+     * @param fileNameList 所有文件集合
+     */
+    public static void getAllFileNames(String path, List<String> fileNameList) {
+        File file = new File(path);
+        File[] files = file.listFiles();
+        String[] names = file.list();
+        if (names != null) {
+            String[] completNames = new String[names.length];
+            for (int i = 0; i < names.length; i++) {
+                completNames[i] = path + names[i];
+            }
+            fileNameList.addAll(Arrays.asList(completNames));
+        }
+        for (File tmpFile : files) {
+            //如果文件夹下有子文件夹，获取子文件夹下的所有文件全路径。
+            if (tmpFile.isDirectory()) {
+                getAllFileNames(tmpFile.getAbsolutePath() + File.separator, fileNameList);
+            }
+        }
+    }
 }

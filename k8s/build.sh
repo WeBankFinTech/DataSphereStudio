90d2a773c7c4c32dfbdae6446ecb130e714d343e
HmhWz <934178858@qq.com>
2022-05-27 15:02:38 +0800

    Remove special field

--- k8s/build.sh
+++ k8s/build.sh
@@ -2,10 +2,10 @@
 
 if [ -n "$1" ]; then
   echo 'start building images'
-  docker pull nm.hub.com/luban/base:1.0.0
+  docker pull base:1.0.0
   cat k8s/build.info | while read line; do
     {
-      imageName="nm.hub.com/luban/${line}:$1"
+      imageName="${line}:$1"
       echo "build image: ${imageName}"
 
       docker build -t $imageName -f k8s/dockerfile/${line}.Dockerfile assembly/dss-package/target/out/dss-1.0.1

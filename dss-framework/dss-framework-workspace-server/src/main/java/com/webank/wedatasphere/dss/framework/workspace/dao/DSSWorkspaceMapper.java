724197e6744dfe45ca40bd993ba41956eae2fad3
HmhWz <934178858@qq.com>
2022-06-23 17:24:26 +0800

    make scriptis entrance always on page top.

--- dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/dao/DSSWorkspaceMapper.java
+++ dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/dao/DSSWorkspaceMapper.java
@@ -53,4 +53,13 @@ public interface DSSWorkspaceMapper {
             "</script>"
     })
     void setDefaultComponentRoles(@Param("privs") List<DSSWorkspaceComponentPriv> dssWorkspaceComponentPrivs);
+
+    @Select("select count(1) from dss_workspace_user_favorites_appconn where menu_appconn_id=#{menuAppId} and workspace_id=#{workspaceId}" +
+            " and username=#{userName} and type=#{type}")
+    int getByMenuAppIdAndUser(@Param("menuAppId") Long menuAppId, @Param("workspaceId") Long workspaceId,
+                              @Param("userName") String userName, @Param("type") String type);
+
+    @Select("select id from dss_workspace_menu_appconn where title_en=#{appName}")
+    Long getMenuAppIdByName(@Param("appName") String appName);
+
 }

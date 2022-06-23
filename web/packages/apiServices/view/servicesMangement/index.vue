617f4a3f893a91460a29f59f10ba8c2368310caf
ryanlei <ryanlei@webank.com>
2022-06-23 15:54:52 +0800

    put back APIservice

---
+++ web/packages/apiServices/view/servicesMangement/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <servicesMangement />
+  </div>
+</template>
+
+<script>
+import dataServicesMangement from '../../module/servicesMangement';
+
+export default {
+  components: {
+    servicesMangement: dataServicesMangement.component
+  }
+}
+</script>

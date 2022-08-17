b1fb8c13d4ad52bc04c9b0e236ea909d63ed055d
zqburde <zhangqi111213@163.com>
2022-08-17 10:05:30 +0800

    Add copyOrchestrator and listOrchestratorCopyHistory interface.

---
+++ dss-orchestrator/dss-orchestrator-common/src/main/java/com/webank/wedatasphere/dss/orchestrator/common/entity/DSSOrchestratorCopyInfo.java
@@ -0,0 +1,218 @@
+package com.webank.wedatasphere.dss.orchestrator.common.entity;
+
+import java.util.Date;
+import java.util.List;
+
+public class DSSOrchestratorCopyInfo {
+
+    private String id;
+
+    private String username;
+
+    private String type;
+
+    private Long workspaceId;
+
+    private Long sourceOrchestratorId;
+
+    private String sourceOrchestratorName;
+
+    private String targetOrchestratorName;
+
+    private String sourceProjectName;
+
+    private String targetProjectName;
+
+    /**
+     * 目标工作流节点后缀
+     */
+    private String workflowNodeSuffix;
+
+    private String microserverName;
+
+    private String exceptionInfo;
+
+    /**
+     * 复制任务最终状态，1成功，0失败
+     */
+    private Integer status;
+
+    /**
+     * 当前编排是否在被复制，1有，0没有
+     */
+    private Integer isCopying;
+
+    /**
+     * 复制成功的节点
+     */
+    private List<String> successNode;
+
+    /**
+     * 复制开始时间
+     */
+    private Date startTime;
+
+    /**
+     * 复制结束时间
+     */
+    private Date endTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getWorkspaceId() {
+        return workspaceId;
+    }
+
+    public void setWorkspaceId(Long workspaceId) {
+        this.workspaceId = workspaceId;
+    }
+
+    public Long getSourceOrchestratorId() {
+        return sourceOrchestratorId;
+    }
+
+    public void setSourceOrchestratorId(Long sourceOrchestratorId) {
+        this.sourceOrchestratorId = sourceOrchestratorId;
+    }
+
+    public String getSourceOrchestratorName() {
+        return sourceOrchestratorName;
+    }
+
+    public void setSourceOrchestratorName(String sourceOrchestratorName) {
+        this.sourceOrchestratorName = sourceOrchestratorName;
+    }
+
+    public String getTargetOrchestratorName() {
+        return targetOrchestratorName;
+    }
+
+    public void setTargetOrchestratorName(String targetOrchestratorName) {
+        this.targetOrchestratorName = targetOrchestratorName;
+    }
+
+    public String getSourceProjectName() {
+        return sourceProjectName;
+    }
+
+    public void setSourceProjectName(String sourceProjectName) {
+        this.sourceProjectName = sourceProjectName;
+    }
+
+    public String getTargetProjectName() {
+        return targetProjectName;
+    }
+
+    public void setTargetProjectName(String targetProjectName) {
+        this.targetProjectName = targetProjectName;
+    }
+
+    public String getWorkflowNodeSuffix() {
+        return workflowNodeSuffix;
+    }
+
+    public void setWorkflowNodeSuffix(String workflowNodeSuffix) {
+        this.workflowNodeSuffix = workflowNodeSuffix;
+    }
+
+    public String getMicroserverName() {
+        return microserverName;
+    }
+
+    public void setMicroserverName(String microserverName) {
+        this.microserverName = microserverName;
+    }
+
+    public String getExceptionInfo() {
+        return exceptionInfo;
+    }
+
+    public void setExceptionInfo(String exceptionInfo) {
+        this.exceptionInfo = exceptionInfo;
+    }
+
+    public Integer isStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer isCopying() {
+        return isCopying;
+    }
+
+    public void setCopying(Integer copying) {
+        isCopying = copying;
+    }
+
+    public List<String> getSuccessNode() {
+        return successNode;
+    }
+
+    public void setSuccessNode(List<String> successNode) {
+        this.successNode = successNode;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "DSSOrchestratorCopyInfo{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", type='" + type + '\'' +
+                ", workspaceId='" + workspaceId + '\'' +
+                ", sourceOrchestratorId='" + sourceOrchestratorId + '\'' +
+                ", sourceOrchestratorName='" + sourceOrchestratorName + '\'' +
+                ", targetOrchestratorName='" + targetOrchestratorName + '\'' +
+                ", sourceProjectName='" + sourceProjectName + '\'' +
+                ", targetProjectName='" + targetProjectName + '\'' +
+                ", workflowNodeSuffix='" + workflowNodeSuffix + '\'' +
+                ", microserverName='" + microserverName + '\'' +
+                ", exceptionInfo='" + exceptionInfo + '\'' +
+                ", status=" + status +
+                ", isCopying=" + isCopying +
+                ", successNode=" + successNode +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
+}

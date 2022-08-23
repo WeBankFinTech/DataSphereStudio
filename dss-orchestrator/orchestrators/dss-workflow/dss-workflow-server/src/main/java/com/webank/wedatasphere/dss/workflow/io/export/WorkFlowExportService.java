3a4d88ce3544bce38165d691f601f0cf2ece55c1
xlinliu <xlinliu@163.com>
2022-08-23 22:22:43 +0800

    添加注释

--- dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/io/export/WorkFlowExportService.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/io/export/WorkFlowExportService.java
@@ -24,23 +24,24 @@ import java.util.List;
 public interface WorkFlowExportService {
     /**
      * 将一个工作流导出成压缩包，存放到本地磁盘，并返回压缩包的路径
-     * @param dssProjectId
-     * @param projectName
-     * @param rootFlowId
-     * @param userName
-     * @param workspace
-     * @param dssLabels
+     * @param dssProjectId 所属工作空间id
+     * @param projectName 所属项目名称
+     * @param rootFlowId 工作流根节点id
+     * @param userName 用户名
+     * @param workspace 所属工作空间
+     * @param dssLabels label列表
      * @return 导出的工作流压缩包的文件地址
      * @throws Exception
      */
     String exportFlowInfo(Long dssProjectId, String projectName, long rootFlowId, String userName, Workspace workspace, List<DSSLabel> dssLabels) throws Exception;
 
     /**
-     * 导出工作流中的各种资源。
+     * 导出工作流中的各种资源，放到projectSavePath中。
+     * 工作流中的资源包括工作流资源和节点资源
      * @param projectSavePath 保存资源的目录
      * @param flowJson 工作流元信息
      * @param flowName 工作流明
-     * @param dssLabels
+     * @param dssLabels label列表
      * @throws Exception
      */
     void exportFlowResources(String userName, Long projectId, String projectName, String projectSavePath, String flowJson, String flowName, Workspace workspace,List<DSSLabel> dssLabels) throws Exception;

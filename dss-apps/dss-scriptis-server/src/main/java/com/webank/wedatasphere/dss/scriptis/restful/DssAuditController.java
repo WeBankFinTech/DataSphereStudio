25205313efa8edfd531917b31a5065c2e64fc4e2
wushengyeyouya <690574002@qq.com>
2022-09-13 15:54:16 +0800

    Move the resultset download audit record ability to Scriptis micro-service.

--- dss-apps/dss-scriptis-server/src/main/java/com/webank/wedatasphere/dss/scriptis/restful/DssAuditController.java
+++ dss-apps/dss-scriptis-server/src/main/java/com/webank/wedatasphere/dss/scriptis/restful/DssAuditController.java
@@ -1,37 +1,32 @@
-package com.webank.wedatasphere.dss.framework.admin.restful;
+package com.webank.wedatasphere.dss.scriptis.restful;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.webank.wedatasphere.dss.framework.admin.pojo.entity.DssScriptDownloadAudit;
-import com.webank.wedatasphere.dss.framework.admin.service.DssScriptDownloadService;
-import lombok.extern.slf4j.Slf4j;
+import com.webank.wedatasphere.dss.common.conf.DSSCommonConf;
+import com.webank.wedatasphere.dss.scriptis.pojo.entity.DssScriptDownloadAudit;
+import com.webank.wedatasphere.dss.scriptis.service.DssScriptDownloadService;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.linkis.server.Message;
 import org.apache.linkis.server.security.SecurityFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.apache.linkis.server.Message;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
-/**
- * @Auther: Han Tang
- * @Date: 2022/1/11-01-11-14:20
- */
-
 
 @RestController
-@RequestMapping(path = "/dss/framework/admin/audit", produces = {"application/json"})
-@Slf4j
+@RequestMapping(path = "/dss/scriptis/audit", produces = {"application/json"})
 public class DssAuditController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DssAuditController.class);
     @Autowired
-    DssScriptDownloadService dssScriptDownloadService;
+    private DssScriptDownloadService dssScriptDownloadService;
 
-    @RequestMapping(path = "script/download/save", method = RequestMethod.POST)
+    @RequestMapping(path = "download/save", method = RequestMethod.POST)
     public Message saveScriptDownload(@RequestBody @Valid DssScriptDownloadAudit dssScriptDownloadAudit, HttpServletRequest request) {
         String userName = SecurityFilter.getLoginUsername(request);
         dssScriptDownloadAudit.setCreator(userName);
@@ -42,14 +37,22 @@ public class DssAuditController {
         }
         LOGGER.info("user {} try to saveScriptDownload, params:{}", userName, dssScriptDownloadAudit);
         dssScriptDownloadService.save(dssScriptDownloadAudit);
-        return Message.ok();
+        return Message.ok("保存成功！");
     }
 
-    @RequestMapping(path = "script/download/query", method = RequestMethod.GET)
-    public Message getScriptDownload(@RequestParam(required = false) String userName, @RequestParam(required = false) String startTime,
-                                     @RequestParam(required = false) String endTime, @RequestParam Integer pn) {
+    @RequestMapping(path = "download/query", method = RequestMethod.GET)
+    public Message getScriptDownload(HttpServletRequest req,
+            @RequestParam(required = false) String userName, @RequestParam(required = false) String startTime,
+                                     @RequestParam(required = false) String endTime, @RequestParam(required = false) Integer pn) {
+        if(pn == null || pn < 1) {
+            pn = 1;
+        }
+        String loginUser = SecurityFilter.getLoginUsername(req);
+        if(!ArrayUtils.contains(DSSCommonConf.SUPER_ADMIN_LIST, loginUser)) {
+            return Message.error("Only super admin can query the download history of user " + userName);
+        }
         PageHelper.startPage(pn, 10, true);
-        LOGGER.info("user {} try to queryScriptDownload, startTime:{}, endTime:{}, page:{}", userName, startTime, endTime, pn);
+        LOGGER.info("user {} try to queryScriptDownload, query userName: {}, startTime:{}, endTime:{}, page:{}", loginUser, userName, startTime, endTime, pn);
         List<DssScriptDownloadAudit> userPage = dssScriptDownloadService.getDownloadAuditList(userName, startTime, endTime);
         PageInfo<DssScriptDownloadAudit> pageInfo = new PageInfo<>(userPage);
         return Message.ok().data("data", pageInfo);

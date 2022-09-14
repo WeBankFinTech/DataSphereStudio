315d6e6cf0167da02bae77cc2a981e6eac556349
wushengyeyouya <690574002@qq.com>
2022-09-14 15:10:46 +0800

    Resolve the problem of changing java function to scala function.

---
+++ dss-commons/dss-common/src/main/scala/com/webank/wedatasphere/dss/common/utils/ScalaFunctionUtils.scala
@@ -0,0 +1,34 @@
+package com.webank.wedatasphere.dss.common.utils
+
+import java.util.function.{BiFunction, Consumer, Predicate, Supplier}
+
+import java.util.function
+
+import org.apache.linkis.protocol.util.ImmutablePair
+
+/**
+ *
+ * @date 2022-09-14
+ * @author enjoyyin
+ * @since 0.5.0
+ */
+object ScalaFunctionUtils {
+
+  def doConsumer[P](consumer: Consumer[P]): (P) => Unit = p => consumer.accept(p)
+
+  def doPredicate[P](predicate: Predicate[P]): (P) => Boolean = p => predicate.test(p)
+
+  def doSupplier[R](supplier: Supplier[R]): () => R = () => supplier.get()
+
+  def doFunction[P, R](function: function.Function[P, R]): (P) => R = p => function.apply(p)
+
+  def doBiFunction[P1, P2, R](function: BiFunction[P1, P2, R]): (P1, P2) => R =
+    (p1, p2) => function.apply(p1, p2)
+
+  def do3Function[P1, P2, P3, R](function: BiFunction[ImmutablePair[P1, P2], P3, R]): (P1, P2, P3) => R =
+    (p1, p2, p3) => function.apply(new ImmutablePair[P1, P2](p1, p2), p3)
+
+  def do4Function[P1, P2, P3, P4, R](function: BiFunction[ImmutablePair[P1, P2], ImmutablePair[P3, P4], R]): (P1, P2, P3, P4) => R =
+    (p1, p2, p3, p4) => function.apply(new ImmutablePair[P1, P2](p1, p2), new ImmutablePair[P3, P4](p3, p4))
+
+}

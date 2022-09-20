d2ddc7f1f060b80114e049ddccfc8469d89fb71d
Zosimer <315630932@qq.com>
2022-09-20 22:30:19 +0800

    Some bugs solved

--- plugins/dolphinscheduler/dss-dolphinscheduler-client/src/main/scala/com/webank/wedatasphere/dss/plugins/dolphinscheduler/linkis/client/job/DolphinSchedulerAppConnLinkisJob.scala
+++ plugins/dolphinscheduler/dss-dolphinscheduler-client/src/main/scala/com/webank/wedatasphere/dss/plugins/dolphinscheduler/linkis/client/job/DolphinSchedulerAppConnLinkisJob.scala
@@ -4,8 +4,8 @@ import com.webank.wedatasphere.dss.linkis.node.execution.job.AbstractAppConnLink
 import com.webank.wedatasphere.dss.plugins.dolphinscheduler.linkis.client.conf.LinkisJobTypeConf
 
 class DolphinSchedulerAppConnLinkisJob extends AbstractAppConnLinkisJob {
-  override def getSubmitUser: String = if (getJobProps.get(LinkisJobTypeConf.FLOW_SUBMIT_USER) != null
-    && getJobProps.get(LinkisJobTypeConf.FLOW_SUBMIT_USER).isEmpty) getJobProps.get(LinkisJobTypeConf.PROXY_USER) else getJobProps.get(LinkisJobTypeConf.FLOW_SUBMIT_USER)
+  override def getSubmitUser: String = if (getJobProps.get(LinkisJobTypeConf.FLOW_SUBMIT_USER) == null
+    || getJobProps.get(LinkisJobTypeConf.FLOW_SUBMIT_USER).isEmpty) getJobProps.get(LinkisJobTypeConf.PROXY_USER) else getJobProps.get(LinkisJobTypeConf.FLOW_SUBMIT_USER)
 
   override def getUser: String = getJobProps.get(LinkisJobTypeConf.PROXY_USER)
 

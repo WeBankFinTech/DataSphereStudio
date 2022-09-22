046363cb84e7398043f0cf675494cbde8586c7ad
wushengyeyouya <690574002@qq.com>
2022-09-22 22:19:18 +0800

    Optimize ProjectHttpRequestHook and add execution after hook for project CRUD.

--- dss-framework/dss-framework-project-server/src/main/java/com/webank/wedatasphere/dss/framework/project/service/ProjectHttpRequestHook.java
+++ dss-framework/dss-framework-project-server/src/main/java/com/webank/wedatasphere/dss/framework/project/service/ProjectHttpRequestHook.java
@@ -4,6 +4,7 @@ import com.webank.wedatasphere.dss.framework.project.entity.request.ProjectCreat
 import com.webank.wedatasphere.dss.framework.project.entity.request.ProjectDeleteRequest;
 import com.webank.wedatasphere.dss.framework.project.entity.request.ProjectModifyRequest;
 import com.webank.wedatasphere.dss.framework.project.entity.request.ProjectQueryRequest;
+import com.webank.wedatasphere.dss.framework.project.entity.vo.DSSProjectVo;
 import org.apache.linkis.server.Message;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,7 +20,7 @@ public interface ProjectHttpRequestHook {
      * 如果前置操作失败，则返回 Message，否则返回 null即可
      * @param request
      * @param projectRequest
-     * @return
+     * @return 前置操作失败，请返回 Message，否则返回 null
      */
     Message beforeGetAllProjects(HttpServletRequest request, ProjectQueryRequest projectRequest);
 
@@ -27,31 +28,57 @@ public interface ProjectHttpRequestHook {
      * 如果前置操作失败，则返回 Message，否则返回 null即可
      * @param request
      * @param projectCreateRequest
-     * @return
+     * @return 前置操作失败，请返回 Message，否则返回 null
      */
     Message beforeCreateProject(HttpServletRequest request, ProjectCreateRequest projectCreateRequest);
+
+    /**
+     * 创建成功后的后置操作。
+     * 请注意：我们忽略了创建失败的后置操作。
+     * @param request
+     * @param projectCreateRequest
+     * @param dssProjectVo
+     */
+    default void afterCreateProject(HttpServletRequest request, ProjectCreateRequest projectCreateRequest, DSSProjectVo dssProjectVo){
+    }
 
     /**
      * 如果前置操作失败，则返回 Message，否则返回 null即可
      * @param request
      * @param projectModifyRequest
-     * @return
+     * @return 前置操作失败，请返回 Message，否则返回 null
      */
     Message beforeModifyProject(HttpServletRequest request, ProjectModifyRequest projectModifyRequest);
 
     /**
-     * 如果前置操作失败，则返回 Message，否则返回 null即可
+     * 更新成功后的后置操作。
+     * 请注意：我们忽略了更新失败的后置操作。
+     * @param request
+     * @param projectModifyRequest
+     */
+    default void afterModifyProject(HttpServletRequest request, ProjectModifyRequest projectModifyRequest) {
+    }
+    /**
+     * 如果前置操作失败，请返回 Message，否则返回 null即可
      * @param request
      * @param projectDeleteRequest
-     * @return
+     * @return 前置操作失败，请返回 Message，否则返回 null
      */
     Message beforeDeleteProject(HttpServletRequest request, ProjectDeleteRequest projectDeleteRequest);
 
     /**
+     * 更新成功后的后置操作。
+     * 请注意：我们忽略了更新失败的后置操作。
+     * @param request
+     * @param projectDeleteRequest
+     */
+    default void afterDeleteProject(HttpServletRequest request, ProjectDeleteRequest projectDeleteRequest) {
+    }
+    /**
      * 如果前置操作失败，则返回 Message，否则返回 null即可
      * @param request
      * @param projectRequest
-     * @return
+     * @return 前置操作失败，请返回 Message，否则返回 null
      */
     Message beforeGetDeletedProject(HttpServletRequest request, ProjectQueryRequest projectRequest);
 }

22279cf7c15f95f8e290ad4245b203acbe866f01
ryanlei <ryanlei@webank.com>
2022-10-09 14:53:11 +0800

    get proxy user list after set workspace

--- web/packages/dss/view/microApp/index.vue
+++ web/packages/dss/view/microApp/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="iframeClass">111
+  <div class="iframeClass">
     <iframe
       class="iframeClass"
       v-if="visualSrc"
@@ -7,12 +7,15 @@
       :src="visualSrc"
       frameborder="0"
       width="100%"
+      sandbox="allow-same-origin allow-scripts allow-top-navigation"
       :height="height">
     </iframe>
   </div>
 </template>
 <script>
 import api from '@dataspherestudio/shared/common/service/api';
+import util from '@dataspherestudio/shared/common/util/index';
+import mixin from '@dataspherestudio/shared/common/service/mixin';
 
 export default {
   data() {
@@ -22,6 +25,7 @@ export default {
       isRefresh: true
     };
   },
+  mixins: [mixin],
   mounted() {
     // 创建的时候设置宽高
     this.resize(window.innerHeight);
@@ -37,44 +41,56 @@ export default {
     resize(height) {
       this.height = height;
     },
+    generateUrlTail(){
+      const tail = [];
+      tail.push('app_id=facade-framework');
+      tail.push('timestamp=1659924920342');
+      tail.push('nonce=12345');
+      tail.push('user=dss');
+      tail.push('signature=eca1a93c2c2bb8fc55972d76d0c1267c7782f51552a5d4562a2d871a63f64168');
+      return tail.join('&');
+    },
     async initMicroApp() {
       try {
         // 注册微应用信息
-        await api.fetch(`/mfgov/fesdk/register/v1`, {
-          // 微前端（唯一）名称
-          mf_name: 'lineage',
-          mf_func_desc: 'LINEAGE',
-          // 微前端静态资源访问相对路径
-          mf_res_entrance_location: '/lineage',
-          // 微前端静态资源版本
-          mf_latest_ver: '1.0.0',
-          mf_ref_backend_apis: '/lineage',
-        }, {
-          transformRequest: [
-            function (data) {
-              let ret = ''
-              for (let it in data) {
-                ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-              }
-              ret = ret.substring(0, ret.lastIndexOf('&'));
-              return ret
-            }
-          ],
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          baseURL: 'http://127.0.0.1:9090',
-        });
+        // await api.fetch(`/mfgov/fesdk/register/v1?${this.generateUrlTail()}`, {
+        //   // 微前端（唯一）名称
+        //   mf_name: 'data_lineage',
+        //   mf_func_desc: 'DATA LINEAGE',
+        //   // 微前端静态资源访问相对路径
+        //   mf_res_entrance_location: '/data_lineage',
+        //   // 微前端静态资源版本
+        //   mf_latest_ver: '1.0.0',
+        //   mf_ref_backend_apis: '/data_lineage',
+        // }, {
+        //   transformRequest: [
+        //     function (data) {
+        //       let ret = ''
+        //       for (let it in data) {
+        //         ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        //       }
+        //       ret = ret.substring(0, ret.lastIndexOf('&'));
+        //       return ret
+        //     }
+        //   ],
+        //   headers: {
+        //     'Content-Type': 'application/x-www-form-urlencoded',
+        //   },
+        //   baseURL: 'http://127.0.0.1:9090',
+        // });
 
         // 查询绑定信息
-        const data = await api.fetch(`/mfgov/fesdk/bindQuery/v1`, {
+        const data = await api.fetch(`/mfgov/fesdk/bindQuery/v1?${this.generateUrlTail()}`, {
           main_mf_name: 'dss',
-          sub_mf_name: 'lineage',
+          sub_mf_name: 'data_lineage',
         }, {
           method: 'get',
           baseURL: 'http://127.0.0.1:9090',
+          headers: {
+            proxyUser: this.getUserName(),
+          }
         });
-        this.visualSrc = data.accessLocation || '';
+        this.visualSrc = util.replaceHolder(`${data.accessLocation}?baseUrl=${data.accessLocation.split('#')[0].replace(/\/$/, '')}` || '');
       } catch (err) {
         console.warn('-------', err);
       }

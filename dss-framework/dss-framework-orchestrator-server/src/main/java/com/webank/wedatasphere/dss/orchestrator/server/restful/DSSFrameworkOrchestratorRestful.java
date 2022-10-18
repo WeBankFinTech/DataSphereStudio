163151e13a657e7edc272e36d4b8b5a8d03c9c6e
HmhWz <934178858@qq.com>
2022-10-18 15:54:24 +0800

    Support to unlock the workflow corresponding to orchestration.

--- dss-framework/dss-framework-orchestrator-server/src/main/java/com/webank/wedatasphere/dss/orchestrator/server/restful/DSSFrameworkOrchestratorRestful.java
+++ dss-framework/dss-framework-orchestrator-server/src/main/java/com/webank/wedatasphere/dss/orchestrator/server/restful/DSSFrameworkOrchestratorRestful.java
@@ -19,8 +19,11 @@ package com.webank.wedatasphere.dss.orchestrator.server.restful;
 import com.webank.wedatasphere.dss.appconn.manager.utils.AppConnManagerUtils;
 import com.webank.wedatasphere.dss.common.auditlog.OperateTypeEnum;
 import com.webank.wedatasphere.dss.common.auditlog.TargetTypeEnum;
+import com.webank.wedatasphere.dss.common.label.DSSLabel;
+import com.webank.wedatasphere.dss.common.label.EnvDSSLabel;
 import com.webank.wedatasphere.dss.common.utils.AuditLogUtils;
 import com.webank.wedatasphere.dss.orchestrator.common.entity.DSSOrchestratorCopyInfo;
+import com.webank.wedatasphere.dss.orchestrator.common.entity.OrchestratorVo;
 import com.webank.wedatasphere.dss.orchestrator.server.constant.OrchestratorLevelEnum;
 import com.webank.wedatasphere.dss.orchestrator.server.entity.request.*;
 import com.webank.wedatasphere.dss.orchestrator.server.entity.vo.CommonOrchestratorVo;
@@ -172,6 +175,22 @@ public class DSSFrameworkOrchestratorRestful {
     }
 
     /**
+     * 获取编排复制任务状态
+     *
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(path = "unlockOrchestrator", method = RequestMethod.POST)
+    public Message unlockOrchestrator(@RequestBody OrchestratorUnlockRequest unlockRequest) throws Exception {
+        String username = SecurityFilter.getLoginUsername(httpServletRequest);
+        Workspace workspace = SSOHelper.getWorkspace(httpServletRequest);
+        CommonOrchestratorVo orchestratorVo = orchestratorFrameworkService.unlockOrchestrator(username, workspace, unlockRequest);
+        AuditLogUtils.printLog(username, workspace.getWorkspaceId(), workspace.getWorkspaceName(), TargetTypeEnum.ORCHESTRATOR,
+                orchestratorVo.getOrchestratorId(), orchestratorVo.getOrchestratorName(), OperateTypeEnum.DELETE, unlockRequest);
+        return Message.ok("解锁成功");
+    }
+
+    /**
      * 查看编排复制历史
      *
      * @param orchestratorId
@@ -194,5 +213,57 @@ public class DSSFrameworkOrchestratorRestful {
         List<OrchestratorLevelEnum> levels = Arrays.asList(OrchestratorLevelEnum.values());
         LOGGER.info("user {} try to get OrchestratorLevels, workspaceId:{}, result:{}", username, workspace.getWorkspaceId(), levels);
         return Message.ok("获取编排重要级别列表成功").data("orchestratorLevels", levels);
+    }
+
+    @RequestMapping(path = "rollbackOrchestrator", method = RequestMethod.POST)
+    public Message rollbackOrchestrator(HttpServletRequest request, @RequestBody RollbackOrchestratorRequest rollbackOrchestratorRequest) {
+        String username = SecurityFilter.getLoginUsername(request);
+        Long orchestratorId = rollbackOrchestratorRequest.getOrchestratorId();
+        String version = rollbackOrchestratorRequest.getVersion();
+        Long projectId = rollbackOrchestratorRequest.getProjectId();
+        String projectName = rollbackOrchestratorRequest.getProjectName();
+        Workspace workspace = SSOHelper.getWorkspace(request);
+        DSSLabel envDSSLabel = new EnvDSSLabel(rollbackOrchestratorRequest.getLabels().getRoute());
+        try {
+            LOGGER.info("user {} begin to rollbackOrchestrator, params:{}", username, rollbackOrchestratorRequest);
+            String newVersion = orchestratorService.rollbackOrchestrator(username, projectId, projectName, orchestratorId, version, envDSSLabel, workspace);
+            Message message = Message.ok("回滚版本成功").data("newVersion", newVersion);
+            return message;
+        } catch (final Throwable t) {
+            LOGGER.error("Failed to rollback orchestrator for user {} orchestratorId {}, projectId {} version {}",
+                    username, orchestratorId, projectId, version, t);
+            return Message.error("回滚工作流版本失败");
+        }
+    }
+
+    @RequestMapping(path = "openOrchestrator", method = RequestMethod.POST)
+    public Message openOrchestrator(HttpServletRequest req, @RequestBody OpenOrchestratorRequest openOrchestratorRequest) throws Exception {
+        String openUrl = "";
+        String userName = SecurityFilter.getLoginUsername(req);
+        Workspace workspace = SSOHelper.getWorkspace(req);
+        List<DSSLabel> dssLabelList = Arrays.asList(new EnvDSSLabel(openOrchestratorRequest.getLabels().getRoute()));
+        Long orchestratorId = openOrchestratorRequest.getOrchestratorId();
+        if (orchestratorFrameworkService.getOrchestratorCopyStatus(orchestratorId)){
+            return Message.error("当前工作流正在被复制，不允许被打开，请稍后");
+        }
+        LOGGER.info("user {} try to openOrchestrator, params:{}", userName, openOrchestratorRequest);
+        openUrl = orchestratorService.openOrchestrator(userName, workspace, orchestratorId, dssLabelList);
+        OrchestratorVo orchestratorVo = orchestratorService.getOrchestratorVoById(orchestratorId);
+        LOGGER.info("open url is {}, orcId is {}, dssLabels is {}", openUrl, orchestratorId, dssLabelList);
+        return Message.ok().data("OrchestratorOpenUrl", openUrl).
+                data("OrchestratorVo", orchestratorVo);
+    }
+
+    /**
+     * 获取编排模式下的所有版本号
+     *
+     * @param queryOrchestratorVersion
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(path = "getVersionByOrchestratorId", method = RequestMethod.POST)
+    public Message getVersionByOrchestratorId(@RequestBody QueryOrchestratorVersion queryOrchestratorVersion) throws Exception {
+        List list = orchestratorService.getVersionByOrchestratorId(queryOrchestratorVersion.getOrchestratorId());
+        return Message.ok().data("list", list);
     }
 }

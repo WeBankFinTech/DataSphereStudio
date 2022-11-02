d7eff4872f93ffaff019b7e1601a2600889220ad
HmhWz <934178858@qq.com>
2022-11-02 16:31:08 +0800

    Auto correct subflow dependencies in db when saveFLow.

--- dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/constant/DSSWorkFlowConstant.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/constant/DSSWorkFlowConstant.java
@@ -43,6 +43,7 @@ public class DSSWorkFlowConstant {
     public static final String SCHEDULER_APP_CONN_NAME = "schedulerAppConnName";
     public static final String REF_PROJECT_ID_KEY = "refProjectId";
     public static final String TITLE_KEY = "title";
+    public static final String JOBTYPE_KEY = "jobType";
 
     /**
      * 用户已锁定编辑错误码

001bfbe4d09bdf396c77d804e8b70ef858f79f99
wushengyeyouya <690574002@qq.com>
2022-11-22 20:54:08 +0800

    Add feature to support DSS SSO integration standard with DolphinScheduler.

--- plugins/dolphinscheduler/dolphinscheduler-prod-metrics/src/main/java/org/apache/dolphinscheduler/api/interceptor/DolphinSchedulerHttpRequestUserInterceptor.java
+++ plugins/dolphinscheduler/dolphinscheduler-prod-metrics/src/main/java/org/apache/dolphinscheduler/api/interceptor/DolphinSchedulerHttpRequestUserInterceptor.java
@@ -46,7 +46,7 @@ public class DolphinSchedulerHttpRequestUserInterceptor implements HttpRequestUs
         }
         String sessionId = sessionService.createSession(userObj, BaseController.getClientIpAddress(req));
         Cookie cookie = new Cookie(Constants.SESSION_ID, sessionId);
-        cookie.setHttpOnly(true);
+//        cookie.setHttpOnly(true);
         logger.info("added a new session.id {} for DSS user {}.", sessionId, user);
         return new HttpServletRequestWrapper(req) {
             @Override

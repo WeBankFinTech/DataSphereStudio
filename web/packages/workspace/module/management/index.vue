b315ef6ae5692601db134d566ed9320dc6c6e39c
ryanlei <ryanlei@webank.com>
2022-11-28 16:12:59 +0800

    enginelist & create table form validate

--- web/packages/workspace/module/management/index.vue
+++ web/packages/workspace/module/management/index.vue
@@ -50,6 +50,12 @@ export default {
       //   path: 'dataSourceAdministration',
       //   children: [],
       // },
+      {
+        icon: 'enginelist',
+        name: i18n.t('message.workspaceManagement.engineInfo'),
+        path: 'enginelist',
+        children: [],
+      }
     ];
     return {
       mode: '',

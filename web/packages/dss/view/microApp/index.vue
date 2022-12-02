4508cab9df3258e565d6ab8f257b53b6ee1d1c42
ryanlei <ryanlei@webank.com>
2022-12-02 16:53:30 +0800

    同步修复问题

--- web/packages/dss/view/microApp/index.vue
+++ web/packages/dss/view/microApp/index.vue
@@ -2,7 +2,7 @@
   <div class="iframeClass">
     <iframe
       class="iframeClass"
-      v-if="visualSrc"
+      v-if="isRefresh"
       id="mircoApp"
       :src="visualSrc"
       frameborder="0"
@@ -37,9 +37,20 @@ export default {
   beforeDestroy() {
     window.removeEventListener('resize', this.resize(window.innerHeight));
   },
+  watch: {
+    '$route.params.appName'() {
+      // 切换应用, 重新获取数据
+      this.initMicroApp();
+      this.reload();
+    },
+  },
   methods: {
     resize(height) {
       this.height = height;
+    },
+    reload() {
+      this.isRefresh = false;
+      this.$nextTick(() => this.isRefresh = true);
     },
     generateUrlTail(){
       const tail = [];
@@ -50,13 +61,16 @@ export default {
       tail.push('signature=eca1a93c2c2bb8fc55972d76d0c1267c7782f51552a5d4562a2d871a63f64168');
       return tail.join('&');
     },
+    humpToLine(str){
+      return str.replace(/([A-Z])/g, '_$1').toLowerCase().slice(1);
+    },
     async initMicroApp() {
       console.log(process.env.VUE_APP_MICRO_PREFIX)
       try {
         // 查询绑定信息
         const data = await api.fetch(`/mfgov/fesdk/bindQuery/v1?${this.generateUrlTail()}`, {
           main_mf_name: 'dss',
-          sub_mf_name: 'data_lineage',
+          sub_mf_name: this.humpToLine(this.$route.params.appName),
         }, {
           method: 'get',
           baseURL: process.env.VUE_APP_MICRO_PREFIX,

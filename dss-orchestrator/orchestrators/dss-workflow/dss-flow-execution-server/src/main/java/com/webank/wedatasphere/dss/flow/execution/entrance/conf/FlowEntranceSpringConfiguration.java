3c22b3abcfd07732b9eafbf172351cf8924aa4a1
xlinliu <xlinliu@163.com>
2023-01-18 16:42:18 +0800

    定义dss自己的entrance interceptor

--- dss-orchestrator/orchestrators/dss-workflow/dss-flow-execution-server/src/main/java/com/webank/wedatasphere/dss/flow/execution/entrance/conf/FlowEntranceSpringConfiguration.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-flow-execution-server/src/main/java/com/webank/wedatasphere/dss/flow/execution/entrance/conf/FlowEntranceSpringConfiguration.java
@@ -11,13 +11,17 @@ import org.apache.linkis.entrance.interceptor.impl.LabelCheckInterceptor;
 import org.apache.linkis.entrance.interceptor.impl.LogPathCreateInterceptor;
 import org.apache.linkis.entrance.interceptor.impl.StorePathEntranceInterceptor;
 import org.apache.linkis.entrance.constant.ServiceNameConsts;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FlowEntranceSpringConfiguration  {
+    private  final Logger logger = LoggerFactory.getLogger(getClass());
     @Bean(name=ServiceNameConsts.ENTRANCE_INTERCEPTOR)
     public EntranceInterceptor[] entranceInterceptors() {
+        logger.info("dss workflow entrance load entranceInterceptors");
         return new EntranceInterceptor[] {
                 new LabelCheckInterceptor(),
                 new LogPathCreateInterceptor(),

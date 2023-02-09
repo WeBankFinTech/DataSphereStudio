703566a0ac34f1b5f7f5419f5cb46c25c6bc2aa3
xlinliu <xlinliu@163.com>
2023-02-09 15:43:49 +0800

    DssProxyUserService support passing proxy user by header

--- dss-framework/dss-framework-proxy-user-service/src/main/java/com/webank/wedatasphere/dss/framework/proxy/service/DssProxyUserService.java
+++ dss-framework/dss-framework-proxy-user-service/src/main/java/com/webank/wedatasphere/dss/framework/proxy/service/DssProxyUserService.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public interface DssProxyUserService {
+    String TOKEN_PROXY_USER_HEADER_KEY="Token-Proxy-User";
 
     /**
      * 查询代理用户数据
@@ -29,6 +30,11 @@ public interface DssProxyUserService {
     }
 
     default String getProxyUser(HttpServletRequest request) throws DSSProxyUserErrorException {
+        //header的优先级更高，如果header里指定了代理用户，则直接返回。
+        String proxyUser=request.getHeader(TOKEN_PROXY_USER_HEADER_KEY);
+        if(proxyUser!=null){
+            return proxyUser;
+        }
         Option<String> proxyUser = ProxyUserSSOUtils.getProxyUserUsername(request);
         if(proxyUser.isEmpty()) {
             throw new DSSProxyUserErrorException(60050, "proxy user is not exists in cookies.");

98a2c05fc10e4e3dd4840f0952e38126edd5f325
HmhWz <934178858@qq.com>
2023-03-22 10:40:37 +0800

    Merge orchestrator-server,workflow-server,flow-execution.

--- dss-orchestrator/orchestrators/dss-workflow/dss-flow-execution-server/src/main/scala/com/webank/wedatasphere/dss/flow/execution/entrance/job/parser/FlowJobFlowParser.scala
+++ dss-orchestrator/orchestrators/dss-workflow/dss-flow-execution-server/src/main/scala/com/webank/wedatasphere/dss/flow/execution/entrance/job/parser/FlowJobFlowParser.scala
@@ -15,20 +15,16 @@
  */
 
 package com.webank.wedatasphere.dss.flow.execution.entrance.job.parser
-import com.webank.wedatasphere.dss.common.protocol.{ProxyUserCheckRequest, RequestQueryWorkFlow, ResponseProxyUserCheck}
+
 import com.webank.wedatasphere.dss.common.utils.DSSCommonUtils
-import com.webank.wedatasphere.dss.flow.execution.entrance.conf.FlowExecutionEntranceConfiguration
-import com.webank.wedatasphere.dss.flow.execution.entrance.exception.FlowExecutionErrorException
 import com.webank.wedatasphere.dss.flow.execution.entrance.job.FlowEntranceJob
 import com.webank.wedatasphere.dss.flow.execution.entrance.utils.FlowExecutionUtils
-import com.webank.wedatasphere.dss.linkis.node.execution.utils.LinkisJobExecutionUtils
-import com.webank.wedatasphere.dss.sender.service.DSSSenderServiceFactory
-import com.webank.wedatasphere.dss.workflow.common.protocol.ResponseQueryWorkflow
+import com.webank.wedatasphere.dss.workflow.WorkFlowManager
+import com.webank.wedatasphere.dss.workflow.common.entity.DSSFlow
 import com.webank.wedatasphere.dss.workflow.core.WorkflowFactory
 import com.webank.wedatasphere.dss.workflow.core.entity.Workflow
 import org.apache.linkis.common.utils.Logging
-import org.apache.linkis.rpc.Sender
-import org.apache.commons.lang3.StringUtils
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 
@@ -37,14 +33,17 @@ import org.springframework.stereotype.Component
 @Component
 class FlowJobFlowParser extends FlowEntranceJobParser with Logging {
 
+  @Autowired
+  private var workFlowManager: WorkFlowManager = _
+
   override def parse(flowEntranceJob: FlowEntranceJob): Unit = {
     info(s"Start to parse flow of Job(${flowEntranceJob.getId}).")
     val code = flowEntranceJob.jobToExecuteRequest().code
     val label = flowEntranceJob.getParams.getOrDefault(DSSCommonUtils.DSS_LABELS_KEY, DSSCommonUtils.ENV_LABEL_VALUE_DEV).asInstanceOf[String]
     val codeLanguageLabel = FlowExecutionUtils.getRunTypeLabel(flowEntranceJob.getJobRequest().getLabels)
-    if("json" == codeLanguageLabel.getCodeType // 兼容老版本
+    if ("json" == codeLanguageLabel.getCodeType // 兼容老版本
       || "flow" == codeLanguageLabel.getCodeType) {
-      val flowExecutionCode =DSSCommonUtils.COMMON_GSON.fromJson(code, classOf[java.util.Map[String, Any]])
+      val flowExecutionCode = DSSCommonUtils.COMMON_GSON.fromJson(code, classOf[java.util.Map[String, Any]])
       val flowId = flowExecutionCode.get("flowId").asInstanceOf[Number].longValue()
       val workflow = getDSSScheduleFlowById(flowEntranceJob.getUser, flowId, label)
       flowEntranceJob.setFlow(workflow)
@@ -52,13 +51,11 @@ class FlowJobFlowParser extends FlowEntranceJobParser with Logging {
     }
   }
 
-  private def getDSSScheduleFlowById(userName: String , flowId:Long, label:String): Workflow = {
-    val req = RequestQueryWorkFlow(userName, flowId)
-    val sendWorkflowName = FlowExecutionEntranceConfiguration.WORKFLOW_APPLICATION_NAME.getValue + label.toUpperCase
-    logger.info("Send query workflow json to "+ sendWorkflowName)
-    val response: ResponseQueryWorkflow= Sender.getSender(sendWorkflowName)
-                                               .ask(req).asInstanceOf[ResponseQueryWorkflow]
-    WorkflowFactory.INSTANCE.getJsonToFlowParser.parse(response.getDssFlow)
+  private def getDSSScheduleFlowById(userName: String, flowId: Long, label: String): Workflow = {
+//    val sendWorkflowName = FlowExecutionEntranceConfiguration.WORKFLOW_APPLICATION_NAME.getValue + label.toUpperCase
+    logger.info("Send query workflow call to workflowManager")
+    val dssflow: DSSFlow = workFlowManager.queryWorkflow(userName, flowId);
+    WorkflowFactory.INSTANCE.getJsonToFlowParser.parse(dssflow)
   }
 
 }

f2f53e47e02cacab336be7cdcd2d05777f3c51ca
zqburde <zhangqi111213@163.com>
2023-04-14 10:06:59 +0800

    Add common alter interface and modify monitor task.

--- dss-framework/dss-framework-orchestrator-server/src/main/java/com/webank/wedatasphere/dss/orchestrator/server/job/CheckOrchestratorConversionJobTask.java
+++ dss-framework/dss-framework-orchestrator-server/src/main/java/com/webank/wedatasphere/dss/orchestrator/server/job/CheckOrchestratorConversionJobTask.java
@@ -1,12 +1,16 @@
 package com.webank.wedatasphere.dss.orchestrator.server.job;
 
+import com.webank.wedatasphere.dss.common.alter.ExecuteAlter;
 import com.webank.wedatasphere.dss.common.conf.DSSCommonConf;
 import com.webank.wedatasphere.dss.common.protocol.JobStatus;
+import com.webank.wedatasphere.dss.flow.execution.entrance.job.CheckWorkflowExecuteTask;
 import com.webank.wedatasphere.dss.orchestrator.db.dao.OrchestratorJobMapper;
 import com.webank.wedatasphere.dss.orchestrator.common.entity.OrchestratorPublishJob;
 import com.webank.wedatasphere.dss.sender.service.conf.DSSSenderServiceConf;
 import org.apache.linkis.common.ServiceInstance;
 import org.apache.linkis.rpc.Sender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -23,8 +27,12 @@ import java.util.stream.Collectors;
 @EnableScheduling
 public class CheckOrchestratorConversionJobTask {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CheckOrchestratorConversionJobTask.class);
     @Autowired
     private OrchestratorJobMapper orchestratorJobMapper;
+
+    @Autowired
+    private ExecuteAlter executeAlter;
 
     List<OrchestratorPublishJob> failedJobs;
 
@@ -33,25 +41,32 @@ public class CheckOrchestratorConversionJobTask {
 
         ServiceInstance[] allActionInstances = Sender.getInstances(DSSSenderServiceConf.DSS_SERVER_NAME.getValue());
         if (allActionInstances.length == DSSCommonConf.DSS_INSTANCE_NUMBERS.getValue()){
-         return;
+            LOGGER.info("All instances are active!");
+            return;
         }
 
         List<OrchestratorPublishJob> maybeFailedJobs = orchestratorJobMapper.getPublishJobByJobStatuses
                 (Arrays.asList(JobStatus.Inited.getStatus(), JobStatus.Running.getStatus()));
-
+        LOGGER.info("These tasks maybe are failed. " + maybeFailedJobs.toString());
         List<String> activeInstance = Arrays.stream(allActionInstances).map(ServiceInstance::getInstance).collect(Collectors.toList());
-
-        for (OrchestratorPublishJob maybeFailedJob : maybeFailedJobs) {
-            if (!activeInstance.contains(maybeFailedJob.getInstanceName())) {
-                maybeFailedJob.setStatus(JobStatus.Failed.getStatus());
-                failedJobs.add(maybeFailedJob);
+        LOGGER.info("Active instances are " + activeInstance);
+        if (maybeFailedJobs.size() > 0) {
+            for (OrchestratorPublishJob maybeFailedJob : maybeFailedJobs) {
+                if (!activeInstance.contains(maybeFailedJob.getInstanceName())) {
+                    maybeFailedJob.setStatus(JobStatus.Failed.getStatus());
+                    failedJobs.add(maybeFailedJob);
+                }
             }
         }
 
         // update publish job status to failed
-        orchestratorJobMapper.batchUpdatePublishJob(failedJobs);
+        if (failedJobs.size() > 0) {
+            orchestratorJobMapper.batchUpdatePublishJob(failedJobs);
+            failedJobs.clear();
+        }
 
         // send alter
+//        executeAlter.sendAlter();
 
     }
 

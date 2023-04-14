9827eca9074f06623584c4126f791ad2a5bbb7da
zqburde <zhangqi111213@163.com>
2023-04-14 10:06:59 +0800

    Add common alter interface and modify monitor task.

--- dss-commons/dss-common/src/main/java/com/webank/wedatasphere/dss/common/entity/ExceptionAlterSender.java
+++
@@ -1,8 +0,0 @@
-package com.webank.wedatasphere.dss.common.entity;
-
-public interface ExceptionAlterSender {
-    /**
-     * send alter to user when server exception
-     */
-    void sendAlter(Alter alter);
-}

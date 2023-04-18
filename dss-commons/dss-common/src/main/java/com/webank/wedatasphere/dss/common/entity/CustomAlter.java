af3a8ec87f1b5d1145f0b7652bea832bbd09bfe4
zqburde <zhangqi111213@163.com>
2023-04-18 15:50:42 +0800

    Modify Alter.

--- dss-commons/dss-common/src/main/java/com/webank/wedatasphere/dss/common/entity/CustomAlter.java
+++ dss-commons/dss-common/src/main/java/com/webank/wedatasphere/dss/common/entity/CustomAlter.java
@@ -2,32 +2,14 @@ package com.webank.wedatasphere.dss.common.entity;
 
 public class CustomAlter extends Alter{
 
-    private String alterLevel;
-
-    private String alterReceiver;
 
     public CustomAlter() {
     }
 
     public CustomAlter(String alterTitle, String alterInfo, String alterLevel, String alterReceiver) {
-        super(alterTitle, alterInfo);
-        this.alterLevel = alterLevel;
-        this.alterReceiver = alterReceiver;
+        super(alterTitle, alterInfo, alterLevel, alterReceiver);
+
     }
 
-    public String getAlterLevel() {
-        return alterLevel;
-    }
 
-    public void setAlterLevel(String alterLevel) {
-        this.alterLevel = alterLevel;
-    }
-
-    public String getAlterReceiver() {
-        return alterReceiver;
-    }
-
-    public void setAlterReceiver(String alterReceiver) {
-        this.alterReceiver = alterReceiver;
-    }
 }

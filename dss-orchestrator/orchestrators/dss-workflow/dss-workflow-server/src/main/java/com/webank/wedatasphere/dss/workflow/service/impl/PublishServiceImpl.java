d11ccd95c01a4a555ee5126b4f065fc25b8db9c2
HmhWz <934178858@qq.com>
2023-04-25 18:46:04 +0800

    rpc bug fix.

--- dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/service/impl/PublishServiceImpl.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/service/impl/PublishServiceImpl.java
@@ -67,15 +67,10 @@ public class PublishServiceImpl implements PublishService {
 
     @Override
     public String submitPublish(String convertUser, Long workflowId,
-        Map<String, Object> dssLabel, Workspace workspace, String comment) throws Exception {
+                                Map<String, Object> dssLabel, Workspace workspace, String comment) throws Exception {
         LOGGER.info("User {} begins to convert workflow {}.", convertUser, workflowId);
         //1 获取对应的orcId 和 orcVersionId
         //2.进行提交
-        RequestFrameworkConvertOrchestration requestFrameworkConvertOrchestration = getRequestFrameworkConvertOrchestration();
-        requestFrameworkConvertOrchestration.setComment(comment);
-        requestFrameworkConvertOrchestration.setOrcAppId(workflowId);
-        requestFrameworkConvertOrchestration.setUserName(convertUser);
-        requestFrameworkConvertOrchestration.setWorkspace(workspace);
         DSSFlow dssFlow = null;
         try {
             dssFlow = dssFlowService.getFlow(workflowId);
@@ -90,18 +85,16 @@ public class PublishServiceImpl implements PublishService {
                 DSSExceptionUtils.dealErrorException(63335, "工作流所在工作空间和cookie中不一致，请刷新页面后，再次发布！", DSSErrorException.class);
             }
             String schedulerAppConnName = workFlowParser.getValueWithKey(dssFlow.getFlowJson(), DSSWorkFlowConstant.SCHEDULER_APP_CONN_NAME);
-            if(StringUtils.isBlank(schedulerAppConnName)) {
+            if (StringUtils.isBlank(schedulerAppConnName)) {
                 // 向下兼容老版本
                 schedulerAppConnName = DEFAULT_SCHEDULER_APP_CONN.getValue();
             }
             SchedulerAppConn schedulerAppConn = (SchedulerAppConn) AppConnManager.getAppConnManager().getAppConn(schedulerAppConnName);
             // 只是为了获取是否需要发布所有Orc，这里直接拿第一个AppInstance即可。
             AppInstance appInstance = schedulerAppConn.getAppDesc().getAppInstances().get(0);
-            requestFrameworkConvertOrchestration.setConvertAllOrcs(schedulerAppConn.getOrCreateConversionStandard().getDSSToRelConversionService(appInstance).isConvertAllOrcs());
-            requestFrameworkConvertOrchestration.setLabels(dssLabel);
-            ResponseConvertOrchestrator response = RpcAskUtils.processAskException(getOrchestratorSender().ask(requestFrameworkConvertOrchestration),
-                    ResponseConvertOrchestrator.class, RequestFrameworkConvertOrchestration.class);
-            if(response.getResponse().isFailed()) {
+            ResponseConvertOrchestrator response = requestConvertOrchestration(comment, workflowId, convertUser,
+                    workspace, schedulerAppConn, dssLabel, appInstance);
+            if (response.getResponse().isFailed()) {
                 throw new DSSErrorException(50311, response.getResponse().getMessage());
             }
             return response.getId();
@@ -116,27 +109,36 @@ public class PublishServiceImpl implements PublishService {
     }
 
     /**
-     * 可覆写该方法自定义实现发布对象
+     * 可覆写该方法自定义实现request对象
      *
      * @return
      */
-    protected RequestFrameworkConvertOrchestration getRequestFrameworkConvertOrchestration() {
-        return new RequestFrameworkConvertOrchestration();
+    protected ResponseConvertOrchestrator requestConvertOrchestration(String comment, Long workflowId, String convertUser, Workspace workspace,
+                                                                      SchedulerAppConn schedulerAppConn, Map<String, Object> dssLabel, AppInstance appInstance) {
+        RequestFrameworkConvertOrchestration requestFrameworkConvertOrchestration = new RequestFrameworkConvertOrchestration();
+        requestFrameworkConvertOrchestration.setComment(comment);
+        requestFrameworkConvertOrchestration.setOrcAppId(workflowId);
+        requestFrameworkConvertOrchestration.setUserName(convertUser);
+        requestFrameworkConvertOrchestration.setWorkspace(workspace);
+        requestFrameworkConvertOrchestration.setConvertAllOrcs(schedulerAppConn.getOrCreateConversionStandard().getDSSToRelConversionService(appInstance).isConvertAllOrcs());
+        requestFrameworkConvertOrchestration.setLabels(dssLabel);
+        ResponseConvertOrchestrator response = RpcAskUtils.processAskException(getOrchestratorSender().ask(requestFrameworkConvertOrchestration),
+                ResponseConvertOrchestrator.class, RequestFrameworkConvertOrchestration.class);
+        return response;
     }
+
 
     @Override
     public ResponseConvertOrchestrator getStatus(String username, String taskId) {
-        if (LOGGER.isDebugEnabled()){
+        if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("{} is asking status of {}.", username, taskId);
         }
-        ResponseConvertOrchestrator response =new ResponseConvertOrchestrator();
+        ResponseConvertOrchestrator response = new ResponseConvertOrchestrator();
         //通过rpc的方式去获取到最新status
         try {
-            RequestFrameworkConvertOrchestrationStatus req = getRequestFrameworkConvertOrchestrationStatus();
-            req.setId(taskId);
-            response = RpcAskUtils.processAskException(getOrchestratorSender().ask(req), ResponseConvertOrchestrator.class, RequestFrameworkConvertOrchestrationStatus.class);
+            response = requestConvertOrchestrationStatus(taskId);
             LOGGER.info("user {} gets status of {}, status is {}，msg is {}", username, taskId, response.getResponse().getJobStatus(), response.getResponse().getMessage());
-        }catch (Exception t){
+        } catch (Exception t) {
             LOGGER.error("failed to getStatus {} ", taskId, t);
 
         }
@@ -144,11 +146,15 @@ public class PublishServiceImpl implements PublishService {
     }
 
     /**
-     * 可覆写该方法自定义实现发布状态
+     * 可覆写该方法自定义实现request对象
      *
      * @return
      */
-    protected RequestFrameworkConvertOrchestrationStatus getRequestFrameworkConvertOrchestrationStatus(){
-        return new RequestFrameworkConvertOrchestrationStatus();
+    protected ResponseConvertOrchestrator requestConvertOrchestrationStatus(String taskId){
+        RequestFrameworkConvertOrchestrationStatus req =  new RequestFrameworkConvertOrchestrationStatus();
+        req.setId(taskId);
+        return RpcAskUtils.processAskException(getOrchestratorSender().ask(req), ResponseConvertOrchestrator.class,
+                RequestFrameworkConvertOrchestrationStatus.class);
     }
+
 }

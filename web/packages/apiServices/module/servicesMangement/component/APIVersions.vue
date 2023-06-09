07611cde505b1b622344a3cd0357fff197590fb2
ryanlei <ryanlei@webank.com>
2023-06-10 01:12:08 +0800

    数据服务合并提单

--- web/packages/apiServices/module/servicesMangement/component/APIVersions.vue
+++ web/packages/apiServices/module/servicesMangement/component/APIVersions.vue
@@ -120,12 +120,14 @@ export default {
           key: "status",
           align: "center",
           render: (h, scope) => {
+            const colors = ['red','rgb(18, 150, 219)','#0F1222','#F29360']
+            const list = [this.$t('message.apiServices.disable'),this.$t('message.apiServices.enable'),this.$t('message.apiServices.unsubmit'),this.$t('message.apiServices.submited')]
             return h(
               "span",
               {
-                style: { color: scope.row.status === 1 ? 'rgb(18, 150, 219)': 'red'}
+                style: { color: colors[scope.row.status]}
               },
-              scope.row.status === 1 ? this.$t('message.apiServices.enable') : this.$t('message.apiServices.disable')
+              list[scope.row.status]
             );
           }
         },
@@ -137,6 +139,11 @@ export default {
         {
           title: this.$t('message.apiServices.apiTable.creator'),
           key: "creator",
+          align: "center"
+        },
+        {
+          title: this.$t('message.apiServices.apiTable.executeUser'),
+          key: "executeUser",
           align: "center"
         },
         {

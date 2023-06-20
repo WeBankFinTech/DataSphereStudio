af59992eea914cde9e799902f4a5e2ba858388c9
zqburde <zhangqi111213@163.com>
2023-06-20 17:43:36 +0800

    Adopt to Linkis 1.4.0.

--- dss-appconn/appconns/dss-sendemail-appconn/sendemail-appconn-core/src/main/scala/com/webank/wedatasphere/dss/appconn/sendemail/emailcontent/parser/AbstractEmailContentParser.scala
+++ dss-appconn/appconns/dss-sendemail-appconn/sendemail-appconn-core/src/main/scala/com/webank/wedatasphere/dss/appconn/sendemail/emailcontent/parser/AbstractEmailContentParser.scala
@@ -26,7 +26,7 @@ import org.apache.linkis.common.io.resultset.ResultSetReader
 import org.apache.linkis.common.io.{MetaData, Record}
 import org.apache.linkis.common.utils.Utils
 import org.apache.linkis.storage.LineRecord
-import org.apache.linkis.storage.resultset.ResultSetReader
+import org.apache.linkis.storage.resultset.ResultSetReaderFactory
 import org.apache.commons.io.IOUtils
 
 abstract class AbstractEmailContentParser[T] extends EmailContentParser {
@@ -41,8 +41,8 @@ abstract class AbstractEmailContentParser[T] extends EmailContentParser {
     case _ =>
   }
 
-  protected def getResultSetReader(fsPathStore: FsPathStoreEmailContent): ResultSetReader[_ <: MetaData, _ <: Record] = {
-    val reader = ResultSetReader.getResultSetReader(fsPathStore.getFsPath.getSchemaPath)
+  protected def getResultSetReader(fsPathStore: FsPathStoreEmailContent): ResultSetReader[_, _ ] = {
+    val reader = ResultSetReaderFactory.getResultSetReader(fsPathStore.getFsPath.getSchemaPath)
     reader.getMetaData
     reader
   }

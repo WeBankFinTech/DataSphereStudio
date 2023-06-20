a587a49f1db6e5f9fee272b939b338ff8607ba91
zqburde <zhangqi111213@163.com>
2023-06-20 17:43:36 +0800

    Adopt to Linkis 1.4.0.

--- dss-appconn/linkis-appconn-engineplugin/src/main/scala/org/apache/linkis/manager/engineplugin/appconn/AppConnEngineConnPlugin.scala
+++ dss-appconn/linkis-appconn-engineplugin/src/main/scala/org/apache/linkis/manager/engineplugin/appconn/AppConnEngineConnPlugin.scala
@@ -45,7 +45,7 @@ class AppConnEngineConnPlugin extends EngineConnPlugin {
   private val defaultLabels: util.List[Label[_]] = new util.ArrayList[Label[_]]()
 
 
-  override def init(params: util.Map[String, Any]): Unit = {
+  override def init(params: util.Map[String, AnyRef]): Unit = {
     val engineTypeLabel = EngineTypeLabelCreator.createEngineTypeLabel(EngineType.APPCONN.toString)
     this.defaultLabels.add(engineTypeLabel)
   }

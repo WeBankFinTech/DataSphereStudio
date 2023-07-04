bba75d0be3b7c421a2143702160465e5436db2ff
zqburde <zhangqi111213@163.com>
2023-07-04 20:12:52 +0800

    Add get notice feature.

---
+++ dss-commons/dss-common/src/main/java/com/webank/wedatasphere/dss/common/entity/NoticeContent.java
@@ -0,0 +1,53 @@
+package com.webank.wedatasphere.dss.common.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 首页公告
+ * Author: xlinliu
+ * Date: 2023/3/13
+ */
+@TableName(value = "dss_notice")
+public class NoticeContent implements Serializable {
+    /**
+     * 公告内容
+     */
+    private String content;
+    /**
+     * 公告生效时间
+     */
+    private Date startTime;
+    /**
+     * 公告失效时间
+     */
+    private Date endTime;
+
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+}

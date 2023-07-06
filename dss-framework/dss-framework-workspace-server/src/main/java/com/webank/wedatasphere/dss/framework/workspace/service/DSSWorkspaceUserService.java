1677e08bf21bc739c4b0a724da86bd4f388f5b90
wuxb <wuxb78@qq.com>
2023-07-06 17:40:44 +0800

    Modify the return type Map to DSSWorkspaceRoleVO

--- dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/DSSWorkspaceUserService.java
+++ dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/DSSWorkspaceUserService.java
@@ -18,11 +18,10 @@ package com.webank.wedatasphere.dss.framework.workspace.service;
 
 
 import com.webank.wedatasphere.dss.common.entity.PageInfo;
+import com.webank.wedatasphere.dss.framework.workspace.bean.vo.DSSWorkspaceRoleVO;
 import com.webank.wedatasphere.dss.framework.workspace.bean.vo.StaffInfoVO;
-import com.webank.wedatasphere.dss.standard.app.sso.Workspace;
 
 import java.util.List;
-import java.util.Map;
 
 
 public interface DSSWorkspaceUserService {
@@ -62,7 +61,7 @@ public interface DSSWorkspaceUserService {
     Long getCountByUsername(String username,int workspaceId);
     Long getUserCount(long workspaceId);
 
-    List<Map<String,Object>> getUserRoleByUserName(String userName);
+    List<DSSWorkspaceRoleVO> getUserRoleByUserName(String userName);
 
     void clearUserByUserName(String userName);
 

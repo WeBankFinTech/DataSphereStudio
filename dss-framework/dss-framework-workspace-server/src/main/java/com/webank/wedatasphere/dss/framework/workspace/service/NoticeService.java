b22e68ebc1eda1ba4304a74014482ad599bb25ab
zhaobincai <zhaobincai@webank.com>
2023-07-07 18:29:46 +0800

    remove the Notice to Workspace

---
+++ dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/NoticeService.java
@@ -0,0 +1,11 @@
+package com.webank.wedatasphere.dss.framework.workspace.service;
+
+import com.webank.wedatasphere.dss.framework.workspace.bean.NoticeContent;
+
+import java.util.List;
+
+public interface NoticeService {
+
+    List<NoticeContent> getNoticeContent();
+
+}

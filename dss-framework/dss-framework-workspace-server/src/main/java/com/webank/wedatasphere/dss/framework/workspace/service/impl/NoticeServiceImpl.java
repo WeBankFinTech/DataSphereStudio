92197132c9ecef2a8dd6a3ca2b99f6881029f916
zhaobincai <zhaobincai@webank.com>
2023-07-07 18:29:46 +0800

    remove the Notice to Workspace

--- dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/impl/NoticeServiceImpl.java
+++ dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/impl/NoticeServiceImpl.java
@@ -1,9 +1,9 @@
-package com.webank.wedatasphere.dss.common.service.impl;
+package com.webank.wedatasphere.dss.framework.workspace.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.webank.wedatasphere.dss.common.dao.NoticeMapper;
-import com.webank.wedatasphere.dss.common.entity.NoticeContent;
-import com.webank.wedatasphere.dss.common.service.NoticeService;
+import com.webank.wedatasphere.dss.framework.workspace.bean.NoticeContent;
+import com.webank.wedatasphere.dss.framework.workspace.dao.NoticeMapper;
+import com.webank.wedatasphere.dss.framework.workspace.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +11,7 @@ import java.util.Date;
 import java.util.List;
 
 @Service
-public class NoticeServiceImpl  implements NoticeService {
+public class NoticeServiceImpl implements NoticeService {
 
     @Autowired
     private NoticeMapper noticeMapper;

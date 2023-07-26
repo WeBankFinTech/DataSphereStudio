692cb83ff566950c100efbe96783976276943cd2
ryanlei <ryanlei@webank.com>
2023-07-26 09:57:10 +0800

    开源测试发现的问题

--- web/packages/shared/components/dynamicForm/index.vue
+++ web/packages/shared/components/dynamicForm/index.vue
@@ -2,7 +2,7 @@
   <div class="dynamic-form">
     <div class="dynamic-form-header-wrap">
       <h4 class="dynamic-form-header-title">{{ title }}</h4>
-      <a target="_blank" href="/_book/知识库/用户手册/Scriptis/变量管理.html">{{ $t('message.common.Help') }}</a>
+      <a v-if="$APP_CONF && $APP_CONF.showScirptVarHelp !== false" target="_blank" href="/_book/知识库/用户手册/Scriptis/变量管理.html">{{ $t('message.common.Help') }}</a>
     </div>
     <Form ref="dynamicForm" :model="formDynamic" class="dynamic-form-content">
       <FormItem v-for="(item, index) in formDynamic.list" :key="index">

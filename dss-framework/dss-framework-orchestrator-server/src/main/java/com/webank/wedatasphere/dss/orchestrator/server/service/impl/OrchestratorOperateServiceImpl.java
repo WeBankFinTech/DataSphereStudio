8fea8fc458939488d71f2cb9836cbca41a2cc123
yuankang134 <373346037@qq.com>
2023-07-31 15:13:56 +0800

    add OrchestratorOperateService

---
+++ dss-framework/dss-framework-orchestrator-server/src/main/java/com/webank/wedatasphere/dss/orchestrator/server/service/impl/OrchestratorOperateServiceImpl.java
@@ -0,0 +1,12 @@
+package com.webank.wedatasphere.dss.orchestrator.server.service.impl;
+
+import com.webank.wedatasphere.dss.orchestrator.server.service.OrchestratorOperateService;
+
+public class OrchestratorOperateServiceImpl implements OrchestratorOperateService {
+
+    @Override
+    public void deleteTemplateOperate(Long orchestratorId) {
+        // delete operate
+    }
+
+}

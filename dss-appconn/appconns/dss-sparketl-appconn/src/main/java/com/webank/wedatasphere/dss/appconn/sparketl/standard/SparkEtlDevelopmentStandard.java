7b95fddd1afabac1f7af82b702cb134bc331c57f
wushengyeyouya <690574002@qq.com>
2023-08-11 11:21:18 +0800

    SparkEtl AppConn optimization, support to query jumpUrl.

--- dss-appconn/appconns/dss-sparketl-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/sparketl/standard/SparkEtlDevelopmentStandard.java
+++ dss-appconn/appconns/dss-sparketl-appconn/src/main/java/com/webank/wedatasphere/dss/appconn/sparketl/standard/SparkEtlDevelopmentStandard.java
@@ -1,7 +1,9 @@
 package com.webank.wedatasphere.dss.appconn.sparketl.standard;
 
 import com.webank.wedatasphere.dss.appconn.sparketl.execution.SparkEtlExecutionService;
+import com.webank.wedatasphere.dss.appconn.sparketl.query.SparkEtlRefQueryService;
 import com.webank.wedatasphere.dss.standard.app.development.service.RefExecutionService;
+import com.webank.wedatasphere.dss.standard.app.development.service.RefQueryService;
 import com.webank.wedatasphere.dss.standard.app.development.standard.OnlyExecutionDevelopmentStandard;
 
 public class SparkEtlDevelopmentStandard extends OnlyExecutionDevelopmentStandard {
@@ -9,6 +11,11 @@ public class SparkEtlDevelopmentStandard extends OnlyExecutionDevelopmentStandar
     @Override
     protected RefExecutionService createRefExecutionService() {
         return new SparkEtlExecutionService();
+    }
+
+    @Override
+    protected RefQueryService createRefQueryService() {
+        return new SparkEtlRefQueryService();
     }
 
     @Override

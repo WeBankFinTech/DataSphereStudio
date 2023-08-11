79bf43831c053509835c0cc79a050ec57eddd757
wushengyeyouya <690574002@qq.com>
2023-08-11 11:19:55 +0800

    support to execute AppConn2Linkis and bml2linkis node type.

--- dss-orchestrator/orchestrators/dss-workflow/dss-linkis-node-execution/src/main/java/com/webank/wedatasphere/dss/linkis/node/execution/execution/impl/LinkisNodeExecutionImpl.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-linkis-node-execution/src/main/java/com/webank/wedatasphere/dss/linkis/node/execution/execution/impl/LinkisNodeExecutionImpl.java
@@ -21,10 +21,7 @@ import com.webank.wedatasphere.dss.linkis.node.execution.exception.LinkisJobExec
 import com.webank.wedatasphere.dss.linkis.node.execution.execution.LinkisNodeExecution;
 import com.webank.wedatasphere.dss.linkis.node.execution.job.Job;
 import com.webank.wedatasphere.dss.linkis.node.execution.listener.LinkisExecutionListener;
-import com.webank.wedatasphere.dss.linkis.node.execution.parser.CodeParser;
-import com.webank.wedatasphere.dss.linkis.node.execution.parser.JobParamsParser;
-import com.webank.wedatasphere.dss.linkis.node.execution.parser.JobParser;
-import com.webank.wedatasphere.dss.linkis.node.execution.parser.JobRuntimeParamsParser;
+import com.webank.wedatasphere.dss.linkis.node.execution.parser.*;
 import com.webank.wedatasphere.dss.linkis.node.execution.service.LinkisURLService;
 import com.webank.wedatasphere.dss.linkis.node.execution.service.impl.BuildJobActionImpl;
 import com.webank.wedatasphere.dss.linkis.node.execution.utils.LinkisJobExecutionUtils;
@@ -59,6 +56,8 @@ public class LinkisNodeExecutionImpl implements LinkisNodeExecution , LinkisExec
         registerJobParser(new CodeParser());
         registerJobParser(new JobRuntimeParamsParser());
         registerJobParser(new JobParamsParser());
+        registerJobParser(new BML2LinkisJobParser());
+        registerJobParser(new AppConn2LinkisJobParser());
     }
 
     public static LinkisNodeExecution getLinkisNodeExecution() {

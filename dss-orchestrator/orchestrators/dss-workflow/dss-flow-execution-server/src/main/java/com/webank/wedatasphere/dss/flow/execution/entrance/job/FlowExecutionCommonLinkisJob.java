684be92fae0a8f20f5c5ba606eea620b2dad6be6
xlinliu <xlinliu@163.com>
2023-08-25 17:11:55 +0800

    工作流实时执行支持代理用户执行

--- dss-orchestrator/orchestrators/dss-workflow/dss-flow-execution-server/src/main/java/com/webank/wedatasphere/dss/flow/execution/entrance/job/FlowExecutionCommonLinkisJob.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-flow-execution-server/src/main/java/com/webank/wedatasphere/dss/flow/execution/entrance/job/FlowExecutionCommonLinkisJob.java
@@ -34,7 +34,7 @@ public class FlowExecutionCommonLinkisJob extends AbstractCommonLinkisJob {
     @Override
     public String getUser() {
         String labels = getJobProps().getOrDefault(DSSCommonUtils.DSS_LABELS_KEY, DSSCommonUtils.ENV_LABEL_VALUE_DEV);
-        boolean executeByProxyUserInDevEnable = "true".equalsIgnoreCase(getJobProps().get(DSSCommonUtils.DSS_LABELS_KEY));
+        boolean executeByProxyUserInDevEnable = "true".equalsIgnoreCase(getJobProps().get(DSSCommonUtils.DSS_EXECUTE_BY_PROXY_USER_KEY));
         String submitUser = getSubmitUser();
         if(DSSCommonUtils.ENV_LABEL_VALUE_DEV.equals(labels) && !executeByProxyUserInDevEnable) {
             return submitUser;

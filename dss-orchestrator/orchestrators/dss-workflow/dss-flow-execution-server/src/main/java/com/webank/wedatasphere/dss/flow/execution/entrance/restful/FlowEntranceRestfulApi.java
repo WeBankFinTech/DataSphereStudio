05aaf8ac5847813507ae6dfbf603d928aa3a0053
xlinliu <xlinliu@163.com>
2023-08-26 10:49:26 +0800

    fix

--- dss-orchestrator/orchestrators/dss-workflow/dss-flow-execution-server/src/main/java/com/webank/wedatasphere/dss/flow/execution/entrance/restful/FlowEntranceRestfulApi.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-flow-execution-server/src/main/java/com/webank/wedatasphere/dss/flow/execution/entrance/restful/FlowEntranceRestfulApi.java
@@ -176,9 +176,9 @@ public class FlowEntranceRestfulApi extends EntranceRestfulApi {
         logger.info("End to get status for execId:{}", id);
         return message;
     }
-
+    @Override
     @RequestMapping(path = {"/{id}/kill"},method = {RequestMethod.GET})
-    public Message kill(@PathVariable("id") String id, @RequestParam(value = "taskID",required = false) Long taskID) {
+    public Message kill(HttpServletRequest req @PathVariable("id") String id, @RequestParam(value = "taskID",required = false) Long taskID) {
         logger.info("Begin to kill task:{}", id);
         String realId = ZuulEntranceUtils.parseExecID(id)[3];
         Option job;

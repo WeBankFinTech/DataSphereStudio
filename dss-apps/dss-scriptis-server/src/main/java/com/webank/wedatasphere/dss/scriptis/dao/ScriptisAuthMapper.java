59f1268944d6d000e370d89bbdfff60fe490a5db
wuxb <wuxb78@qq.com>
2023-09-05 17:05:57 +0800

    Partial user restrictions

--- dss-apps/dss-scriptis-server/src/main/java/com/webank/wedatasphere/dss/scriptis/dao/ScriptisAuthMapper.java
+++ dss-apps/dss-scriptis-server/src/main/java/com/webank/wedatasphere/dss/scriptis/dao/ScriptisAuthMapper.java
@@ -1,13 +1,13 @@
 package com.webank.wedatasphere.dss.scriptis.dao;
 
 
-import com.webank.wedatasphere.dss.scriptis.pojo.entity.DssConfig;
+import com.webank.wedatasphere.dss.scriptis.pojo.entity.DssUserLimit;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface ScriptisAuthMapper {
 
-    List<DssConfig> getUserLimits(@Param("limitName") String limitName);
+    List<DssUserLimit> getUserLimits(@Param("limitName") String limitName);
 
 }

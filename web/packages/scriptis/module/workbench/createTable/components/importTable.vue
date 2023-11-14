0f0030e51b62de64af86cd7de72f6cce5235b9f8
ryanlei <ryanlei@webank.com>
2023-11-14 11:30:47 +0800

    version 1.1.17

--- web/packages/scriptis/module/workbench/createTable/components/importTable.vue
+++ web/packages/scriptis/module/workbench/createTable/components/importTable.vue
@@ -16,7 +16,9 @@
         :options="source.table"
         :sheetArray="target.sheetName"
         @isPathLeaf="$emit('isPathLeaf', arguments)"
-        @get-fields="getFields"></csv>
+        @get-fields="getFields"
+        @clear-fields="handleClearFields"
+        @on-change="handleSheetChange"></csv>
     </Card>
     <Card>
       <p slot="title">
@@ -27,6 +29,7 @@
         class="import-table-fields"
         :modal="target.importFieldsData">
         <fields-table
+          :key="fieldTableKey"
           :fields="target.importFieldsData.fields"
           :table-columns="source.importType === 'hive' ? hiveFieldsColumns : csvOrXlsxfieldsColumns"
           :can-be-delete="true"
@@ -93,6 +96,7 @@ export default {
       partTableColumns: config.importTablePartitionsConfig,
       specialFieldKey: 'sourceFields',
       activedFieldItem: null,
+      fieldTableKey: '',
     };
   },
   methods: {
@@ -121,18 +125,17 @@ export default {
         escapeQuotes = true;
         quote = option.quote;
       }
-      const url = `/filesystem/formate?path=${option.exportPath}&encoding=${encoding}&fieldDelimiter=${fieldDelimiter}&hasHeader=${option.isHasHeader}&escapeQuotes=${escapeQuotes}&quote=${quote}`;
+      const url = `/filesystem/getSheetInfo?path=${option.exportPath}&encoding=${encoding}&fieldDelimiter=${fieldDelimiter}&hasHeader=${option.isHasHeader}&escapeQuotes=${escapeQuotes}&quote=${quote}`;
       api.fetch(url, {}, {
         method: 'get',
         timeout: '600000',
       }).then((rst) => {
-        if (!rst.formate.columnName.length) {
-          return this.target.importFieldsData.fields = [];
-        }
-        this.target.importFieldsData.fields = rst.formate.columnName.map((field, index) => {
+        const sheetList = this.buildList(rst.sheetInfo || {});
+        this.target.sheetName = sheetList;
+        this.target.importFieldsData.fields = this.buildList(sheetList[0].value || {}).map((item, index) => {
           return {
-            name: field,
-            type: '',
+            name: item.label,
+            type: item.value,
             alias: '',
             sourceFields: '',
             rule: '',
@@ -144,18 +147,48 @@ export default {
             index
           };
         });
-        rst.formate.columnType.forEach((type, index) => {
-          this.target.importFieldsData.fields[index].type = type;
-        });
-        this.target.importFieldsData.fields = this.target.importFieldsData.fields.slice();
-        this.target.sheetName = rst.formate.sheetName;
-        if(this.target.sheetName.length > 0) {
-          this.source.table.sheet = [this.target.sheetName[0]];
+        if(sheetList.length > 0) {
+          this.source.table.sheet = sheetList[0].label;
         } else {
-          this.source.table.sheet = [];
+          this.source.table.sheet = '';
         }
       }).catch(() => {
       });
+    },
+    handleSheetChange(val) {
+      this.fieldTableKey = val;
+      if(val) {
+        const item = this.target.sheetName.find(item => item.label === val).value || {};
+        const arr = this.buildList(item);
+        this.target.importFieldsData.fields  = arr.map((item, index) => {
+          return {
+            name: item.label,
+            type: item.value,
+            alias: '',
+            sourceFields: '',
+            rule: '',
+            partitionField: 0,
+            primary: 0,
+            comment: '',
+            length: '',
+            express: '',
+            index
+          };
+        });
+      } else {
+        this.target.importFieldsData.fields = [];
+      }
+    },
+    buildList(obj) {
+      let res = [];
+      Object.keys(obj).forEach(key => {
+        res.push({ value: obj[key], label: key })
+      })
+      return res;
+    },
+    handleClearFields() {
+      this.target.sheetName = [];
+      this.target.importFieldsData.fields = [];
     },
     onDelete(item, index) {
       this.target.importFieldsData.fields.splice(index, 1);

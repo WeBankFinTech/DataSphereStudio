04ef2296a9cc8b2315befc9950c02e4b46e11219
ryanlei <ryanlei@webank.com>
2023-12-13 09:57:21 +0800

    1.1.18 列筛选

--- web/packages/scriptis/module/hiveSidebar/index.js
+++ web/packages/scriptis/module/hiveSidebar/index.js
@@ -23,7 +23,7 @@ export default {
   dispatchs: ['Workbench:add', 'Workbench:run', 'Workbench:pasteInEditor', 'HiveSidebar:getTables', 'WorkSidebar:showTree', 'HdfsSidebar:showTree', 'IndexedDB:getTree', 'IndexedDB:appendTree'],
   data: {
     API_PATH: process.env.VUE_APP_MN_CONFIG_PREFIX || `http://${window.location.host}/api/rest_j/v1/`,
-    ALLOW_MAP: ['_ind', '_share', '_work', '_qml'],
+    ALLOW_MAP: ['_ind', '_share', '_work', '_qml', '_tmp', '_bak'],
   },
   methods: {
     getAllDbsAndTables() {

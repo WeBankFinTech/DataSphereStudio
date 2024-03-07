61349f070b5f9ffb427e681a0f2700e3f4ec4ba8
yuankang134 <373346037@qq.com>
2024-03-07 09:58:01 +0800

    bug fix

--- dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/impl/DSSWorkspaceServiceImpl.java
+++ dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/impl/DSSWorkspaceServiceImpl.java
@@ -229,7 +229,7 @@ public class DSSWorkspaceServiceImpl implements DSSWorkspaceService {
         String roleId = StringUtils.isBlank(roleName) ? null : String.valueOf(workspaceDBHelper.getRoleIdByName(roleName));
         PageHelper.startPage(pageNow, pageSize);
         // 转译用户名模糊查询中的特殊字符
-        String queryName = username.contains("_") ? username.split("_")[0] + "\\" + username.split("_")[1] : username;
+        String queryName = StringUtils.isNotEmpty(username) && username.contains("_") ? username.split("_")[0] + "\\" + username.split("_")[1] : username;
         List<DSSWorkspaceUser> workspaceUsers = dssWorkspaceUserMapper.getWorkspaceUsers(workspaceId, queryName, roleId);
         PageInfo<DSSWorkspaceUser> pageInfo = new PageInfo<>(workspaceUsers);
         total.add(pageInfo.getTotal());

9765d3bb76bcee59b61555c4d44b83f712aa2d00
zhaobincai <zhaobincai@webank.com>
2024-04-02 11:54:42 +0800

    git workspace add

--- dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/impl/DSSWorkspaceGitServiceImpl.java
+++ dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/impl/DSSWorkspaceGitServiceImpl.java
@@ -4,7 +4,9 @@ import com.webank.wedatasphere.dss.framework.workspace.bean.GitUserEntity;
 import com.webank.wedatasphere.dss.framework.workspace.dao.DSSWorkspaceGitMapper;
 import com.webank.wedatasphere.dss.framework.workspace.service.DSSWorkspaceGitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DSSWorkspaceGitServiceImpl implements DSSWorkspaceGitService {
 
     @Autowired

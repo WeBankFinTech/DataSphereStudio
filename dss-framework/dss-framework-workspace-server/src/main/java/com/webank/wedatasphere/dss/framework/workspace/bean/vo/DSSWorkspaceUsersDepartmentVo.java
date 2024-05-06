09de82868c5d43b766014d1b91af82e54c19cf31
nullpoint12138 <zhouxuannihao@qq.com>
2024-05-06 12:22:33 +0800

    fix error

---
+++ dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/bean/vo/DSSWorkspaceUsersDepartmentVo.java
@@ -0,0 +1,25 @@
+package com.webank.wedatasphere.dss.framework.workspace.bean.vo;
+
+import java.util.List;
+
+public class DSSWorkspaceUsersDepartmentVo {
+
+
+    private List<DepartmentUserVo> editUsers;
+
+    private List<DepartmentUserVo> accessUsers;
+
+    private List<DepartmentUserVo> releaseUsers;
+
+    public void setAccessUsers(List<DepartmentUserVo> accessUsers) {
+        this.accessUsers = accessUsers;
+    }
+
+    public void setReleaseUsers(List<DepartmentUserVo> releaseUsers) {
+        this.releaseUsers = releaseUsers;
+    }
+
+    public void setEditUsers(List<DepartmentUserVo> editUsers) {
+        this.editUsers = editUsers;
+    }
+}

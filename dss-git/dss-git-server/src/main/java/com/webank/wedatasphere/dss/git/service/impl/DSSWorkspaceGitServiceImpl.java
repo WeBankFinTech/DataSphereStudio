aecad25e7090228e2dda4bf558da082dad156541
qweqnotallow <747839893@qq.com>
2024-05-23 09:34:39 +0800

    git server : password token encrypt

--- dss-git/dss-git-server/src/main/java/com/webank/wedatasphere/dss/git/service/impl/DSSWorkspaceGitServiceImpl.java
+++ dss-git/dss-git-server/src/main/java/com/webank/wedatasphere/dss/git/service/impl/DSSWorkspaceGitServiceImpl.java
@@ -2,14 +2,28 @@ package com.webank.wedatasphere.dss.git.service.impl;
 
 import com.webank.wedatasphere.dss.common.exception.DSSErrorException;
 import com.webank.wedatasphere.dss.git.common.protocol.GitUserEntity;
+import com.webank.wedatasphere.dss.git.common.protocol.config.GitServerConfig;
+import com.webank.wedatasphere.dss.git.common.protocol.constant.GitConstant;
 import com.webank.wedatasphere.dss.git.common.protocol.request.GitUserUpdateRequest;
 import com.webank.wedatasphere.dss.git.common.protocol.request.GitUserInfoRequest;
 import com.webank.wedatasphere.dss.git.common.protocol.response.GitUserUpdateResponse;
 import com.webank.wedatasphere.dss.git.common.protocol.response.GitUserInfoResponse;
+import com.webank.wedatasphere.dss.git.constant.DSSGitConstant;
 import com.webank.wedatasphere.dss.git.dao.DSSWorkspaceGitMapper;
 import com.webank.wedatasphere.dss.git.service.DSSWorkspaceGitService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 @Service
 public class DSSWorkspaceGitServiceImpl implements DSSWorkspaceGitService {
@@ -21,13 +35,22 @@ public class DSSWorkspaceGitServiceImpl implements DSSWorkspaceGitService {
     @Override
     public GitUserUpdateResponse associateGit(GitUserUpdateRequest gitUserCreateRequest) throws DSSErrorException {
         if (gitUserCreateRequest == null) {
-            throw  new DSSErrorException(010101, "gitUserCreateRequest is null");
+            throw new DSSErrorException(010101, "gitUserCreateRequest is null");
         }
         GitUserEntity gitUser = gitUserCreateRequest.getGitUser();
         String userName = gitUserCreateRequest.getUsername();
         // 不存在则更新，存在则新增
         GitUserEntity oldGitUserDo = selectGit(gitUser.getWorkspaceId(), gitUser.getType());
         gitUser.setUpdateBy(userName);
+        // 密码 token 加密处理
+        if (!StringUtils.isEmpty(gitUser.getGitPassword())) {
+            String encryptPassword = generateKeys(gitUser.getGitPassword(), Cipher.ENCRYPT_MODE);
+            gitUser.setGitPassword(encryptPassword);
+        }
+        if (!StringUtils.isEmpty(gitUser.getGitToken())) {
+            String encryptToken = generateKeys(gitUser.getGitToken(), Cipher.ENCRYPT_MODE);
+            gitUser.setGitToken(encryptToken);
+        }
         if (oldGitUserDo == null) {
             gitUser.setCreateBy(userName);
             workspaceGitMapper.insert(gitUser);
@@ -47,6 +70,18 @@ public class DSSWorkspaceGitServiceImpl implements DSSWorkspaceGitService {
         Long workspaceId = gitUserInfoRequest.getWorkspaceId();
         String type = gitUserInfoRequest.getType();
         GitUserEntity gitUserEntity = selectGit(workspaceId, type);
+        Boolean decrypt = gitUserInfoRequest.getDecrypt();
+        if (decrypt != null && decrypt) {
+            // 密码 token 解密处理
+            if (!StringUtils.isEmpty(gitUserEntity.getGitPassword())) {
+                String encryptPassword = generateKeys(gitUserEntity.getGitPassword(), Cipher.DECRYPT_MODE);
+                gitUserEntity.setGitPassword(encryptPassword);
+            }
+            if (!StringUtils.isEmpty(gitUserEntity.getGitToken())) {
+                String encryptToken = generateKeys(gitUserEntity.getGitToken(), Cipher.DECRYPT_MODE);
+                gitUserEntity.setGitToken(encryptToken);
+            }
+        }
 
         GitUserInfoResponse gitUserInfoResponse = new GitUserInfoResponse();
         gitUserInfoResponse.setGitUser(gitUserEntity);
@@ -55,7 +90,30 @@ public class DSSWorkspaceGitServiceImpl implements DSSWorkspaceGitService {
 
     @Override
     public GitUserEntity selectGit(Long workspaceId, String type) {
-        return workspaceGitMapper.selectByWorkspaceId(workspaceId, type);
+        GitUserEntity gitUserEntity = workspaceGitMapper.selectByWorkspaceId(workspaceId, type);
+        String decryptPassword = generateKeys(gitUserEntity.getGitToken(), Cipher.DECRYPT_MODE);
+        gitUserEntity.setGitToken(decryptPassword);
+        return gitUserEntity;
+    }
+
+    private String generateKeys(String password, int mode) {
+        // 定义一个字符串作为密钥源
+        String keyString = GitServerConfig.LINKIS_MYSQL_PUB_KEY.getValue();
+        if (keyString.length() < 16) {
+            throw new DSSErrorException(800001, "密钥长度必须大于16位");
+        }
+        try {
+            // 确保密钥长度合适，AES 密钥长度为 128 位 (16 字节)
+            byte[] keyBytes = keyString.substring(0, 16).getBytes();
+            Key key = new SecretKeySpec(keyBytes, "AES");
+            // 加密Cipher对象
+            Cipher encryptCipher = Cipher.getInstance("AES");
+            encryptCipher.init(mode, key);
+            byte[] encrypted = encryptCipher.doFinal(password.getBytes());
+            return Arrays.toString(encrypted);
+        } catch (Exception e) {
+            throw new DSSErrorException(800001, "加密失败");
+        }
     }
 
 }

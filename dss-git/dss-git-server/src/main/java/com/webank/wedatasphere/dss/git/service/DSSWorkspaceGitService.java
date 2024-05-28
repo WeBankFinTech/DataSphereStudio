98e4ae2255edaee12173124aefe9cd1c630ddfe5
Zhaobincai <747839893@qq.com>
2024-05-28 11:49:17 +0800

    dss-server: add associateGit to workspaces

--- dss-git/dss-git-server/src/main/java/com/webank/wedatasphere/dss/git/service/DSSWorkspaceGitService.java
+++ dss-git/dss-git-server/src/main/java/com/webank/wedatasphere/dss/git/service/DSSWorkspaceGitService.java
@@ -3,9 +3,11 @@ package com.webank.wedatasphere.dss.git.service;
 import com.webank.wedatasphere.dss.common.exception.DSSErrorException;
 import com.webank.wedatasphere.dss.git.common.protocol.GitUserEntity;
 import com.webank.wedatasphere.dss.git.common.protocol.request.GitConnectRequest;
+import com.webank.wedatasphere.dss.git.common.protocol.request.GitUserInfoByRequest;
 import com.webank.wedatasphere.dss.git.common.protocol.request.GitUserUpdateRequest;
 import com.webank.wedatasphere.dss.git.common.protocol.request.GitUserInfoRequest;
 import com.webank.wedatasphere.dss.git.common.protocol.response.GitConnectResponse;
+import com.webank.wedatasphere.dss.git.common.protocol.response.GitUserInfoListResponse;
 import com.webank.wedatasphere.dss.git.common.protocol.response.GitUserUpdateResponse;
 import com.webank.wedatasphere.dss.git.common.protocol.response.GitUserInfoResponse;
 
@@ -18,4 +20,6 @@ public interface DSSWorkspaceGitService {
     GitUserEntity selectGit(Long workspaceId, String type, Boolean decrypt);
 
     GitConnectResponse gitTokenTest(GitConnectRequest connectTestRequest) throws DSSErrorException;
+
+    GitUserInfoListResponse getGitUserByType(GitUserInfoByRequest typeRequest);
 }

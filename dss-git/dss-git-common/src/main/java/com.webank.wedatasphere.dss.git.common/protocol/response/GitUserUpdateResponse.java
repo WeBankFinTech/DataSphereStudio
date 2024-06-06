9f8f82e43a36950d97a0af92b00ec22b701483d5
Zhaobincai <747839893@qq.com>
2024-06-06 10:28:42 +0800

    git-server: fix errMsg

--- dss-git/dss-git-common/src/main/java/com.webank.wedatasphere.dss.git.common/protocol/response/GitUserUpdateResponse.java
+++ dss-git/dss-git-common/src/main/java/com.webank.wedatasphere.dss.git.common/protocol/response/GitUserUpdateResponse.java
@@ -5,6 +5,14 @@ public class GitUserUpdateResponse {
     private int status;
     private String msg;
 
+    private Long workspaceId;
+
+    public GitUserUpdateResponse(int status, String msg, Long workspaceId) {
+        this.status = status;
+        this.msg = msg;
+        this.workspaceId = workspaceId;
+    }
+
     public int getStatus() {
         return status;
     }
@@ -19,5 +27,13 @@ public class GitUserUpdateResponse {
 
     public void setMsg(String msg) {
         this.msg = msg;
+    }
+
+    public Long getWorkspaceId() {
+        return workspaceId;
+    }
+
+    public void setWorkspaceId(Long workspaceId) {
+        this.workspaceId = workspaceId;
     }
 }

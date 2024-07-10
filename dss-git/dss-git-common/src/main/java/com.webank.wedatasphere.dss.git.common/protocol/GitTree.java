7b962ae0c901eaf8ce31100cce8a294aef29754e
Zhaobincai <747839893@qq.com>
2024-07-10 19:33:59 +0800

    add diffFlow

--- dss-git/dss-git-common/src/main/java/com.webank.wedatasphere.dss.git.common/protocol/GitTree.java
+++ dss-git/dss-git-common/src/main/java/com.webank.wedatasphere.dss.git.common/protocol/GitTree.java
@@ -5,23 +5,36 @@ import java.util.Map;
 
 public class GitTree {
     private String name;
+    // 标识当前变动路径状态 modified-修改 missing-删除 untracked-新增
+    private String status;
+    private Boolean meta;
     private Map<String, GitTree> children = new HashMap<>();
 
     public GitTree(String name) {
         this.name = name;
     }
 
+    public GitTree(String name, Boolean meta) {
+        this.name = name;
+        this.meta = meta;
+    }
+
     // 添加子节点
-    public void addChild(String path) {
+    public void addChild(String path, String status) {
         String[] parts = path.split("/", 2);
         String currentPart = parts[0];
         String restPart = parts.length > 1 ? parts[1] : null;
 
-        children.putIfAbsent(currentPart, new GitTree(currentPart));
+        GitTree gitTree = new GitTree(currentPart);
+        // 标识当前文件状态
+        if (restPart == null) {
+            gitTree.setStatus(status);
+        }
+        children.putIfAbsent(currentPart, gitTree);
         GitTree child = children.get(currentPart);
 
         if (restPart != null) {
-            child.addChild(restPart);
+            child.addChild(restPart, status);
         }
     }
 
@@ -40,5 +53,21 @@ public class GitTree {
 
     public void setChildren(Map<String, GitTree> children) {
         this.children = children;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Boolean getMeta() {
+        return meta;
+    }
+
+    public void setMeta(Boolean meta) {
+        this.meta = meta;
     }
 }

dff1ecceade363c4e285a4d7b7aba969d7a85386
Zhaobincai <747839893@qq.com>
2024-07-11 11:46:20 +0800

    add diffFlowContent

--- dss-git/dss-git-common/src/main/java/com.webank.wedatasphere.dss.git.common/protocol/request/GitFileContentRequest.java
+++ dss-git/dss-git-common/src/main/java/com.webank.wedatasphere.dss.git.common/protocol/request/GitFileContentRequest.java
@@ -1,6 +1,10 @@
 package com.webank.wedatasphere.dss.git.common.protocol.request;
 
 
+import com.webank.wedatasphere.dss.common.entity.BmlResource;
+
+import java.util.Map;
+
 public class GitFileContentRequest extends GitBaseRequest{
     private String commitId;
     /**
@@ -8,19 +12,39 @@ public class GitFileContentRequest extends GitBaseRequest{
      */
     private String filePath;
 
+    /**
+     * 待修改的文件（key：文件相对路径，value：文件对应BML）
+     */
+    private Map<String, BmlResource> bmlResourceMap;
+
+    private String username;
+
+    private Boolean publish;
+
     public GitFileContentRequest() {
     }
 
-    public GitFileContentRequest(String commitId, String filePath) {
-        this.commitId = commitId;
-        this.filePath = filePath;
+    public GitFileContentRequest(Long workspaceId, String projectName) {
+        super(workspaceId, projectName);
     }
 
-    public GitFileContentRequest(Long workspaceId, String projectName, String commitId, String filePath) {
+    public GitFileContentRequest(String commitId, String filePath, Map<String, BmlResource> bmlResourceMap, String username, Boolean publish) {
+        this.commitId = commitId;
+        this.filePath = filePath;
+        this.bmlResourceMap = bmlResourceMap;
+        this.username = username;
+        this.publish = publish;
+    }
+
+    public GitFileContentRequest(Long workspaceId, String projectName, String commitId, String filePath, Map<String, BmlResource> bmlResourceMap, String username, Boolean publish) {
         super(workspaceId, projectName);
         this.commitId = commitId;
         this.filePath = filePath;
+        this.bmlResourceMap = bmlResourceMap;
+        this.username = username;
+        this.publish = publish;
     }
+
 
     public String getCommitId() {
         return commitId;
@@ -38,4 +62,27 @@ public class GitFileContentRequest extends GitBaseRequest{
         this.filePath = filePath;
     }
 
+    public Boolean getPublish() {
+        return publish;
+    }
+
+    public void setPublish(Boolean publish) {
+        this.publish = publish;
+    }
+
+    public Map<String, BmlResource> getBmlResourceMap() {
+        return bmlResourceMap;
+    }
+
+    public void setBmlResourceMap(Map<String, BmlResource> bmlResourceMap) {
+        this.bmlResourceMap = bmlResourceMap;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }

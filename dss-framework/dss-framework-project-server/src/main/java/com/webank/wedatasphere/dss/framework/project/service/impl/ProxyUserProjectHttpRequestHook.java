08192da62a19a4b55997b047bcc64fe21d855a65
SunpengWan <825559776@qq.com>
2024-07-12 12:47:21 +0800

    add line space

--- dss-framework/dss-framework-project-server/src/main/java/com/webank/wedatasphere/dss/framework/project/service/impl/ProxyUserProjectHttpRequestHook.java
+++ dss-framework/dss-framework-project-server/src/main/java/com/webank/wedatasphere/dss/framework/project/service/impl/ProxyUserProjectHttpRequestHook.java
@@ -136,6 +136,7 @@ public class ProxyUserProjectHttpRequestHook implements ProjectHttpRequestHook {
                 projectModifyRequest.setReleaseUsers(projectResponseList.get(0).getReleaseUsers());
             }
 
+
             return null;
         });
     }

7f33dc15c8d2ad4af1e9b9b3f350e5613b74a202
Alexkun <xuanyu710@qq.com>
2024-07-15 22:23:25 +0800

    1，refactor config GLOBAL_CIPILOT_WHITELIST

--- dss-commons/dss-common/src/main/java/com/webank/wedatasphere/dss/common/conf/DSSCommonConf.java
+++ dss-commons/dss-common/src/main/java/com/webank/wedatasphere/dss/common/conf/DSSCommonConf.java
@@ -37,12 +37,6 @@ public class DSSCommonConf {
 
     public static final CommonVars<String> GLOBAL_LIMIT_PREFIX = CommonVars.apply("wds.dss.global.limit.prefix", "wds.dss.global.limit.");
 
-    public static final CommonVars<String> GLOBAL_COPILOT_WHITELIST = CommonVars.apply("wds.dss.global.copilot.whitelist", "hadoop");
-
-    public static final String DELIMITER_COMMA = ",";
-
-    public static final String COPILOT_ENABLE_KEY = "copilotEnable";
-
     /**
      * deploy DSS instance numbers
      */

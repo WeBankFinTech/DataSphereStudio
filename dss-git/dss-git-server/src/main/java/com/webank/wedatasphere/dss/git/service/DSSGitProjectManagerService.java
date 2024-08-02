6cec6af3b5a7749b561bcb5e5acc2ce2ad6a4dd5
Zhaobincai <747839893@qq.com>
2024-08-02 15:40:29 +0800

    getAllProjects: fix gitUser gitToken

--- dss-git/dss-git-server/src/main/java/com/webank/wedatasphere/dss/git/service/DSSGitProjectManagerService.java
+++ dss-git/dss-git-server/src/main/java/com/webank/wedatasphere/dss/git/service/DSSGitProjectManagerService.java
@@ -14,4 +14,6 @@ public interface DSSGitProjectManagerService {
 
     GitCheckProjectResponse checkProject(GitCheckProjectRequest request) throws DSSErrorException;
 
+    GitUserByWorkspaceResponse getProjectGitUserInfo(GitUserByWorkspaceIdRequest request);
+
 }

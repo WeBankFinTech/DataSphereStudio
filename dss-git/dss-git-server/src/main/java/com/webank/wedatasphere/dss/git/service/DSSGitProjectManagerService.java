2a335de0b3a9f68aed045afdd71797cda604d09e
Zhaobincai <747839893@qq.com>
2024-08-02 09:21:32 +0800

    gitTokenTest fix4

--- dss-git/dss-git-server/src/main/java/com/webank/wedatasphere/dss/git/service/DSSGitProjectManagerService.java
+++ dss-git/dss-git-server/src/main/java/com/webank/wedatasphere/dss/git/service/DSSGitProjectManagerService.java
@@ -1,6 +1,7 @@
 package com.webank.wedatasphere.dss.git.service;
 
 import com.webank.wedatasphere.dss.common.exception.DSSErrorException;
+import com.webank.wedatasphere.dss.git.common.protocol.exception.GitErrorException;
 import com.webank.wedatasphere.dss.git.common.protocol.request.*;
 import com.webank.wedatasphere.dss.git.common.protocol.response.*;
 
@@ -9,7 +10,7 @@ import java.util.concurrent.ExecutionException;
 public interface DSSGitProjectManagerService {
     GitCreateProjectResponse create(GitCreateProjectRequest request) throws DSSErrorException;
 
-    GitArchivePorjectResponse archive(GitArchiveProjectRequest request) throws DSSErrorException;
+    GitArchivePorjectResponse archive(GitArchiveProjectRequest request) throws GitErrorException, DSSErrorException;
 
     GitCheckProjectResponse checkProject(GitCheckProjectRequest request) throws DSSErrorException;
 

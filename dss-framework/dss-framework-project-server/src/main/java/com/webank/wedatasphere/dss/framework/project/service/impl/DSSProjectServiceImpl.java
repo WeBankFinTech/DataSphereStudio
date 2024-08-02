3738c61b9d7437535f47608173d4f43d40d5c0e3
Zhaobincai <747839893@qq.com>
2024-08-02 15:40:29 +0800

    getAllProjects: fix gitUser gitToken

--- dss-framework/dss-framework-project-server/src/main/java/com/webank/wedatasphere/dss/framework/project/service/impl/DSSProjectServiceImpl.java
+++ dss-framework/dss-framework-project-server/src/main/java/com/webank/wedatasphere/dss/framework/project/service/impl/DSSProjectServiceImpl.java
@@ -52,8 +52,11 @@ import com.webank.wedatasphere.dss.framework.project.service.ExportService;
 import com.webank.wedatasphere.dss.framework.project.service.ImportService;
 import com.webank.wedatasphere.dss.framework.project.utils.ProjectStringUtils;
 import com.webank.wedatasphere.dss.framework.workspace.bean.DSSWorkspaceUser;
+import com.webank.wedatasphere.dss.git.common.protocol.GitUserEntity;
 import com.webank.wedatasphere.dss.git.common.protocol.request.GitArchiveProjectRequest;
+import com.webank.wedatasphere.dss.git.common.protocol.request.GitUserByWorkspaceIdRequest;
 import com.webank.wedatasphere.dss.git.common.protocol.response.GitArchivePorjectResponse;
+import com.webank.wedatasphere.dss.git.common.protocol.response.GitUserByWorkspaceResponse;
 import com.webank.wedatasphere.dss.orchestrator.server.entity.request.OrchestratorRequest;
 import com.webank.wedatasphere.dss.orchestrator.server.entity.vo.OrchestratorBaseInfo;
 import com.webank.wedatasphere.dss.orchestrator.server.service.OrchestratorService;
@@ -218,6 +221,7 @@ public class DSSProjectServiceImpl extends ServiceImpl<DSSProjectMapper, DSSProj
             return new ArrayList<>();
         }
 
+        Map<String, GitUserEntity> projectGitUserInfo = getProjectGitUserInfo(projectRequest.getUsername(), projectRequest.getWorkspaceId());
         List<ProjectResponse> projectResponseList = new ArrayList<>();
         ProjectResponse projectResponse;
         for (QueryProjectVo projectVo : list) {
@@ -237,6 +241,13 @@ public class DSSProjectServiceImpl extends ServiceImpl<DSSProjectMapper, DSSProj
             projectResponse.setCreateTime(projectVo.getCreateTime());
             projectResponse.setUpdateTime(projectVo.getUpdateTime());
             projectResponse.setAssociateGit(projectVo.getAssociateGit());
+            if (projectVo.getAssociateGit()) {
+                GitUserEntity gitUserEntity = projectGitUserInfo.get(projectVo.getName());
+                if (gitUserEntity != null) {
+                    projectResponse.setGitUser(gitUserEntity.getGitUser());
+                    projectResponse.setGitToken(gitUserEntity.getGitToken());
+                }
+            }
             projectResponse.setDevProcessList(ProjectStringUtils.convertList(projectVo.getDevProcess()));
             projectResponse.setOrchestratorModeList(ProjectStringUtils.convertList(projectVo.getOrchestratorMode()));
             if (projectVo.getDataSourceListJson() != null) {
@@ -372,6 +383,19 @@ public class DSSProjectServiceImpl extends ServiceImpl<DSSProjectMapper, DSSProj
             GitArchivePorjectResponse responseWorkflowValidNode = RpcAskUtils.processAskException(ask, GitArchivePorjectResponse.class, GitArchiveProjectRequest.class);
             LOGGER.info("-------=======================End to archive project: {}=======================-------: {}", dssProjectDO.getName(), responseWorkflowValidNode);
         }
+    }
+
+    private Map<String, GitUserEntity> getProjectGitUserInfo(String username, Long workspaceId) {
+        Sender gitSender = DSSSenderServiceFactory.getOrCreateServiceInstance().getGitSender();
+        Map<String, BmlResource> file = new HashMap<>();
+        // 测试数据 key表示项目名、value为项目BmlResource资源
+        GitUserByWorkspaceIdRequest request1 = new GitUserByWorkspaceIdRequest();
+        request1.setWorkspaceId(workspaceId);
+        request1.setUsername(username);
+        Object ask = gitSender.ask(request1);
+        GitUserByWorkspaceResponse workspaceResponse = RpcAskUtils.processAskException(ask, GitUserByWorkspaceResponse.class, GitUserByWorkspaceIdRequest.class);
+
+        return workspaceResponse.getMap();
     }
 
     @Override

a1660382669e3c497bd0c238327985775fa775e1
Zhaobincai <747839893@qq.com>
2024-09-04 20:24:10 +0800

    update editflow: fix3

---
+++ dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/dto/NodeTypeDO.java
@@ -0,0 +1,22 @@
+package com.webank.wedatasphere.dss.workflow.dto;
+
+public class NodeTypeDO {
+    private String nodeType;
+    private String key;
+
+    public String getNodeType() {
+        return nodeType;
+    }
+
+    public void setNodeType(String nodeType) {
+        this.nodeType = nodeType;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}

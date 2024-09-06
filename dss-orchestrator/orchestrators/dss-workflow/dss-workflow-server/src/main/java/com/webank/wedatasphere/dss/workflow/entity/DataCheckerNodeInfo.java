29d7187a0afa940e0eb7939a09db89367e5ec12a
SunpengWan <825559776@qq.com>
2024-09-06 17:11:28 +0800

    fix: 节点台账 bug2

--- dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/entity/DataCheckerNodeInfo.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/entity/DataCheckerNodeInfo.java
@@ -3,31 +3,9 @@ package com.webank.wedatasphere.dss.workflow.entity;
 import java.util.Date;
 import java.util.Map;
 
-public class DataCheckerNodeInfo {
+public class DataCheckerNodeInfo extends  NodeBaseInfo {
 
-    // 项目名称
-    private String projectName;
 
-    // 工作流名称
-    private String orchestratorName;
-
-    // 工作流ID
-
-    private Long orchestratorId;
-
-    // 节点名称
-    private String nodeName;
-
-    // 节点类型
-    private String nodeType;
-
-    private String nodeKey;
-
-    // 节点小类
-    private String nodeTypeName;
-
-    // 节点ID
-    private String nodeId;
 
     // 节点描述
     private String nodeDesc;
@@ -46,70 +24,6 @@ public class DataCheckerNodeInfo {
 
     // qualities 校验
     private Boolean qualitisCheck;
-
-
-    private  Long projectId;
-
-    private Date updateTime;
-
-    private Date createTime;
-
-    private Map<String,String> nodeContent;
-
-    // 节点配置ID
-    private Long contentId;
-
-    /**
-     * 1-接入git 0-不接入（默认）
-     */
-    private Boolean associateGit;
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getOrchestratorName() {
-        return orchestratorName;
-    }
-
-    public void setOrchestratorName(String orchestratorName) {
-        this.orchestratorName = orchestratorName;
-    }
-
-    public String getNodeName() {
-        return nodeName;
-    }
-
-    public void setNodeName(String nodeName) {
-        this.nodeName = nodeName;
-    }
-
-    public String getNodeType() {
-        return nodeType;
-    }
-
-    public void setNodeType(String nodeType) {
-        this.nodeType = nodeType;
-    }
-
-    public String getNodeTypeName() {
-        return nodeTypeName;
-    }
-
-    public void setNodeTypeName(String nodeTypeName) {
-        this.nodeTypeName = nodeTypeName;
-    }
-
-    public String getNodeId() {
-        return nodeId;
-    }
-
-    public void setNodeId(String nodeId) {
-        this.nodeId = nodeId;
-    }
 
     public String getNodeDesc() {
         return nodeDesc;
@@ -157,69 +71,5 @@ public class DataCheckerNodeInfo {
 
     public void setQualitisCheck(Boolean qualitisCheck) {
         this.qualitisCheck = qualitisCheck;
-    }
-
-    public Long getOrchestratorId() {
-        return orchestratorId;
-    }
-
-    public void setOrchestratorId(Long orchestratorId) {
-        this.orchestratorId = orchestratorId;
-    }
-
-    public Long getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(Long projectId) {
-        this.projectId = projectId;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getNodeKey() {
-        return nodeKey;
-    }
-
-    public void setNodeKey(String nodeKey) {
-        this.nodeKey = nodeKey;
-    }
-
-    public Map<String, String> getNodeContent() {
-        return nodeContent;
-    }
-
-    public void setNodeContent(Map<String, String> nodeContent) {
-        this.nodeContent = nodeContent;
-    }
-
-    public Long getContentId() {
-        return contentId;
-    }
-
-    public void setContentId(Long contentId) {
-        this.contentId = contentId;
-    }
-
-    public Boolean getAssociateGit() {
-        return associateGit;
-    }
-
-    public void setAssociateGit(Boolean associateGit) {
-        this.associateGit = associateGit;
     }
 }
--- dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/entity/DataViewNodeInfo.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-workflow-server/src/main/java/com/webank/wedatasphere/dss/workflow/entity/DataViewNodeInfo.java
@@ -3,101 +3,14 @@ package com.webank.wedatasphere.dss.workflow.entity;
 import java.util.Date;
 import java.util.Map;
 
-public class DataViewNodeInfo {
+public class DataViewNodeInfo extends  NodeBaseInfo {
 
-    // 项目名称
-    private String projectName;
-
-    // 工作流名称
-    private String orchestratorName;
-
-    // 节点名称
-    private String nodeName;
-
-    // 节点类型
-
-    private String nodeType;
-
-    // 节点小类
-    private String nodeTypeName;
-
-    // 节点描述
-    private String nodeDesc;
 
     // 视图ID
     private String viewId;
 
-    //节点ID
-    private String nodeId;
-
-    //节点配置ID
-    private Long contentId;
-
-
-    // 工作流ID
-    private Long orchestratorId;
-
-    private  Long projectId;
-
-    private Date updateTime;
-
-    private Date createTime;
-
-    private String nodeKey;
-
-    private Map<String,String> nodeContent;
-
-    /**
-     * 1-接入git 0-不接入（默认）
-     */
-    private Boolean associateGit;
-    public String getProjectName() {
-        return projectName;
-    }
-
-    public void setProjectName(String projectName) {
-        this.projectName = projectName;
-    }
-
-    public String getOrchestratorName() {
-        return orchestratorName;
-    }
-
-    public void setOrchestratorName(String orchestratorName) {
-        this.orchestratorName = orchestratorName;
-    }
-
-    public String getNodeName() {
-        return nodeName;
-    }
-
-    public void setNodeName(String nodeName) {
-        this.nodeName = nodeName;
-    }
-
-    public String getNodeType() {
-        return nodeType;
-    }
-
-    public void setNodeType(String nodeType) {
-        this.nodeType = nodeType;
-    }
-
-    public String getNodeTypeName() {
-        return nodeTypeName;
-    }
-
-    public void setNodeTypeName(String nodeTypeName) {
-        this.nodeTypeName = nodeTypeName;
-    }
-
-    public String getNodeDesc() {
-        return nodeDesc;
-    }
-
-    public void setNodeDesc(String nodeDesc) {
-        this.nodeDesc = nodeDesc;
-    }
+    // 节点描述
+    private String nodeDesc;
 
     public String getViewId() {
         return viewId;
@@ -107,77 +20,11 @@ public class DataViewNodeInfo {
         this.viewId = viewId;
     }
 
-    public String getNodeId() {
-        return nodeId;
+    public String getNodeDesc() {
+        return nodeDesc;
     }
 
-    public void setNodeId(String nodeId) {
-        this.nodeId = nodeId;
-    }
-
-
-
-    public Long getOrchestratorId() {
-        return orchestratorId;
-    }
-
-    public void setOrchestratorId(Long orchestratorId) {
-        this.orchestratorId = orchestratorId;
-    }
-
-    public Long getContentId() {
-        return contentId;
-    }
-
-    public void setContentId(Long contentId) {
-        this.contentId = contentId;
-    }
-
-    public Long getProjectId() {
-        return projectId;
-    }
-
-    public void setProjectId(Long projectId) {
-        this.projectId = projectId;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getNodeKey() {
-        return nodeKey;
-    }
-
-    public void setNodeKey(String nodeKey) {
-        this.nodeKey = nodeKey;
-    }
-
-    public Map<String, String> getNodeContent() {
-        return nodeContent;
-    }
-
-    public void setNodeContent(Map<String, String> nodeContent) {
-        this.nodeContent = nodeContent;
-    }
-
-    public Boolean getAssociateGit() {
-        return associateGit;
-    }
-
-    public void setAssociateGit(Boolean associateGit) {
-        this.associateGit = associateGit;
+    public void setNodeDesc(String nodeDesc) {
+        this.nodeDesc = nodeDesc;
     }
 }

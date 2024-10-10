c558534101e40f3ebfe14cca8a5a9291fabae469
Zhaobincai <747839893@qq.com>
2024-10-10 11:31:37 +0800

    fix: saveAllFlowMetaData

--- dss-framework/framework-plugins/dss-framework-orchestrator-publish/src/main/java/com/webank/wedatasphere/dss/orchestrator/publish/impl/ImportDSSOrchestratorPluginImpl.java
+++ dss-framework/framework-plugins/dss-framework-orchestrator-publish/src/main/java/com/webank/wedatasphere/dss/orchestrator/publish/impl/ImportDSSOrchestratorPluginImpl.java
@@ -231,8 +231,7 @@ public class ImportDSSOrchestratorPluginImpl extends AbstractDSSOrchestratorPlug
             DSSFlow flowByID = dssFlowService.getFlow(appId);
             if (flowByID != null) {
                 dssFlowService.deleteFlowMetaData(versionOrchestratorId);
-                DSSFlow flowWithJsonAndSubFlowsByID = dssFlowService.getFlowWithJsonAndSubFlowsByID(appId);
-                saveAllFlowMetaData(flowWithJsonAndSubFlowsByID, versionOrchestratorId);
+                dssFlowService.saveAllFlowMetaData(appId, versionOrchestratorId);
             }
             DSSProject projectInfo = DSSFlowEditLockManager.getProjectInfo(projectId);
             if (projectInfo.getAssociateGit()) {
@@ -241,18 +240,6 @@ public class ImportDSSOrchestratorPluginImpl extends AbstractDSSOrchestratorPlug
         }
 
         return dssOrchestratorVersion;
-    }
-
-    private void saveAllFlowMetaData(DSSFlow dssFlow, Long orchestratorId) {
-        if (dssFlow == null) return;
-        List<? extends DSSFlow> subFlows = dssFlow.getChildren();
-        dssFlowService.saveFlowMetaData(dssFlow.getId(), dssFlow.getFlowJson(), orchestratorId);
-        if (subFlows != null) {
-            // 递归遍历子节点保存
-            for (DSSFlow subFlow : subFlows) {
-                saveAllFlowMetaData(subFlow, orchestratorId);
-            }
-        }
     }
 
     @Override

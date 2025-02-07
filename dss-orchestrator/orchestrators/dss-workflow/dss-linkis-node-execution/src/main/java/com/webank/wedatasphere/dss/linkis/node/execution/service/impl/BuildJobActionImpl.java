2d5ce5df950f245054bd0ab4c1b4b30e1dd403c2
SunpengWan <825559776@qq.com>
2025-02-07 09:25:42 +0800

    支持工作流选择Spark引擎版本

--- dss-orchestrator/orchestrators/dss-workflow/dss-linkis-node-execution/src/main/java/com/webank/wedatasphere/dss/linkis/node/execution/service/impl/BuildJobActionImpl.java
+++ dss-orchestrator/orchestrators/dss-workflow/dss-linkis-node-execution/src/main/java/com/webank/wedatasphere/dss/linkis/node/execution/service/impl/BuildJobActionImpl.java
@@ -25,7 +25,9 @@ import com.webank.wedatasphere.dss.linkis.node.execution.service.BuildJobAction;
 import org.apache.commons.lang3.SerializationUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.linkis.common.conf.CommonVars;
+import org.apache.linkis.manager.label.conf.LabelCommonConfig;
 import org.apache.linkis.manager.label.constant.LabelKeyConstant;
+import org.apache.linkis.manager.label.entity.engine.EngineType;
 import org.apache.linkis.manager.label.entity.engine.EngineTypeLabel;
 import org.apache.linkis.manager.label.utils.EngineTypeLabelCreator;
 import org.apache.linkis.protocol.constants.TaskConstant;
@@ -49,8 +51,12 @@ public class BuildJobActionImpl implements BuildJobAction {
     private Logger logger = LoggerFactory.getLogger(BuildJobActionImpl.class);
     private static BuildJobAction buildJobAction = new BuildJobActionImpl();
     private static final  String  NEBULA = "nebula";
+    private static final  String  SPARK = "spark";
     private static final CommonVars<String> NEBULA_ENGINE_VERSION =
             CommonVars.apply("wds.linkis.nebula.engine.version", "3.0.0");
+
+    private static final CommonVars<String> SPARK3_ENGINE_VERSION =
+            CommonVars.apply("wds.linkis.spark3.engine.version", "3.4.4");
 
     private BuildJobActionImpl() {
 
@@ -136,12 +142,23 @@ public class BuildJobActionImpl implements BuildJobAction {
 
         String code = parseExecutionCodeFor1X(job);
 
+        // 根据全局变量sparkVersion 处理Spark引擎版本,默认为2.x版本
+        if(SPARK.equalsIgnoreCase(job.getEngineType())){
+            Map<String,String> variable = (Map<String, String>) job.getParams().get("variable");
+            String sparkVersion = variable.get("sparkVersion");
+            if (StringUtils.isNotEmpty(sparkVersion) && "3".equals(sparkVersion.trim())){
+                EngineTypeLabelCreator.registerVersion(EngineType.SPARK().toString(),SPARK3_ENGINE_VERSION.getValue());
+            }else{
+                EngineTypeLabelCreator.registerVersion(EngineType.SPARK().toString(), LabelCommonConfig.SPARK_ENGINE_VERSION.getValue());
+            }
+        }
+
         EngineTypeLabel engineTypeLabel = EngineTypeLabelCreator.createEngineTypeLabel(parseAppConnEngineType(job.getEngineType(), job));
         //TODO 升级linkis1.7.0版本之后，这段特殊的硬编码逻辑要去掉
         if(NEBULA.equalsIgnoreCase( engineTypeLabel.getEngineType())){
             engineTypeLabel.setVersion(NEBULA_ENGINE_VERSION.getValue());
         }
-
+        logger.info("engine type key is {} ", engineTypeLabel.getStringValue());
         labels.put(LabelKeyConstant.ENGINE_TYPE_KEY, engineTypeLabel.getStringValue());
         labels.put(LabelKeyConstant.USER_CREATOR_TYPE_KEY, job.getUser() + "-" + LINKIS_JOB_CREATOR_1_X.getValue(job.getJobProps()));
         labels.put(LabelKeyConstant.CODE_TYPE_KEY, parseRunType(job.getEngineType(), job.getRunType(), job));

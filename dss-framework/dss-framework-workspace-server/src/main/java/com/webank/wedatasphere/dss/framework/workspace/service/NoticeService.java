33965fe1ae28164604bef3f29a3c3c4bccfa85c6
SunpengWan <825559776@qq.com>
2025-02-13 15:53:05 +0800

    更改部署启动报错的代码

--- dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/NoticeService.java
+++ dss-framework/dss-framework-workspace-server/src/main/java/com/webank/wedatasphere/dss/framework/workspace/service/NoticeService.java
@@ -1,11 +1,11 @@
-package com.webank.wedatasphere.dss.framework.workspace.service;
-
-import com.webank.wedatasphere.dss.framework.workspace.bean.NoticeContent;
-
-import java.util.List;
-
-public interface NoticeService {
-
-    List<NoticeContent> getNoticeContent();
-
-}
+//package com.webank.wedatasphere.dss.framework.workspace.service;
+//
+//import com.webank.wedatasphere.dss.framework.workspace.bean.NoticeContent;
+//
+//import java.util.List;
+//
+//public interface NoticeService {
+//
+//    List<NoticeContent> getNoticeContent();
+//
+//}

369e543b8b971a3868e11c1917a5fa8e64ca2d1d
SunpengWan <825559776@qq.com>
2025-04-15 20:18:01 +0800

    fix: selectProxyUserList 未添加param

--- dss-apps/dss-scriptis-server/src/main/java/com/webank/wedatasphere/dss/scriptis/dao/ScriptisProxyUserMapper.java
+++ dss-apps/dss-scriptis-server/src/main/java/com/webank/wedatasphere/dss/scriptis/dao/ScriptisProxyUserMapper.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Mapper
 public interface ScriptisProxyUserMapper extends BaseMapper<ScriptisProxyUser> {
 
-    List<ScriptisProxyUser> selectProxyUserList(String userName,String expireTime);
+    List<ScriptisProxyUser> selectProxyUserList(@Param("userName")String userName,@Param("expireTime") String expireTime);
 
     int insertUser(ScriptisProxyUser user);
 

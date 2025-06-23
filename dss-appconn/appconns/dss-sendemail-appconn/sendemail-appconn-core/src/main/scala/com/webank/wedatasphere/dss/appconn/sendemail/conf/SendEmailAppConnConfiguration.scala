a317c0e79aa898613b00b3d52070a217dcf560a4
zqburde <zhangqi111213@163.com>
2025-06-23 11:19:59 +0800

    Revert "support sendemail about md file."
    This reverts commit 31d9469f2c8e511d13b2fdb9e4bb87fa9b2f5f7b.

--- dss-appconn/appconns/dss-sendemail-appconn/sendemail-appconn-core/src/main/scala/com/webank/wedatasphere/dss/appconn/sendemail/conf/SendEmailAppConnConfiguration.scala
+++ dss-appconn/appconns/dss-sendemail-appconn/sendemail-appconn-core/src/main/scala/com/webank/wedatasphere/dss/appconn/sendemail/conf/SendEmailAppConnConfiguration.scala
@@ -52,6 +52,5 @@ object SendEmailAppConnConfiguration {
   val EMAIL_ENTITY_CLASSES = CommonVars("wds.dss.appconn.email.hook.entity.classes",
     "com.webank.wedatasphere.dss.appconn.sendemail.hook.entity.newvisualis.NewVisualisEmailInfo," +
       "com.webank.wedatasphere.dss.appconn.sendemail.hook.entity.visualis.VisualisEmailInfo," +
-      "com.webank.wedatasphere.dss.appconn.sendemail.hook.entity.visualis.MetaBaseEmailInfo," +
-      "com.webank.wedatasphere.dss.appconn.sendemail.hook.entity.mlss.MlssEmailInfo")
+      "com.webank.wedatasphere.dss.appconn.sendemail.hook.entity.visualis.MetaBaseEmailInfo")
 }
